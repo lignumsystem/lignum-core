dea820791a1418c2a30d537b7c79fee0f6d0348c
jarip <>
1999-06-24 09:35:43 +0000

    Comments added to explain the creation of a simple tree.

--- stl-lignum/main.cc
+++ stl-lignum/main.cc
@@ -43,29 +43,43 @@ int main(int argc, char *argv[])
   clarg = ParseCommandLine(argc,argv,"-file");
   if (clarg != empty)
     InitializeTree(tree,clarg);
+
+  //create a tree with a structure [TS,[[B],[B]],B]
+  //i.e, [TS,BP,B] which expands to [TS,[A,A],B] and to [TS,[[B],[B]],B]
+  //A= Axis, BP = BranchingPoint, TS = TreeSegment and B = Bud 
   Axis<MyTreeSegment>& axis = GetAxis(tree);
+  //create the first tree segment
   TreeSegment<MyTreeSegment> *ts = 
     new TreeSegment<MyTreeSegment>(Point<METER>(0,0,0),PositionVector(0,0,1.0),
 				   0,1,0.5,0.2,&tree);
+  //create the branching point
   BranchingPoint<MyTreeSegment> *bp = 
-    new BranchingPoint<MyTreeSegment>(Point<METER>(0,0,0),PositionVector(0,0,1.0),&tree);
+    new BranchingPoint<MyTreeSegment>(Point<METER>(0,0,0),
+				      PositionVector(0,0,1.0),&tree);
+  //create the terminating bud
   Bud<MyTreeSegment> *bud = new  Bud<MyTreeSegment>(Point<METER>(0,0,0),
 						    PositionVector(0,0,1.0),
 						    0,
 						    &tree);
+  //create a branch with one bud (Axis containing one Bud) into the branching point
   InsertTerminatingBud(*bp,new Bud<MyTreeSegment>(Point<METER>(0,0,0),
 						  PositionVector(0,0,1.0),
 						  0,
 						  &tree));
+  //create another branch with one bud (Axis containing one Bud) into the branching point
   InsertTerminatingBud(*bp,new Bud<MyTreeSegment>(Point<METER>(0,0,0),
 						  PositionVector(0,0,1.0),
 						  0,
 						  &tree));
-		       
+
+
+  //The tree will now look as  [TS,[[B],[B]],B]
   InsertTreeCompartment(axis,ts);
   InsertTreeCompartment(axis,bp);
   InsertTreeCompartment(axis,bud);
 
+  //traverse the tree and print out  the datatypes of tree compartments
+  //see class DisplayType in TreeFunctor.h and in TreeFunctor.cc
   axis = GetAxis(tree);
   list<TreeCompartment<MyTreeSegment>*>& ls = GetTreeCompartmentList(axis);
   for_each(ls.begin(),ls.end(),DisplayType<MyTreeSegment>());
