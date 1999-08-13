355c395519667a635ffe1d2de9adadecc73a7ee6
jarip <>
1999-08-13 15:54:08 +0000

    Moved the constructor of MyTreeSegment to the file MyTreeSegment.h

--- stl-lignum/TreeSegment/MyTreeSegment.cc
+++ stl-lignum/TreeSegment/MyTreeSegment.cc
@@ -1,8 +1,2 @@
 #include <MyTreeSegment.h>
 
-MyTreeSegment::MyTreeSegment(const Point<METER>& p, const PositionVector& d, const TP go,
-			     const METER l, const METER r, const METER rn, 
-			     Tree<MyTreeSegment>* t)
-  :TreeSegment<MyTreeSegment>(p,d,go,l,r,rn,t)
-{
-}
