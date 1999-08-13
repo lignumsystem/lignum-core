a7ecd7b4c5e8098f2b122d0aa17974d1821f02d2
jarip <>
1999-08-13 07:39:08 +0000

    Creating a model (template) for Heartwood Tree Segment called HwTreeSegment.

---
+++ stl-lignum/include/HwTreeSegment.h
@@ -0,0 +1,14 @@
+#ifndef HWTREESEGMENT_HPP
+#define HWTREESEGMENT_HPP
+
+#include <Tree.h>
+
+template <class TS>
+class HwTreeSegment:public TreeSegment<TS>{
+public:
+  HwTreeSegment(const Point<METER>& p,const PositionVector& pv,const TP go,const METER l,
+		const METER r,const METER rn,Tree<TS>* tree)
+    :TreeSegment<TS>(p,pv,go,l,r,rn,tree);
+};
+
+#endif
