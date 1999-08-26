d391d89acf80b46afda24b4610bd9224c7715a78
mlehtone <>
1999-08-26 10:01:39 +0000

    ml

---
+++ stl-lignum/include/VisualFunctor.h
@@ -0,0 +1,39 @@
+#ifndef VISUALFUNCTOR_H
+#define VISUALFUNCTOR_H
+
+#include <vector>
+#include <Point.h>
+
+template <class TS, class BUD>
+class GUS{
+public:
+  GUS(int init = 0):flag(init){}  
+  int flag;
+  Point<METER> point;
+  vector<TreeSegment<TS,BUD>*> v;
+};
+
+template <class TS,class BUD>
+class GetUpperSegment{
+public:
+  GUS<TS,BUD>& operator()(GUS<TS,BUD>& gus,TreeCompartment<TS,BUD>* tc)const;
+};
+
+
+template <class TS, class BUD>
+class GLS{
+public:
+  GLS(int init = 0){current=NULL;segment = NULL;} 
+  TreeSegment<TS,BUD> *current; 
+  TreeSegment<TS,BUD> *segment;
+};
+
+template <class TS,class BUD>
+class GetLowerSegment{
+public:
+  GLS<TS,BUD>& operator()(GLS<TS,BUD>& gls,TreeCompartment<TS,BUD>* tc)const;
+};
+
+
+
+#endif
