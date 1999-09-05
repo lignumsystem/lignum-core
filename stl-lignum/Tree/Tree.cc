1946d410103bf03252e70057dd8ab07ff41323fc
jarip <>
1999-09-05 14:46:25 +0000

    Moved none template constructors, methods and functions for Tree here.

---
+++ stl-lignum/Tree/Tree.cc
@@ -0,0 +1,19 @@
+#include <Tree.h>
+
+TreeParameters::TreeParameters()
+{
+  af = ar = lr = mf = 0.0;
+  mr = ms = na = nl = pr = q = sr = ss = rho = xi = 0.0;
+}
+
+TreeAttributes::TreeAttributes()
+{
+  age = 0; lb = 0.0;
+  P = M = Wr = 0.0;
+}
+
+TreeTransitVariables::TreeTransitVariables()
+  :lambda(0.0),g(9.81),eta(1/pow(10,3)),Er((7.993-(6.030e-1))*1000000),k(1/pow(10,12)),rhow(1000.0)
+{
+}
+
