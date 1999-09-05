11af2aa4e10dafd66e43bba1426b20e4a76cf074
jarip <>
1999-09-05 14:46:36 +0000

    Moved none template constructors, methods and functions for TreeSegment here.

---
+++ stl-lignum/TreeSegment/TreeSegment.cc
@@ -0,0 +1,7 @@
+#include <TreeSegment.h>
+
+TreeSegmentAttributes::TreeSegmentAttributes()
+  :age(0),M(0.0),L(0.0),omega(0.0),P(0.0),Qin(0.0),Qabs(0.0),
+   R(0.0),Rn(0.0),Pr(1.0),fin(0.0),fout(0.0)
+{
+}
