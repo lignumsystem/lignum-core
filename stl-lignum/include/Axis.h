f6f25102bac45849daa34708938c145e7f402af4
jarip <>
2000-02-23 15:30:26 +0000

    The problem with template friend functions continue. SGI MipsPro and GNU g++ differ a bit.
    SGI does not allow
    
        friend Bud<TS,BUD>* GetTerminatingBud<TS,BUD>(const Axis<TS,BUD>& axis);
    
    This is required by GNU g++ compiler.
    
    SGI requires
    
        friend Bud<TS,BUD>* GetTerminatingBud(const Axis<TS,BUD>& axis);
    
    This is not allowed by GNU g++ compiler.
    
    Well anyway, the good news is that it is now possible to compile
    both c++adt and stl-lignum in UNIX and in Linux.

--- stl-lignum/include/Axis.h
+++ stl-lignum/include/Axis.h
@@ -10,17 +10,17 @@
 namespace Lignum{
 
 using namespace cxxadt;
-template <class TS,class BUD = DefaultBud<TS> >
+template <class TS,class BUD>
 Bud<TS,BUD>* GetTerminatingBud(const Axis<TS,BUD>& axis);
-template <class TS,class BUD = DefaultBud<TS> >
+template <class TS,class BUD>
 list<TreeCompartment<TS,BUD>*>& GetTreeCompartmentList(Axis<TS,BUD>& axis);
-template <class TS,class BUD = DefaultBud<TS> >
+template <class TS,class BUD>
 void InsertTreeCompartment(Axis<TS,BUD>& axis, TreeCompartment<TS,BUD>* ts);
-template <class TS,class BUD = DefaultBud<TS> >
+template <class TS,class BUD=DefaultBud<TS> >
 class Axis: public TreeCompartment<TS,BUD>{
-  friend Bud<TS,BUD>* GetTerminatingBud<TS,BUD>(const Axis<TS,BUD>& axis);
-  friend list<TreeCompartment<TS,BUD>*>& GetTreeCompartmentList<TS,BUD>(Axis<TS,BUD>& axis);
-  friend void InsertTreeCompartment<TS,BUD>(Axis<TS,BUD>& axis, TreeCompartment<TS,BUD>* ts);
+  friend Bud<TS,BUD>* GetTerminatingBud(const Axis<TS,BUD>& axis);
+  friend list<TreeCompartment<TS,BUD>*>& GetTreeCompartmentList(Axis<TS,BUD>& axis);
+  friend void InsertTreeCompartment(Axis<TS,BUD>& axis, TreeCompartment<TS,BUD>* ts);
 public:
   virtual ~Axis();
   Axis();
