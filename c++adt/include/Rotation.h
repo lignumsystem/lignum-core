b6c1d98a6f550710cda53678f8465211a6bff627
jarip <>
2000-05-17 14:48:51 +0000

    The type ROTATION is now defined in Rotation.h for both RMatrix and HRMatrix classes.

---
+++ c++adt/include/Rotation.h
@@ -0,0 +1,11 @@
+#ifndef ROTATION_H
+#define ROTATION_H
+
+namespace cxxadt{
+
+enum _rotation_direction {ROTATE_X,ROTATE_Y,ROTATE_Z};
+typedef enum _rotation_direction ROTATION;
+
+} //closing namespace cxxadt
+
+#endif
