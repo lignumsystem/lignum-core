5c0200f2c3ceb1968ba87e59f9dee76d55b66930
jarip <>
2000-05-17 14:44:31 +0000

    Moved the definition and the declaration of the constant functions (fn_1, fn_0, neg_sin and neg_cos) used in RMatrix and in HRMatrx to mathsym.h because they are needed in both classes.

--- c++adt/include/mathsym.h
+++ c++adt/include/mathsym.h
@@ -1,6 +1,6 @@
 #ifndef MATHSYM_H
 #define MATHSYM_H
-
+#include <math.h>
 namespace cxxadt{
 
 #define PI_VALUE 3.14159265358979323846
@@ -26,7 +26,15 @@ inline T minimum(T A, T B)
 
 typedef double RADIAN;
 
-//useful mathematical functions implemented
+//constant functions used in RMatrix and in HRMatrix
+//for the rotations in 3D space.
+inline RADIAN fn_1(RADIAN angle){return 1.0;}
+inline RADIAN fn_0(RADIAN angle){return 0.0;}
+inline RADIAN neg_sin(RADIAN angle){return (RADIAN) -sin(angle);}
+inline RADIAN neg_cos(RADIAN angle){return (RADIAN) -cos(angle);}
+
+
+//useful mathematical random function implemented
 //returns uniformally distributed random number [0.0,1.0]
 extern double ran3(int *seed);   
 
