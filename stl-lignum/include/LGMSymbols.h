df90752c93710371584754813db66210ed78fd49
matrps <>
2001-06-12 07:19:54 +0000

    Removed water flow variables fin, fout, Pr, Wm

--- stl-lignum/include/LGMSymbols.h
+++ stl-lignum/include/LGMSymbols.h
@@ -10,8 +10,38 @@ namespace Lignum{
 
 // Tree Attribute Double, Wm lisätty
 // Wr           dry weight of the roots
-enum LGMAD {A,age,dof,fin,fout,H,Hm,ip,lb,LM,LP,LWf,L,lw,M,omega,P,Pr,\
-            Qin,Qabs,R,Rf,Rh,Rtop,sf,sw,tauL,Wf,Wm,Ws,Wh,Wr};  
+enum LGMAD {A,age,dof,H,Hm,ip,lb,LM,LP,LWf,L,lw,M,omega,P,\
+            Qin,Qabs,R,Rf,Rh,Rtop,sf,sw,tauL,Wf,Ws,Wh,Wr};
+
+//  A       0   Area (of what?)
+//  age     1   age of object
+//  dof     2   degree of filling?
+//  H       3   height of tree
+//  Hm      4   ???
+//  ip      5   degree of interction
+//  lb      6   ???
+//  LM      7   ???
+//  LP      8   ???
+//  LWf     9   ???
+//  L       10  length of segment
+//  lw      11  ???
+//  M       12  rate of respiration (= amount of r. during the time step)
+//  omega   13  Gravelius order
+//  P       14  Photosynthetic rate (= amount of p. during time step)
+//  Qin     15  incoming radiant flux
+//  Qabs    16  amount of absorbed radiation
+//  R       17  radius of segment
+//  Rf      18  radius of segment cylinder that contains also foliage (cofers)
+//  Rh      19  radius of heartwood
+//  Rtop    20  raqdiaus of segment at upper end
+//  sf      21  specifi leaf area (=leaf area/ leaf weight)
+//  sw      22  ????
+//  tauL    23  transmission coefficient of leaf (in direction of the ray of light)
+//  Wf      24  foliage mass (kg C)
+//  Ws      25  mass of sapwood (kg C)
+//  Wh      26  mass of heartwood (kg C)
+//  Wr      27  root mass (kg C)
+
 
 
 //Tree Attribute STATUS
