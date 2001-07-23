3e86773a0024c804c3fa7f54c82b47f2f4f244c1
jarip <>
2001-07-23 13:31:18 +0000

    Comments for stdafx.h on Unix.

---
+++ c++adt/include/stdafx.h
@@ -0,0 +1,9 @@
+/******************************************************
+ *This is deliberately empty header file.             *
+ *MS VC++ compiler requires stdafx.h to be            *
+ *included in every source file (suffix cc, cpp etc.).*
+ *On Unix include this file in the beginning of the   *
+ *source file. It is not possible to use conditional  *
+ *compiling. MS VC++ checks if stdafx.h is truly      *
+ *included.                                           *
+ ******************************************************/
