8b7f3eb18ffdc60195376eca6c519a7f92ff81cf
jarip <>
2001-09-21 09:14:43 +0000

    Removed the nested namespace around Tree.h.

--- stl-lignum/include/Lignum.h
+++ stl-lignum/include/Lignum.h
@@ -1,14 +1,9 @@
 #ifndef LIGNUM_H
 #define LIGNUM_H
 
-namespace Lignum{
-
+//Please do not wrap Tree.h into namespace Lignum. 
+//That will create a nested namespace and will cause strange
+//errors difficult to track when including header files
 #include <Tree.h>
-
-}//closing namespace Lignum
-
-
-
-
 
 #endif
