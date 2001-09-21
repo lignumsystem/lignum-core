08ab1161f16c4c810d8475507e3cf38735c7486f
jarip <>
2001-09-21 11:31:15 +0000

    Strange compilation errors in gcc. Trying to compile on SGI instead.

--- LEngine/L2Lignum/L2Lignum.h
+++ LEngine/L2Lignum/L2Lignum.h
@@ -1,16 +1,33 @@
+#ifndef LTOLIGNUM_H
+#define LTOLIGNUM_H
+
 #include <string.h>
 #include <stack> 
 #include <Lignum.h>
 #include <Turtle.h>
 #include <lstring.h>
-#include <lstringiter.h>
+#include <lstriter.h>
 
 using namespace std;
+using namespace Lignum;
 
 template <class TS, class BUD>
-void L2Lignum(BranchingPoint<TS,BUD>& bp, LstringIterator& iterator, 
+int L2Lignum(BranchingPoint<TS,BUD>& bp, LstringIterator& iterator, 
+	      stack<Turtle>& turtle_stack);
+
+template <class TS, class BUD>
+int L2Lignum(Axis<TS,BUD>& axis, LstringIterator& iterator, 
+	     stack<Turtle>& turtle_stack);
+
+
+template <class TS, class BUD>
+int L2Lignum(BranchingPoint<TS,BUD>& bp, LstringIterator& iterator, 
 	      stack<Turtle>& turtle_stack)
 {
+  Axis<TS,BUD> axis(GetPoint(turtle_stack.top()), 
+		    GetHeading(turtle_stack.top()),
+		    &GetTree(bp)); 
+
   const char* name = iterator.GetCurrentModuleName();
 
   //Branching point sees "SB" --> new Axis
@@ -20,34 +37,48 @@ void L2Lignum(BranchingPoint<TS,BUD>& bp, LstringIterator& iterator,
 					  &GetTree(bp));
     InsertAxis(bp,axis);
     //Go to the first symbol of the new axis
-    L2Lignum(*axis,iterator++,turtle_stack);
+    return L2Lignum(*axis,iterator++,turtle_stack);
   }
   //Branching point sees "EB" --> end of Axis
   //Go to next symbol
   else if (strcmp(name,"EB") == 0){
-    L2Lignum(bp,iterator++,turtle_stack);
+    return L2Lignum(bp,iterator++,turtle_stack);
   }
   //This can only be a symbol  "S" in between "SB" and "EB": "] S ["
-  //It means end of branching point, return and let the Axis see the symbol S
-  else return;
+  //It means end of branching point, return and let the calling Axis see the symbol S
+  else{
+    return 1; //end of bp
+  }
 }
   
 template <class TS, class BUD>
-void L2Lignum(Axis<TS,BUD>& axis, LstringIterator& iterator, 
-	      stack<Turtle>& turtle_stack)
+int L2Lignum(Axis<TS,BUD>& axis, LstringIterator& iterator, 
+	     stack<Turtle>& turtle_stack)
 {
   const char* name = NULL;
+  CallerData caller_data;
 
-  if (iterator.AtEnd())
-    return;
-  
-  name = iterator.GetCurrentModuleName(); 
-  //End of Axis, return
-  //Let the Branching point see the "EB" again
-  if (strcmp(name,"EB") == 0){
+  if (iterator.AtEnd()){
+    return 0; //exit
+  }
+
+  else if (strcmp(name,"EB") == 0){
     turtle_stack.pop();
-    return;
-  }  
+    return 2; //end of axis
+  } 
+  //Forward, also create a segment
+  else if (strcmp(name,"F") == 0){
+    double arg1 = 0.0;
+    caller_data.Reset();
+    caller_data.Strct.AddModuleAddr(iterator.Ptr());
+    memcpy(&arg1,caller_data.Strct.pArg(0),sizeof(double));
+    TreeSegment<TS,BUD>* ts = new TreeSegment<TS,BUD>(GetPoint(turtle_stack.top()),
+						      GetHeading(turtle_stack.top()),
+						      0.0,arg1,0.10,0.05,&GetTree(axis));
+    InsertTreeCompartment(axis,ts);
+    turtle_stack.top().forward(arg1);
+    return L2Lignum(axis,iterator++,turtle_stack);
+  }
   //Axis sees "SB" --> new Branching point
   //Also push the current status of the turtle to the stack
   //Don't go forward, let the Branching point see the "SB" again
@@ -57,55 +88,60 @@ void L2Lignum(Axis<TS,BUD>& axis, LstringIterator& iterator,
 							    &GetTree(axis));
     InsertTreeCompartment(axis,bp);
     turtle_stack.push(turtle_stack.top());
-    //Create the branching point
-    L2Lignum(*bp,iterator,turtle_stack);
-  }
-  //Forward, also create a segment
-  else if (strcmp(name,"F") == 0){
-    double arg1 = 0.0;
-    memcpy(&arg1,caller_data.Strct.pArg(0),sizeof(double));
-    TreeSegment<TS,BUD>* ts = new TreeSegment<TS,BUD>(GetPoint(turtle_stack.top()),
-						      GetHeading(turtle_stack.top()),
-						      0.0,arg1,0.10,0.05,&GetTree(axis));
-    InsertTreeCompartment(axis,ts);
-    turtle_stack.top().forward(arg1);
+    //Read the branching point
+    return L2Lignum(*bp,iterator,turtle_stack);
   }
   //Turn
   else if (strcmp(name,"Turn") == 0){
     double arg1 = 0.0;
+    caller_data.Reset();
+    caller_data.Strct.AddModuleAddr(iterator.Ptr());
     memcpy(&arg1,caller_data.Strct.pArg(0),sizeof(double));
     turtle_stack.top().turn(arg1);
+    return L2Lignum(axis,iterator++,turtle_stack);
   }
   //Pitch
   else if (strcmp(name,"Pitch") == 0){
     double arg1 = 0.0;
+    caller_data.Reset();
+    caller_data.Strct.AddModuleAddr(iterator.Ptr());
     memcpy(&arg1,caller_data.Strct.pArg(0),sizeof(double));
-    turtle_stack.top().pitch(-arg1);
+    turtle_stack.top().pitch(arg1);
+    return L2Lignum(axis,iterator++,turtle_stack);
   }
   //Roll
   else if (strcmp(name,"Roll") == 0){
     double arg1 = 0.0;
+    caller_data.Reset();
+    caller_data.Strct.AddModuleAddr(iterator.Ptr());
     memcpy(&arg1,caller_data.Strct.pArg(0),sizeof(double));
-    turtle_stack.top().pitch(-arg1);
+    turtle_stack.top().roll(arg1);
+    return L2Lignum(axis,iterator++,turtle_stack);
   }
   //Bud
   else if (strcmp(name,"B") == 0){
     Bud<TS,BUD>* bud = new Bud<TS,BUD>(GetPoint(turtle_stack.top()),
-				       GetHeading(turtle_stack.top()),0.0,,&GetTree(axis));
+				       GetHeading(turtle_stack.top()),
+				       0.0,&GetTree(axis));
+    return L2Lignum(axis,iterator++,turtle_stack);
   }
   //Ignore other symbols, go forward in the string
-  return L2Lignum(bp,iterator++,turtle_stack);
-
+  else 
+    return L2Lignum(axis,iterator++,turtle_stack);
 }
 
 template <class TS, class BUD>
-void L2Lignum(Tree<TS,BUD>& t,const Lstring& s)
+int L2Lignum(Tree<TS,BUD>& t,const Lstring& s)
 {
   stack<Turtle> turtle_stack;
   Turtle turtle;
   LstringIterator iterator(s); 
   Axis<TS,BUD>& a = GetAxis(t);
-  turtle_stack.push(turtle);
 
-  L2Lignum(a,iterator,turtle_stack);
+  turtle_stack.push(turtle);
+  
+  return L2Lignum(a,iterator,turtle_stack);
 }
+
+
+#endif
