43b3f75e2e5d113af391d221df0aecfaf153f0ae
jarip <>
2002-01-31 13:21:14 +0000

    collision initializes to 0.

--- stl-lignum/Bud/Bud.cc
+++ stl-lignum/Bud/Bud.cc
@@ -2,7 +2,7 @@
 namespace Lignum{
 
 BudAttributes::BudAttributes()
-  :collision(-10.0),ip(0.0),omega(0.0),type(0.0),status(0.0),state(state)
+  :collision(0.0),ip(0.0),omega(0.0),type(0.0),status(0.0),state(state)
 {
 }
 
