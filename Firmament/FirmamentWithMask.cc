bf7d34f27ad3e7552af8644d8f7d6afcc468f919
jarip <>
2002-02-13 08:09:33 +0000

    Added  implementation   of  readMask   that  enables  to   define  the configuration  of the  firmament  in  a single  file.   A sample  file Firmament.txt is in stl-lignum.

--- Firmament/FirmamentWithMask.cc
+++ Firmament/FirmamentWithMask.cc
@@ -1,58 +1,102 @@
 #include <stdlib.h>
 #include <FirmamentWithMask.h>
-#include <Lex.h>
+
 
 namespace sky{
 
-FirmamentWithMask::FirmamentWithMask(int no_incl,int no_azim)
-  :Firmament(no_incl,no_azim)
-{
-  /*empty*/
-}
+  FirmamentWithMask::FirmamentWithMask(int no_incl,int no_azim)
+    :Firmament(no_incl,no_azim)
+  {
+    /*empty*/
+  }
 
-void FirmamentWithMask::readMaskFile(const string& file)
-{
-  Lex lex;
-  lex.scan(file);
+  //Intializing the firmament from a file that contains
+  //size (inclinations and azimuths), radiation intensity
+  //and an optional mask for the firmament
+  void FirmamentWithMask::configure(const string& file)
+  {
+    Lex lex;
+    lex.scan(file);
+
+    Token t = lex.getToken();
+    int incl = atoi(t.getValue().c_str());
+    t = lex.getToken();
+    int azim = atoi(t.getValue().c_str());
+    t = lex.getToken();
+    int drp = atof(t.getValue().c_str());
+    //size and radiation intensity
+    resize(incl,azim,drp);
+    //optional mask
+    readMask(lex);
+  }
+    
+  void FirmamentWithMask::readMask(Lex& lex)
+  {
+    //get the first inclinatation
+    Token t1 = lex.getToken();
+    while (t1.getType() != ENDFILE){
+      string v1str =  t1.getValue();
+      //there will be value for inclination (blocking)
+      Token t2 = lex.getToken();
+      string v2str = t2.getValue();
+      //percentage
+      double val = atof(v2str.c_str()); 
+      if (t1.getType() == ID){//zenith sector, keyword "zenith"
+	diffuseRadZenith = diffuseRadZenith*((100.0-val)/100.0);
+      }
+      else{
+	//index
+	int index = atoi(v1str.c_str());
+	//loop through one inclination denoted by index
+	setMask(index,val);
+      }
+      t1 = lex.getToken();
+    }
+  }
   
-  //get the first inclinatation
-  Token t1 = lex.getToken();
-  while (t1.getType() != ENDFILE){
-    string v1str =  t1.getValue();
-    //there will be value for inclination (blocking)
-    Token t2 = lex.getToken();
-    string v2str = t2.getValue();
-    //percentage
-    double val = atof(v2str.c_str()); 
-    if (t1.getType() == ID){//zenith sector, keyword "zenith"
-      diffuseRadZenith = diffuseRadZenith*((100.0-val)/100.0);
+  void FirmamentWithMask::readMaskFile(const string& file)
+  {
+    Lex lex;
+    lex.scan(file);
+  
+    //get the first inclinatation
+    Token t1 = lex.getToken();
+    while (t1.getType() != ENDFILE){
+      string v1str =  t1.getValue();
+      //there will be value for inclination (blocking)
+      Token t2 = lex.getToken();
+      string v2str = t2.getValue();
+      //percentage
+      double val = atof(v2str.c_str()); 
+      if (t1.getType() == ID){//zenith sector, keyword "zenith"
+	diffuseRadZenith = diffuseRadZenith*((100.0-val)/100.0);
+      }
+      else{
+	//index
+	int index = atoi(v1str.c_str());
+	//loop through one inclination denoted by index
+	setMask(index,val);
+      }
+      t1 = lex.getToken();
     }
-    else{
-      //index
-      int index = atoi(v1str.c_str());
-      //loop through one inclination denoted by index
-      setMask(index,val);
+  }
+
+  //sets a mask for one inclination by reducing incoming radiation
+  //by argument percentage
+  void FirmamentWithMask::setMask(int incl_index,double percentage)
+  {
+    int j = 0;
+    for (j = 0; j < azimDivisions[incl_index]; j++){
+      diffuseRad[incl_index][j] = diffuseRad[incl_index][j]*((100.0-percentage)/100.0);
     }
-    t1 = lex.getToken();
-  }
-}
 
-//sets a mask for one inclination by reducing incoming radiation
-//by argument percentage
-void FirmamentWithMask::setMask(int incl_index,double percentage)
-{
-  int j = 0;
-  for (j = 0; j < azimDivisions[incl_index]; j++){
-    diffuseRad[incl_index][j] = diffuseRad[incl_index][j]*((100.0-percentage)/100.0);
-  }
+    diffuseRadBall = diffuseRadZenith;
 
-  diffuseRadBall = diffuseRadZenith;
-
-  vector<double> radiation_direction(3);
-  for (j = 0; j < numberOfRegions(); j++){
-    diffuseRadBall += diffuseRegionRadiationSum(j,radiation_direction);
+    vector<double> radiation_direction(3);
+    for (j = 0; j < numberOfRegions(); j++){
+      diffuseRadBall += diffuseRegionRadiationSum(j,radiation_direction);
+    }
   }
-}
 
 } //closing namespace sky
 
