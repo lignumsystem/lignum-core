0cfb8ee2a858ec11ef659aa950acb63ee8f9c200
jarip <>
2002-04-22 13:20:57 +0000

    Two  classes  Uniform  and   Gauss  return  uniformally  and  normally distributed  random  numbers.   The  algorithms  are the  same  as  in functions "ran3"  and "gauss", but  the classes (and the  instances of the classes ) are independent from each other.  "ran3" declares static variables common throughout the  program.  This means that when "ran3" is  initialized  also  "gauss"  will be  initialized  too  implicitely (because gauss calls ran3).

---
+++ c++adt/include/Gauss.h
@@ -0,0 +1,19 @@
+#ifndef GAUSS_H
+#define GAUSS_H
+
+#include <Uniform.h>
+namespace cxxadt{
+  //Return Normally distributed random number with mean 'm' and 
+  //standard deviate 's".
+  class Gauss{
+  public:
+    Gauss(int seed = -1):uniform(seed){}
+    double init(int seed = -1){return uniform.init(seed);}
+    double operator()(double m, double s, int seed);
+  private:
+    Uniform uniform;
+  };
+
+}//closing namespace
+
+#endif
---
+++ c++adt/include/Uniform.h
@@ -0,0 +1,20 @@
+#ifndef UNIFORM_H
+#define UNIFORM_H
+
+namespace cxxadt{
+  //Return uniform random number in range [0.0,1.0]
+  class Uniform{
+  public:
+    Uniform(int seed = -1):iff(0){init(seed);}
+    double init(int seed = -1){return uran(&seed);}
+    double operator () (int seed){return uran(&seed);}
+  private:
+    double uran(int* idum);
+    int inext,inextp;
+    long ma[56];
+    int iff;
+  };
+
+}//close namespace 
+
+#endif
