6f139475cfff440ff54c5f18218e67e3de445a88
jarip <>
2002-05-06 06:01:13 +0000

    Added cs-plant as an example for context sensistve L-systems.

--- LEngine/L2Lignum/main.cc
+++ LEngine/L2Lignum/main.cc
@@ -19,68 +19,118 @@ namespace Bearberry{
 #include <LSystem.h>
 }
 
-//and for pine, also scotspine9bp.L in lsys.
+//and for pine, see also pine9bp.L in lsys.
 namespace Pine{
 #include <LSystem.h>
 }
 
+namespace CSPlant{
+#include <LSystem.h>
+}
 
 int main(int argc, char** argv)
 {
-  //Create the pl and l to run the rewriting
-  Pine::LSystem pl;
-  Bearberry::LSystem l;
+  //Create the bl  (bearberry) and pl1 and pl2 (two  pines) to run the
+  //L-system  rewriting.  Bearberry  exchanges  data, attributes  from
+  //enumeration type LGMAD of type LGMdouble
+  Bearberry::LSystem<MyTreeSegment,MyBud,LGMAD,LGMdouble> bl;
+  //Pine  does not  include data  exchange.  Demonstrate  that several
+  //individuals of  the same  species can be  simulated. Note  you can
+  //leave out  the attribute  name and type  in the  template argument
+  //list.
+  Pine::LSystem<MyTreeSegment,MyBud> pl1;
+  Pine::LSystem<MyTreeSegment,MyBud> pl2;
 
-  //Create two trees, bearberry and pine
+  CSPlant::LSystem<MyTreeSegment,MyBud> csl;
+  //Create three trees, bearberry and  two pines (well, bearberry is a
+  //berry...)
   Tree<MyTreeSegment,MyBud> bearberry(Point(0,0,0),
-				 PositionVector(0,0,1.0));
-  Tree<MyTreeSegment,MyBud> pine(Point(0,0,0),
-				 PositionVector(0,0,1.0));
-
-  //Bearberry
-  //Typical loop in rewriting
-  //Expand the axiom
-  l.start();
-  //Update  LIGNUM with  interchange of  information In  each  bud the
-  //attributes  LGMtype,LGMstatus and  LGMcollision  are updated.   In
-  //bearberry.L the symbol B contains  (at lest) 3 arguments and their
-  //meaning is the same from left to right.
-  l.lstring2Lignum<MyTreeSegment,MyBud,LGMAD,LGMdouble>(bearberry,3,LGMtype,LGMstatus,LGMcollision);
-  //Loop derivation length of times
-  for (int i=0; i < l.derivationLength(); i++)
+				      PositionVector(0,0,1.0));
+  Tree<MyTreeSegment,MyBud> pine1(Point(0,0,0),
+				  PositionVector(0,0,1.0));
+  Tree<MyTreeSegment,MyBud> pine2(Point(0,0,0),
+				  PositionVector(0,0,1.0));
+  Tree<MyTreeSegment,MyBud> csplant(Point(0,0,0),
+				    PositionVector(0,0,1.0));
+  //Bearberry demonstrates typical loop in L-system rewriting 
+  //1. Expand the axiom.
+  bl.start();
+  //2. Update LIGNUM with  interchange of information.  In each LIGNUM
+  //bud the attributes of the name LGMtype, LGMstatus and LGMcollision
+  //are updated from symbols B in the L-system string.  In bearberry.L
+  //the  symbol  B contains  3  arguments  and  their meaning  and  is
+  //LGMtype, LGMstatus  and LGMcollision from left to  right. The data
+  //type of these attributes is LGMdouble both in LIGNUM and in symbol
+  //B in bearberry.L.
+  bl.lstringToLignum(bearberry,3,LGMtype,LGMstatus,LGMcollision);
+  //3. Loop derivation length of times.
+  for (int i=0; i < 9 ; i++)
   {
     cout << "Step: " << i << endl;
-    //Detect collision, other metabolic processes may follow
-    ForEach(bearberry,EvaluateCollisionForAllBuds<MyTreeSegment,MyBud>(90.0*2.0*PI_VALUE/360.0,0.10));
-    //Update the string; LGMtype,LGMstatus,LGMcollision are copied from buds to symbols B in the string
-    l.lignum2Lstring<MyTreeSegment,MyBud,LGMAD,LGMdouble>(bearberry,3,LGMtype,LGMstatus,LGMcollision);  
-    //Expand the string
-    l.derive();
-    //Update  LIGNUM with  interchange of  information
-    l.lstring2Lignum<MyTreeSegment,MyBud,LGMAD,LGMdouble>(bearberry,3,LGMtype,LGMstatus,LGMcollision);
+    //4.       Detect       collision.       The      argument      in
+    //EvaluateCollisionForAllBuds  is the  opening  angle in  radians,
+    //i.e.  the visibile sector for a bud.  The second argument is the
+    //lookahead  distance  in meters.  Other  metabolic processes  may
+    //follow.
+    ForEach(bearberry,EvaluateCollisionForAllBuds<MyTreeSegment,MyBud>(60.0*2.0*PI_VALUE/360.0,0.10));
+    //5. Update the  string; LGMtype,LGMstatus,LGMcollision are copied
+    //from LIGNUM buds to symbols B in the string.
+    bl.lignumToLstring(bearberry,3,LGMtype,LGMstatus,LGMcollision);  
+    //6. Expand the  string. If you have defined  StartEach or EndEach
+    //statements derive executes these statements.
+    bl.derive();
+    //7.   Update LIGNUM  with interchange  of  information.  LGMtype,
+    //LGMstatus, LGMcollision are copied  from symbols B in the string
+    //to LIGNUM buds.
+    bl.lstringToLignum(bearberry,3,LGMtype,LGMstatus,LGMcollision);
   }
-  //Some clean up if defined in L-file; otherwise end is a dummy method
-  l.end();  
+  //8. Some clean up if  End statement is defined in L-file; otherwise
+  //end is a dummy method.
+  bl.end();  
 
 
-  //Scots pine
-  pl.start();
-  pl.lstring2Lignum<MyTreeSegment,MyBud,LGMAD,LGMdouble>(pine);
+  //Two Scots  pines. Demonstrate that several individuals  of the same
+  //species can be simulated.
+  //Expand axioms.
+  pl1.start();
+  pl2.start();
+  pl1.lstringToLignum(pine1);
+  pl2.lstringToLignum(pine2);
   //Naturally, one can use  other termination conditions than just the
   //derivation length.
-  for (int i=0; i < 7; i++)
+  for (int i=0; i < 1; i++)
   {
     cout << "Step: " << i << endl;
-    //No interchange  of information, but so  far LGMAD,LGMdouble must
-    //be used  to complete the  template arguments. May change  in the
-    //future if possible (default tempalte arguments). 
-    pl.lignum2Lstring<MyTreeSegment,MyBud,LGMAD,LGMdouble>(pine);  
-    pl.derive();
-    pl.lstring2Lignum<MyTreeSegment,MyBud,LGMAD,LGMdouble>(pine);
+    //Update L-strings, no interchange  of information.
+    pl1.lignumToLstring(pine1);
+    pl2.lignumToLstring(pine2);
+    //Structural development for the trees.
+    pl1.derive();
+    pl2.derive();
+    //Update pine structures, no interchange  of information.
+    pl1.lstringToLignum(pine1);
+    pl2.lstringToLignum(pine2);
   }
-  pl.end();  
+  //Clean up.
+  pl1.end();  
+  pl2.end();
+
+  //CSPlant demonstrates context sensitive L-systems 
+  //Grammar needs to be modified for LIGNUM
+  csl.start();
+  for (int i=0; i < 0; i++){
+    cout <<  "Step: " << i << endl;
+    csl.derive();
+  }
+  csl.end();  
 
   //Finally visualize the tree.
-  VisualizeLGMTree(pine);
+  DisplayStructure(bearberry);
+  VisualizeLGMTree(bearberry);
 
 }
+
+
+
+
+
