b111a931c1e2215352ce2296d7ae230ee17ca0bd
jarip <>
2002-05-10 07:27:29 +0000

    Added functor CreateLeaves that creates leaves to a broadleaved trees. Usage: 	vector<PositionVector> v; 	AccumulateDown(tree,v, 		       AppendSequence<vector<PositionVector> >(),                        CreateLeaves<MyHwTreeSegment,MyHwBud>());
    The algorithm is based on LGMstatus field of a Bud. If status is 1 the
    direction  vector  of  the  bud  is  added to  'v'  and  passed  down.
    Branching  point appends  incoming  vectors and  passes the  resulting
    vector downwards  to the  segment below that  creates the  leaves. The
    vector is  cleared by the segment  so that the leaves  are not created
    twice. AppendSequence<S>(s1,s2) is a general purpose functor to append
    the the contents of s1 to the end of s2. s2 can be an empty sequence.

---
+++ stl-lignum/include/CreateLeaves.h
@@ -0,0 +1,71 @@
+#ifndef CREATE_LEAVES_H
+#define CREATE_LEAVES_H
+
+#include <PositionVector.h>
+using namespace cxxadt;
+
+namespace Lignum{
+  /********************************************************************
+   *Two  functors to  be uses  with AccumulateDown  to  create leaves.*
+   *CreateLeaves  created the leaves and AppendDirections  appends the*
+   *directions   of    the   buds   in    branching   points.   Usage:*******
+   *AccumulateDown(tree,vector<PositionVector>(),AppendDirections<Ts,Bud>(),*
+   *                                             CreateLeaves<Ts,Bud>())    *
+   *CreateLeaves uses LGMstatus of 1 for bud to create leaves.              *
+   **************************************************************************/
+  //From buds collect directions if they will create a leaf,
+  //if segment then create the leaves.
+  template <class TS, class BUD>
+    class CreateLeaves{
+    public:
+    vector<PositionVector>& operator()(vector<PositionVector>& v,
+				       TreeCompartment<TS,BUD>* tc)const;
+  };
+
+  template <class TS, class BUD>
+    vector<PositionVector>& CreateLeaves<TS,BUD>::operator()(vector<PositionVector>& v,
+							     TreeCompartment<TS,BUD>* tc)const
+    {
+      if (BUD* b = dynamic_cast<BUD*>(tc)){
+	if (GetValue(*b,LGMstatus) == 1){
+	  v.push_back(GetDirection(*b));
+	  //Leaf created, set status to 0 	
+	  SetValue(*b,LGMstatus,0);
+	}
+      }
+      if (TS* ts = dynamic_cast<TS*>(tc)){
+	ts->createLeaves(v);
+      }
+      return v;
+    }
+
+
+  /********************************************************************
+   *Append the contents of sequence s1  to the end of sequence s2. s2 *
+   *can an empty sequence. Return s2.                                 *
+   ********************************************************************/
+  template <class T>
+    class AppendSequence{
+    public:
+    T& operator()(T& s1, T& s2)const;
+
+  };
+  //use back_insert_iterator to add elements of s1 to the end of s2.
+  template <class T>
+    T& AppendSequence<T>::operator()(T& s1, T& s2)const
+    {
+      T::iterator first = s1.begin();
+      T::iterator last = s1.end();
+      back_insert_iterator<T> back(s2);
+
+      while (first != last)
+	*back++ = *first++;
+      
+      return s2;
+    }
+}//closing namespce
+
+
+
+
+#endif
