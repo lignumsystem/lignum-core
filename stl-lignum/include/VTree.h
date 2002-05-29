bbcc8007608e0b3c36a90ea4fd19594b708f244b
jarip <>
2002-05-29 13:29:09 +0000

    Added dummy public constructor to avoid warnings from g++.

--- stl-lignum/include/VTree.h
+++ stl-lignum/include/VTree.h
@@ -4,6 +4,12 @@
 namespace Lignum{
 
   class VTree{
+  public:
+    //Make one public dummy constructor to avoid compiler warnings.
+    VTree(){}
+  private:
+    //Dummy creates virtual function tables so that we can use dynamic
+    //casts.
     virtual void dummy(){}
   };
 
