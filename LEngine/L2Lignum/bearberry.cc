c5a0bbb3f003eac5d8d82eecb4cca661e1828d66
jarip <>
2003-01-02 11:27:39 +0000

    To test Lsystems for roots added Erythrina roots for bearberry. Added Erythrina L-file in Makefile.

--- LEngine/L2Lignum/bearberry.cc
+++ LEngine/L2Lignum/bearberry.cc
@@ -15,13 +15,19 @@ namespace Bearberry{
 #include <LSystem.h>
 }
 
+namespace Erythrina{
+#include <LSystem.h>
+}
 
 int main(int argc, char** argv)
 {
 
   Bearberry::LSystem<BearberrySegment,BearberryBud,LGMAD,LGMdouble> bearberryL;
+  Erythrina::LSystem<BearberrySegment,BearberryBud,LGMAD,LGMdouble> rootsL;
   Tree<BearberrySegment,BearberryBud> bearberry(Point(0,0,0),
-					  PositionVector(0,0,1.0));
+						PositionVector(0,0,1.0));
+  Tree<BearberrySegment,BearberryBud> bearberry_roots(Point(0,0,0),
+						      PositionVector(0,0,1.0));
 
   bearberryL.start();
   bearberryL.lstringToLignum(bearberry,1,LGMstatus);
@@ -38,6 +44,19 @@ int main(int argc, char** argv)
     		   CreateLeaves<BearberrySegment,BearberryBud>(0.5,0.5,0.5));
   }
   bearberryL.end();  
+
+  rootsL.start();
+  rootsL.lstringToRootSystem(bearberry);
+  for (int i=0; i < rootsL.derivationLength() ; i++){
+    cout << "Roots Step: " << i << endl; 
+    rootsL.rootSystemToLstring(bearberry);
+    cout << "rootSystemToLstring" << endl; 
+    rootsL.derive();
+    cout << "Roots Derived: " << endl;   
+    rootsL.lstringToRootSystem(bearberry);
+    cout << "lstringToRootSystem" << endl;  
+  }
+  rootsL.end();
 
   cout << endl;
   //Visualize LIGNUM - Graphics project needs to be fixed
