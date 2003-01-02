71881c01f66d8f1c7442a49fcf1f21233308b025
jarip <>
2003-01-02 11:06:45 +0000

    Added declarations for root compartments.

--- stl-lignum/include/LGMdecl.h
+++ stl-lignum/include/LGMdecl.h
@@ -8,20 +8,27 @@
 
 namespace Lignum{
 
-template <class TS,class BUD> class Tree;
-template <class TS,class BUD> class TreeCompartment;
-template <class TS,class BUD> class Axis;
-template <class TS,class BUD> class Bud;
-template <class TS,class BUD> class BranchingPoint;
-template <class TS,class BUD> class Bud;
-template <class TS,class BUD> class TreeSegment;
-template <class TS> class DefaultBud;
+  template <class TS,class BUD> class Tree;
+  template <class TS,class BUD> class TreeCompartment;
+  template <class TS,class BUD> class Axis;
+  template <class TS,class BUD> class Bud;
+  template <class TS,class BUD> class BranchingPoint;
+  template <class TS,class BUD> class Bud;
+  template <class TS,class BUD> class TreeSegment;
+  template <class TS> class DefaultBud;
+
+  template <class TREE> class RootCompartment;
+  template <class TREE> class RootSegment;
+  template <class TREE> class RootAxis;
+  template <class TREE> class RootBranchingPoint;
+  template <class TREE> class RootTip;
+ 
 
 #ifdef _MSC_VER
-//Windows assert for GUI 
+  //Windows assert for GUI 
 #define LGMassert(expr) ASSERT(expr)
 #else
-//Standard C-library assert
+  //Standard C-library assert
 #define LGMassert(expr) assert(expr)
 #endif
 
