58c1f32b5cb9a0aaeb1acece387e8fd32c90b620
jarip <>
2003-02-24 13:32:49 +0000

    Removed commented code. Changed attribute age back to tc_age.

--- stl-lignum/include/TreeCompartment.h
+++ stl-lignum/include/TreeCompartment.h
@@ -7,46 +7,35 @@
 #include <LGMUnits.h>
 #include <LGMSymbols.h>
 
-//#include "BendingInfo.h"
-
-
-//class Point;
-
 using namespace cxxadt;
-
-//class PositionVector;
-
-
 
 namespace Lignum{
 
-template <class TS,class BUD = DefaultBud<TS> > class Tree;
-
-template <class TS,class BUD=DefaultBud<TS> > 
-class TreeCompartment
-{
+  template <class TS,class BUD=DefaultBud<TS> > 
+    class TreeCompartment
+    {
 #ifdef _MSC_VER
 
-	  friend Point GetPoint(const TreeCompartment<TS,BUD>& tc);
+      friend Point GetPoint(const TreeCompartment<TS,BUD>& tc);
       friend PositionVector GetDirection(const TreeCompartment<TS,BUD>& tc);
-	  friend void SetPoint(TreeCompartment<TS,BUD>& tc, Point& p);
-	  friend void SetDirection(TreeCompartment<TS,BUD>& tc, PositionVector& pv);
+      friend void SetPoint(TreeCompartment<TS,BUD>& tc, Point& p);
+      friend void SetDirection(TreeCompartment<TS,BUD>& tc, PositionVector& pv);
       friend Tree<TS,BUD>& GetTree(const TreeCompartment<TS,BUD>& tc);
       friend LGMdouble GetValue(const TreeCompartment<TS,BUD>& tc, const LGMAD name);
       friend LGMdouble SetValue(TreeCompartment<TS,BUD>& tc, const LGMAD name, const LGMdouble value);
 
 #else
-	  template <class TS1,class BUD1> 
+      template <class TS1,class BUD1> 
       friend Point GetPoint(const TreeCompartment<TS1,BUD1>& tc);
 
       template <class TS1,class BUD1> 
       friend PositionVector GetDirection(const TreeCompartment<TS1,BUD1>& tc);
 
-	  template <class TS1,class BUD1> 
-	  friend void SetPoint(TreeCompartment<TS,BUD>& tc, Point& p);
+      template <class TS1,class BUD1> 
+      friend void SetPoint(TreeCompartment<TS,BUD>& tc, Point& p);
 	  
-	  template <class TS1,class BUD1> 
-	  friend void SetDirection(TreeCompartment<TS,BUD>& tc, PositionVector& pv);
+      template <class TS1,class BUD1> 
+      friend void SetDirection(TreeCompartment<TS,BUD>& tc, PositionVector& pv);
 
       template <class TS1,class BUD1> 
       friend Tree<TS1,BUD1>& GetTree(const TreeCompartment<TS1,BUD1>& tc);
@@ -58,18 +47,18 @@ class TreeCompartment
       friend LGMdouble SetValue(TreeCompartment<TS1,BUD1>& tc, const LGMAD name, const LGMdouble value);
 #endif // _MSC_VER
     
-public:
+      public:
       TreeCompartment();
       TreeCompartment(const Point& p, const PositionVector& d, Tree<TS,BUD>* t);
       virtual ~TreeCompartment();
       virtual void photosynthesis() { /* Default: do nothing */ }
-	  virtual void respiration() {  }
-protected:
+      virtual void respiration() {  }
+      protected:
       Point point;
       PositionVector direction;
       LGMdouble tc_age;
       Tree<TS,BUD>* tree;
-};
+    };
 
 
 
