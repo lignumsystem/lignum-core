f506a0f349e93f6a08662236b6a5a5526d5b4e3e
jarip <>
2003-03-06 07:40:11 +0000

    Moved AppendSequence to Seqence.h file in c++adt.

--- stl-lignum/include/CreateLeaves.h
+++ stl-lignum/include/CreateLeaves.h
@@ -2,6 +2,8 @@
 #define CREATE_LEAVES_H
 
 #include <PositionVector.h>
+#include <Sequence.h>
+
 using namespace cxxadt;
 
 namespace Lignum{
@@ -73,29 +75,7 @@ namespace Lignum{
     }
 
 
-  /********************************************************************
-   *Append the contents of sequence s1  to the end of sequence s2. s2 *
-   *can an empty sequence. Return s2.                                 *
-   ********************************************************************/
-  template <class T>
-    class AppendSequence{
-    public:
-    T& operator()(T& s1, T& s2)const;
 
-  };
-  //use back_insert_iterator to add elements of s1 to the end of s2.
-  template <class T>
-    T& AppendSequence<T>::operator()(T& s1, T& s2)const
-    {
-      typename T::iterator first = s1.begin();
-      typename T::iterator last = s1.end();
-      back_insert_iterator<T> back(s2);
-
-      while (first != last)
-	*back++ = *first++;
-      
-      return s2;
-    }
 }//closing namespce
 
 
