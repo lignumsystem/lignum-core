652dc19905551ac0a8d3ab76ec6bd73044f8f7a3
jarip <>
2003-03-21 15:00:26 +0000

    Comment layout improved.

--- c++adt/include/Triangle.h
+++ c++adt/include/Triangle.h
@@ -18,30 +18,38 @@ namespace cxxadt{
   public:
     Triangle(const Point& leftcorner, const Point& rightcorner, 
 	     const Point& apexcorner);
-    Point getLeftCorner()  { return leftcorner;};
-    Point getRightCorner() { return rightcorner;};
-    Point getApexCorner()  { return apexcorner;};
+    Point  getLeftCorner()  { return leftcorner;};
+    Point  getRightCorner() { return rightcorner;};
+    Point  getApexCorner()  { return apexcorner;};
     vector<Point>&  getTriangleCorners(vector<Point>& corners);
-    void  setLeftCorner(const Point& p){ leftcorner=p;};
-    void  setRightCorner(const Point& p){ rightcorner=p;};
-    void  setApexCorner(const Point& p){ apexcorner=p;};
-    double getArea();               //the triangle area calculation
-    Point  getCentroidPoint();     //getting the triangle centroid 
+    void   setLeftCorner(const Point& p){ leftcorner=p;};
+    void   setRightCorner(const Point& p){ rightcorner=p;};
+    void   setApexCorner(const Point& p){ apexcorner=p;};
+    double getArea()const;               //the triangle area calculation
+    Point  getCenterPoint();     //getting the triangle center 
+
+                                 //set up the triangle center point
+                                 //end change the triangle corners
+                                 //according the center point 
+    void   setCenterPoint(const Point& center);
   
     double setArea(double area);   //it defines a new triangle from
                                    // the old one
                                    //using the scaling method with the 
                                    //triangle centroid as the scaling center
 
-    double setArea(double area,       //it defines a new triangle from 
-                                      //  the old one 
-		   const Point& base );// with the base point as the center
+                                   //it defines a new triangle from 
+                                   //  the old one 
+                                   // with the base point as the center
+    double setArea(double area, const Point& base );
 
-    bool intersectTriangle(const Point& o,//checking the triangle intersection 
-                                          // with "a light beam"  
-			   const Point& b);//the light beam is the ob vector 
-                                           // o - the observer position
-                                           // b - the vector beam end
+                                   //checking the triangle intersection 
+                                   // with "a light beam" 
+                                   //the light beam is the ob vector 
+                                   // o - the observer position
+                                   // b - the vector beam end
+    bool   intersectTriangle(const Point& o,const Point& b); 
+                                            
   private:
     Point leftcorner,rightcorner,apexcorner;//the triangle corners
   };
