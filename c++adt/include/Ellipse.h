db6368ff23ade57073801f3cec9c64ec0c4be971
jarip <>
2003-03-21 14:59:17 +0000

    Minor improvements in comments. setArea now inline.

--- c++adt/include/Ellipse.h
+++ c++adt/include/Ellipse.h
@@ -33,12 +33,12 @@ namespace cxxadt{
     void   setSemiminorAxis( const double& semiminoraxis0)
                  {semiminoraxis=semiminoraxis0;} ;
                                        //the ellipse area calculation
-    double getArea(){return semimajoraxis*semiminoraxis*PI_VALUE;}; 
+    double getArea()const{return semimajoraxis*semiminoraxis*PI_VALUE;}; 
 
-    double setArea(double area);   //it defines a new ellipse from
-                                   // the old one
-                                   //using the scaling method with the 
-                                   //ellipse center point as the scaling center
+    inline double setArea(double area);   //it defines a new ellipse from
+                                          // the old one
+                                          //using the scaling method with the 
+                                          //ellipse center point as the scaling center
 
     double setArea(double area,       //it defines a new triangle from 
                                       //  the old one 
@@ -64,6 +64,44 @@ namespace cxxadt{
     double semiminoraxis;
   };
 
+  //This case  with the ellipse center as the scaling center 
+  inline double Ellipse::setArea(double area){
+
+   double areaold, areanew,
+         scalcoef; 
+   double adbasex, adbasey, adbasez;
+  
+   areaold=getArea();
+   areanew=area; 
+
+   scalcoef=sqrt(areanew/areaold);
+
+   adbasex=getCenterPoint().getX()*(1-scalcoef);
+   adbasey=getCenterPoint().getY()*(1-scalcoef);
+   adbasez=getCenterPoint().getZ()*(1-scalcoef);
+
+   vector<Point> points;
+   points.push_back(getSemimajorAxisPoint());
+   points.push_back(getSemiminorAxisPoint());
+   Point x,p;
+
+   for(int i=0; i< points.size(); i++){
+    p=(Point)points[i];
+    x.setX(p.getX() *scalcoef  + adbasex);
+    x.setY(p.getY() *scalcoef  + adbasey);
+    x.setZ(p.getZ() *scalcoef  + adbasez);
+    switch(i){
+     case 0:
+      setSemimajorAxis(getCenterPoint() || x);
+      break;
+     case 1:
+      setSemiminorAxis(getCenterPoint() || x);
+      break;
+    };
+   };
+
+   return getArea();
+  }
 
 }//closing cxxadt
 #endif
