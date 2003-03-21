04355d28ee39365215ff64b100f98989250b0c3b
jarip <>
2003-03-21 14:57:39 +0000

    Added getCenterPoint (which in fact simply calls getFocus).

--- c++adt/include/Ellipsis.h
+++ c++adt/include/Ellipsis.h
@@ -14,12 +14,13 @@ public:
   Ellipsis(double a, double b);
   double getArea()const{return PI_VALUE*a*b;}
   double getFocus()const;
+  double getCenterPoint()const{return getFocus();}
   double getSemimajorAxis()const{return a;}
   double getSemiminorAxis()const{return b;}
   double setArea(double area);
   double setMajor(double a);
   double setMinor(double b);
-private:
+ private:
   double a;  //Semimajor axis
   double b;  //Semiminor axis
   double b_div_a; //stores the shape of the ellipsis even if area = 0
