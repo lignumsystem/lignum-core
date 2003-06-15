437a784d3e718717a6aff2146920d5f19c00002e
jarip <>
2003-06-15 15:35:38 +0000

    Added functor TreeDiameterGrowth that can be used in diameter growth. For example: 	CfData cfdata;         AccumulateDown(pine1,cfdata,TreeDiameterGrowth<PineSegment,PineBud>());

--- stl-lignum/include/TreeMetabolism.h
+++ stl-lignum/include/TreeMetabolism.h
@@ -6,53 +6,72 @@
 
 namespace Lignum{
 
-template <class TS,class BUD>
-class TreePhotosynthesis
-{
-public:
-  TreeCompartment<TS,BUD>* operator()(TreeCompartment<TS,BUD>* tc)const{
-    tc->photosynthesis();
-    return tc;
-  }
-};
-
-template <class TS, class BUD>
-class SumTreePhotosynthesis{
-public:
-  LGMdouble operator () (LGMdouble& cumPh, TreeCompartment<TS,BUD>* tc)const
+  template <class TS,class BUD>
+  class TreePhotosynthesis
   {
-    if (TS* tts = dynamic_cast<TS*>(tc)){
-      LGMdouble PP = GetValue(*tts, P);
-      cumPh += PP;
+  public:
+    TreeCompartment<TS,BUD>* operator()(TreeCompartment<TS,BUD>* tc)const{
+      tc->photosynthesis();
+      return tc;
     }
-    return cumPh;
-  }
-};
+  };
+
+  template <class TS, class BUD>
+  class SumTreePhotosynthesis{
+  public:
+    LGMdouble operator () (LGMdouble& cumPh, TreeCompartment<TS,BUD>* tc)const
+    {
+      if (TS* tts = dynamic_cast<TS*>(tc)){
+	LGMdouble PP = GetValue(*tts, P);
+	cumPh += PP;
+      }
+      return cumPh;
+    }
+  };
 
 
-template <class TS,class BUD>
+  template <class TS,class BUD>
   class TreeRespiration{
   public:
-  TreeCompartment<TS,BUD>* operator()(TreeCompartment<TS,BUD>* tc)const{
-    tc->respiration();
-    return tc;
-  }
-};
-
-template <class TS, class BUD>
-class SumTreeRespiration{
-public:
-  LGMdouble operator () (LGMdouble& cumM, TreeCompartment<TS,BUD>* tc)const
-  {
-    if (TS* tts = dynamic_cast<TS*>(tc))
-	{
-      LGMdouble M_ts = GetValue(*tts, M);
-      cumM += M_ts;
+    TreeCompartment<TS,BUD>* operator()(TreeCompartment<TS,BUD>* tc)const{
+      tc->respiration();
+      return tc;
     }
-    return cumM;
-  }
-};
+  };
 
+  template <class TS, class BUD>
+  class SumTreeRespiration{
+  public:
+    LGMdouble operator () (LGMdouble& cumM, TreeCompartment<TS,BUD>* tc)const
+    {
+      if (TS* tts = dynamic_cast<TS*>(tc))
+	{
+	  LGMdouble M_ts = GetValue(*tts, M);
+	  cumM += M_ts;
+	}
+      return cumM;
+    }
+  };
+
+  template <class TS, class BUD>
+  class TreeAging{
+  public:
+    TreeCompartment<TS,BUD>* operator()(TreeCompartment<TS,BUD>* tc)const
+    {
+      tc->aging();
+      return tc;
+    }
+  };
+
+  template <class TS, class BUD>
+  class TreeDiameterGrowth{
+  public:
+     TcData& operator()(TcData& data,TreeCompartment<TS,BUD>* tc)const
+    {
+      data = tc->diameterGrowth(data);
+      return data;
+    }
+  };
 }
 
 #endif
