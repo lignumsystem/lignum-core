53fca8c44f33d624372a2fff5bd1ad30f3df3ef3
jarip <>
2003-07-02 11:38:56 +0000

    BudAttributes constructor:  state of the  bud is ALIVE now  by default (i.e.   state(ALIVE)).    It  seems  to   be  a  bit   dangerous  with enumerations.    Previously   state  was   state   by  default   (i.e. state(state))! Each enumaration, though  integers, is its own type and misuse should generate a compiler warning, but not in CC in SGI.

--- stl-lignum/Bud/Bud.cc
+++ stl-lignum/Bud/Bud.cc
@@ -7,13 +7,13 @@ namespace Lignum{
 
 //cvs: korjaa!
 BudAttributes::BudAttributes()
-  :ip(0.0),omega(0.0)
+  :ip(0.0),omega(0.0),state(ALIVE)
 {
 }
 
 #else
 BudAttributes::BudAttributes()
-  :collision(0.0),ip(0.0),omega(0.0),type(0.0),status(0.0),state(state)
+  :collision(0.0),ip(0.0),omega(0.0),type(0.0),status(0.0),state(ALIVE)
 {
 }
 
