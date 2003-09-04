0cd932cebf6e325f45590be809e5804b37ed9889
jarip <>
2003-09-04 09:33:51 +0000

    Program used in Maija's poster.

--- LEngine/L2Lignum/a-uvaursi.cc
+++ LEngine/L2Lignum/a-uvaursi.cc
@@ -21,21 +21,33 @@ namespace Bearberry{
 
 int main(int argc, char** argv)
 {
+  if (argc != 7){
+    cout << "Usage: auvaursi iter angle distance p1 p2 p3" << endl;
+  }
+  int iter = atoi(argv[1]);
+  double angle =  atof(argv[2]);
+  double distance =  atof(argv[3]);
+  double nddie = atof(argv[4]);//nondominant
+  double sddie = atof(argv[5]);//subdominant
+  double ddie = atof(argv[6]); //dominant
 
   Bearberry::LSystem<BearberrySegment,BearberryBud,LGMAD,LGMdouble> bearberryL;
   Tree<BearberrySegment,BearberryBud> bearberry(Point(0,0,0),
 						PositionVector(0,0,1.0));
-  KillBud<BearberrySegment,BearberryBud> kill_bud(0.11);
+  KillBud<BearberrySegment,BearberryBud> kill_bud(nddie,sddie,ddie);
   Aging<BearberrySegment,BearberryBud> aging;
   ShortenSegment<BearberrySegment,BearberryBud> shorten_segment;
-  LGMdouble coll = 0.0;
-
+  
+  cout << iter << " iterations " << angle << " degrees " 
+       << distance << " meters " 
+       << nddie << " p1 " << sddie << " p2 " << ddie << " p3 " << endl;
+  
   bearberryL.start();
   bearberryL.lstringToLignum(bearberry,3,LGMtype,LGMstatus,LGMcollision);
-  for (int i=0; i < bearberryL.derivationLength() ; i++)
+  for (int i=0; i < iter ; i++)
   {
     cout << "Step: " << i << endl;    
-    ForEach(bearberry,EvaluateCollisionForAllBuds<BearberrySegment,BearberryBud>(55.0*2.0*PI_VALUE/360.0,0.30));
+    ForEach(bearberry,EvaluateCollisionForAllBuds<BearberrySegment,BearberryBud>(angle*PI_VALUE/180.0,distance));
     bearberryL.lignumToLstring(bearberry,3,LGMtype,LGMstatus,LGMcollision);  
     bearberryL.derive();
     bearberryL.lstringToLignum(bearberry,3,LGMtype,LGMstatus,LGMcollision);
@@ -43,15 +55,16 @@ int main(int argc, char** argv)
     //ForEach(bearberry,kill_bud);
   }
   bearberryL.end();  
-  
-  //AccumulateDown(bearberry,coll,shorten_segment);
-  //bearberryL.lignumToLstring(bearberry,3,LGMtype,LGMstatus,LGMcollision);
-  //bearberryL.lstringToLignum(bearberry,3,LGMtype,LGMstatus,LGMcollision);
 
-  vector<PositionVector> v;
-  AccumulateDown(bearberry,v,
-		 AppendSequence<vector<PositionVector> >(),
-		 CreateLeaves<BearberrySegment,BearberryBud, Ellipse>(0.1,0.1,0.1));
+  LGMdouble coll = 0.0;  
+  AccumulateDown(bearberry,coll,shorten_segment);
+  bearberryL.lignumToLstring(bearberry,3,LGMtype,LGMstatus,LGMcollision);
+  bearberryL.lstringToLignum(bearberry,3,LGMtype,LGMstatus,LGMcollision);
+
+//   vector<PositionVector> v;
+//   AccumulateDown(bearberry,v,
+// 		 AppendSequence<vector<PositionVector> >(),
+// 		 CreateLeaves<BearberrySegment,BearberryBud, Ellipse>(0.1,0.1,0.1));
   cout << endl;
   //Visualize LIGNUM - Graphics project needs to be fixed
   VisualizeHwTree<BearberrySegment,BearberryBud,Ellipse>(bearberry);
