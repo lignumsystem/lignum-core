02fa866376195b17e2c742909b68a8019e2117d2
jarip <>
2004-01-30 22:44:03 +0000

    Subtile  variable name clash  removed: For  the shape  of the  leaf, E shape(petiole_end,leaf_normal,a,b), the  'b' was used as  bud too (see if statement for  BUD). Prevented the file from  compiling.  Changed b for bud to 'bud'.

--- stl-lignum/include/CreateLeaves.h
+++ stl-lignum/include/CreateLeaves.h
@@ -22,66 +22,66 @@ namespace Lignum{
   template <class TS, class BUD, class E>
     class CreateLeaves{
     public:
-    CreateLeaves(METER l, METER semi_major, METER semi_minor):pl(l),a(semi_major),b(semi_minor){}
-    vector<PositionVector>& operator()(vector<PositionVector>& v,
-				       TreeCompartment<TS,BUD>* tc)const;
-    METER pl;//Petiole length
-    METER a; //semi major axis of leaf ellips
-    METER b; //semi minor axis od leaf ellips
-  };
-
+      CreateLeaves(METER l, METER semi_major, METER semi_minor)
+	:pl(l),a(semi_major),b(semi_minor){}
+      vector<PositionVector>& operator()(vector<PositionVector>& v,
+					 TreeCompartment<TS,BUD>* tc)const;
+      METER pl;//Petiole length
+      METER a; //semi major axis of leaf ellips
+      METER b; //semi minor axis od leaf ellips
+    };
+  
   template <class TS, class BUD, class E>
-    vector<PositionVector>& CreateLeaves<TS,BUD,E>::operator()(vector<PositionVector>& pdv,
-							       TreeCompartment<TS,BUD>* tc)const
-    {
-      if (BUD* b = dynamic_cast<BUD*>(tc)){
-	if (GetValue(*b,LGAstatus) == 1){
-	  //Leaf to be created
-	  pdv.push_back(GetDirection(*b));
-	  //Leaf created, set status to 0 	
-	  SetValue(*b,LGAstatus,0);
-	}
+  vector<PositionVector>& CreateLeaves<TS,BUD,E>::operator()(vector<PositionVector>& pdv,
+							     TreeCompartment<TS,BUD>* tc)const
+  {
+    if (BUD* bud = dynamic_cast<BUD*>(tc)){
+      if (GetValue(*bud,LGAstatus) == 1){
+	//Leaf to be created
+	pdv.push_back(GetDirection(*bud));
+	//Leaf created, set status to 0 	
+	SetValue(*bud,LGAstatus,0);
       }
-      if (TS* ts = dynamic_cast<TS*>(tc)){
-	Point origo(0,0,0);
-	Point point = GetEndPoint(*ts);
-	PositionVector up(0,0,1);
-	
-	static Uniform u; //uniform random number [0,1] for setting leaf
-	                  //normals;  static makes it  common throughout
-	                  //the  program and  not reinitialized  in each
-           	          //call.
-	int seed = 3267;
-	for (unsigned int i = 0; i <pdv.size(); i++){
-	  PositionVector pdir = pdv[i];
-	  //Leaves are  created at the end  of the segment  where the buds
-	  //are, second argument is the intial length of the petiole
-	  Petiole petiole(point,point + pl*(Point)pdir);
-	  //Randomize  the leaf blade  normal by  rotating in  around axis
-	  //that lies in the horizontal plane defined by cross product of
-	  //petiole direction and up-vector
-	  PositionVector plane = Cross(pdir,up);
-	  //limit the rotation  of the leaf normal to  [-90,90] degrees so
-	  //that the leaf normal does not point downwards
-	  double ran = (-90.0 +180.0*u(seed))*2.0*PI_VALUE/360.0; //(radians)
-	  PositionVector leaf_normal(0,0,1);
-	  leaf_normal.rotate(origo,plane,ran);
-	  E shape((PositionVector)GetEndPoint(petiole),
-		  leaf_normal,a,b); //initial shape of a leaf is Ellips
-	  BroadLeaf<E>* leaf = new BroadLeaf<E>(shape,petiole,leaf_normal,
-						GetFirmament(GetTree(*tc)).numberOfRegions());
-	  //Set parameters for radiation calculation.
-	  SetValue(*leaf,LGAdof,GetValue(GetTree(*tc),LGPdof));
-	  SetValue(*leaf,LGAtauL,GetValue(GetTree(*tc),LGPtauL));
-	  SetValue(*leaf,LGAsf,GetValue(GetTree(*tc),LGPsf));
-	  //Insert leaf
-	  ts->addLeaf(leaf);
-	}
-	//clear the vector; don't create leaves twice
-	pdv.clear();
-      }
-      return pdv;
     }
+    else if (TS* ts = dynamic_cast<TS*>(tc)){
+      Point origo(0,0,0);
+      Point point = GetEndPoint(*ts);
+      PositionVector up(0,0,1);	
+      static Uniform u; //uniform random number [0,1] for setting leaf
+			//normals;  static makes it  common throughout
+			//the  program and  not reinitialized  in each
+			//call.
+      int seed = 3267;
+      for (unsigned int i = 0; i <pdv.size(); i++){
+	PositionVector pdir = pdv[i];
+	//Leaves are  created at the end  of the segment  where the buds
+	//are, second argument is the intial length of the petiole
+	Petiole petiole(point,point + pl*(Point)pdir);
+	//Randomize  the leaf blade  normal by  rotating in  around axis
+	//that lies in the horizontal plane defined by cross product of
+	//petiole direction and up-vector
+	PositionVector plane = Cross(pdir,up);
+	//limit the rotation  of the leaf normal to  [-90,90] degrees so
+	//that the leaf normal does not point downwards
+	double ran = (-90.0 +180.0*u(seed))*2.0*PI_VALUE/360.0; //(radians)
+	PositionVector leaf_normal(0,0,1);
+	leaf_normal.rotate(origo,plane,ran);
+	const PositionVector petiole_end = static_cast<const PositionVector>(GetEndPoint(petiole));
+	E shape(petiole_end,leaf_normal,a,b); //shape of a leaf is Ellips
+	BroadLeaf<E>* leaf = new BroadLeaf<E>(shape,petiole,leaf_normal,
+					      GetFirmament(GetTree(*tc)).numberOfRegions());
+	//Set parameters for radiation calculation.
+	SetValue(*leaf,LGAdof,GetValue(GetTree(*tc),LGPdof));
+	SetValue(*leaf,LGAtauL,GetValue(GetTree(*tc),LGPtauL));
+	SetValue(*leaf,LGAsf,GetValue(GetTree(*tc),LGPsf));
+	//Insert leaf
+	ts->addLeaf(leaf);
+      }
+      //clear the vector; don't create leaves twice
+      pdv.clear();
+    }
+    return pdv;
+  }
 
   /********************************************************************
    *Create triangle  leaves in the  same way as ellipse  shaped leaves
