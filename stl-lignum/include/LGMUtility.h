5b1223f07965885470df21e7d171977dc4a50464
jarip <>
2004-02-04 21:02:43 +0000

    Added generic TreeGrowthAllocator

--- stl-lignum/include/LGMUtility.h
+++ stl-lignum/include/LGMUtility.h
@@ -9,13 +9,29 @@
 /* #include <BroadLeaf.h> */
 /* #include <Algorithms.h> */
 
+#include <TreeGrowthAllocator.h>
+
 //Declarations (interface) of a number useful functions for
 //STL-LIGNUM. Please keep the list of available utilities up to date.
 
-// 1. CountLeaves        Count the number of leaves in a tree
+// 1. TreeGrowthAllocator Allocate net production so that P-M = G. 
+// 2. CountLeaves         Count the number of leaves in a tree.
 
 
 namespace Lignum{
+  //Growth Allocator for Cf and Hw tree.
+  //Usage: TreeGrowthAllocator<TS,BUD,F1,F2) G(tree); 
+  //       Bisection(0,5,G);
+  //Output:  After  the iteration,  the  segment  lengths  in the  new
+  //segments allocate  P-M=G (including the induced  diameter and root
+  //growth).  F1:  set the segment lengths with  lamda.  F2: calculate
+  //the effect of diameter growth  induced by the new segments and the
+  //foliage.   Root growth  is assumed  to be  propotional to  the new
+  //foliage mass:  Wrnew = LGPar*Wfnew. Instead of  Bisection, one can
+  //have any root finding algorithm (to be) implemented in c++adt.
+  template<class TS,class BUD,class F1,class F2> 
+  class TreeGrowthAllocator;
+
 
   //Count the number of leaves in a tree
 
@@ -27,10 +43,9 @@ namespace Lignum{
 
 
 }//closing namespace Lignum
-#include <LGMUtilityI.h>
-
 #endif
 
+#include <LGMUtilityI.h>
 
 
 
