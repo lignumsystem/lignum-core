fead1e2a9fa62f7aa2aea5d3fce30ab712b6c2ca
jarip <>
2004-03-03 17:22:26 +0000

    Added decalarations for prune methods.

--- LEngine/include/LSystem.h
+++ LEngine/include/LSystem.h
@@ -36,6 +36,29 @@ class LSystem{
  int lstringToLignum(Tree<TS,BUD>& t,int argnum = 0,...);
  int rootSystemToLstring(Tree<TS,BUD>& t,int argnum = 0,...);
  int lstringToRootSystem(Tree<TS,BUD>& t,int argnum = 0,...);
+  int prune(Tree<TS,BUD>& t,int argnum = 0,...);
+  template <class P,class I> 
+  int prune(Tree<TS,BUD>& t,P p,I& init,int argnum = 0,...){
+    va_list ap;
+    vector<N> vav;
+
+    //The C library standard requires  that in call to va_arg the second
+    //(last)  type parameter  must *not*  be array,  function,  float or
+    //integer type that changes  when promoted. Unfortunately this means
+    //that the  enumeration N that will  denote the set of  names of the
+    //variables of type F cannot be  used. But we pass int to va_arg and
+    //type cast  the return value back  to N. Some  compilers accept (as
+    //SGI CC) N as such but it should be an error.
+    va_start(ap,argnum);
+    for (int i = 0; i < argnum; i++){
+      vav.push_back(static_cast<N>(va_arg(ap,int)));
+    }
+    va_end(ap);
+
+    //Prune the tree
+    AccumulateDown(t,init,p);
+    return prune(t,vav);
+  }
  protected:
  //above ground part
  int lignum2Lstring(Tree<TS,BUD>& t, vector<N>& vav);
@@ -55,6 +78,13 @@ class LSystem{
 		    typename list<Axis<TS,BUD>*>::iterator current, 
 		    Tree<TS,BUD>& tree, LstringIterator& ltr, 
 		    stack<Turtle>& turtle_stack, vector<N>& vav);
+  int prune(Tree<TS,BUD>& t, vector<N>& vav);
+  int prune(list<Axis<TS,BUD>*>& ls, 
+	    typename list<Axis<TS,BUD>*>::iterator current, 
+	    LstringIterator& ltr, vector<N>& vav);
+  int prune(list<TreeCompartment<TS,BUD>*>& ls,
+	    typename list<TreeCompartment<TS,BUD>*>::iterator current,
+	    LstringIterator& ltr, vector<N>& vav);
  //root system part
  int rootSystem2Lstring(Tree<TS,BUD>& t, vector<N>& vav);
  int rootSystem2Lstring(list<RootAxis<Tree<TS,BUD> >*>& ls, 
@@ -155,5 +185,6 @@ inline void LSystem<TS,BUD,N,F>::print(bool arg)
 
 //Implementation of the conversion for the above ground part
 #include <LSystemI.h>
+#include <TreePruneI.h>
 //Implementation of the conversion for the root system
 #include <LSystemForRootsI.h>
