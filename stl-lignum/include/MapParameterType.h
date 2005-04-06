0279590403928d60942a2d8ab5ddf4ac608cd4ff
jarip <>
2005-04-06 17:45:48 +0000

    I have  problems to  compile InitializeTree with  Sun CC on  corona at CSC.  Some reorganization required.  This files has the implementation of MapParameterType (mapping string "LGP*" with LGP* enumeration name)

---
+++ stl-lignum/include/MapParameterType.h
@@ -0,0 +1,48 @@
+#ifndef MAP_PARAMETERTYPE_H
+#define MAP_PARAMETERTYPE_H
+
+#include <map>
+using namespace std;
+#include <stringutils.h>
+using namespace cxxadt;
+
+namespace Lignum{
+  //MapParameterType maps name of the parameter to its enumeration.
+  //The class can be instantiated with different enumerations 
+  //found in LGMSymbols.h (for template T).
+  //The constructor must be a specialization enlisting the mappings.
+  //The current specializations are in TreeFriend.cc. Please remember to update
+  //current ones if new parameters emerge. 
+  template <class T>
+  class MapParameterType{
+  public:
+    MapParameterType();
+    typename map<string,T,Cmpstring>::iterator begin();
+    typename map<string,T,Cmpstring>::iterator end();
+    typename map<string,T,Cmpstring>::iterator find(const string& l);
+  private:
+    map<string,T,Cmpstring> lgmpd;
+  };
+  template <class T>
+  typename map<string,T,Cmpstring>::iterator MapParameterType<T>::begin()
+    {
+      return lgmpd.begin();
+    }
+
+  template <class T>
+  typename map<string,T,Cmpstring>::iterator MapParameterType<T>::end()
+    {
+      return lgmpd.end();
+    }
+  
+  template <class T>
+  typename map<string,T,Cmpstring>::iterator MapParameterType<T>::find(const string& l)
+    {
+      return lgmpd.find(l);
+    }
+
+
+  
+}
+
+#endif
