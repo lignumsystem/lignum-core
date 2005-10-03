c1e711a787a3c2ebc4384652f1c93f34bde4af12
jarip <>
2005-10-03 06:25:40 +0000

    OpenGLHFiles.h is meant to capture the vendor independent inclusion of OpenGL and  GLUT header  files, currently gl.h,  glu.h and  glut.h. If anything else is needed later, put it here.

---
+++ Graphics/OpenGLHFiles.h
@@ -0,0 +1,17 @@
+#ifndef OPENGLHFILES_H
+#define OPENGLHFILES_H
+//Mac OS X install OpenGL  and GLUT frameworks differently, the latter
+//should work for all others. If anything else is needed, this file is
+//intended to capture the inclusions of OpenGL and GLUT headers in one
+//place.
+#if defined(__APPLE__) || defined(__MACOSX__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
+#endif
