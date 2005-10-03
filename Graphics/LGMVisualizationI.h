5c04006d4cc7bcc24e9fad5843a9ac7235db4b2b
jarip <>
2005-10-03 11:17:26 +0000

    AddHwTree(): Checking if the texture files exist before using them.

--- Graphics/LGMVisualizationI.h
+++ Graphics/LGMVisualizationI.h
@@ -11,43 +11,51 @@ namespace Lignum{
 
 
   template <class TS, class BUD, class SHAPE>
- void LGMVisualization::AddHwTree(Tree<TS,BUD>& t, string stemTex, string folTex)
-{
-  HwWrapper<TS,BUD,SHAPE> *wb = new HwWrapper<TS,BUD,SHAPE>(t);
-  //Keep track of the tallest tree.
-  max_height = max(max_height,GetValue(t,LGAH));
+  void LGMVisualization::AddHwTree(Tree<TS,BUD>& t, string stemTex, string folTex)
+  {
+    HwWrapper<TS,BUD,SHAPE> *wb = new HwWrapper<TS,BUD,SHAPE>(t);
+    //Keep track of the tallest tree.
+    max_height = max(max_height,GetValue(t,LGAH));
 
-   
-    CTexture stem_texture;
-    stem_texture.Load(stemTex.c_str(), 512, 512);
-    stem_texture.use();
+    //Check if the texture files exist
+    ifstream f1(stemTex.c_str());
+    if (!f1){
+      LGMMessage("Stem texture file missing");
+    }
+    ifstream f2(folTex.c_str());
+    if (!f2){
+      LGMMessage("Foliage texture file missing");
+    }
 
-    wb->intStemTexture = stem_texture.texturenum;
- 
-    bool bmpImage = false;
-    
-    if (folTex.find("bmp")< folTex.size() || folTex.find("BMP")< folTex.size())
-    {
-	bmpImage = true;	
-    }  
-    
-    if (bmpImage)
-    {
-	CTexture fol_texture;
-	fol_texture.Load(folTex.c_str(), 512, 512);
-	fol_texture.use();
-	wb->intFoliageTexture = fol_texture.texturenum;
+    if (f1){
+      CTexture stem_texture;
+      stem_texture.Load(stemTex.c_str(), 512, 512);
+      stem_texture.use();
+      wb->intStemTexture = stem_texture.texturenum;
     }
-    else
-    {
-	//Tätä käytetään jos halutaa visualisoida lehdet läpinäkyvyyden avulla.
-	// Tiedosto-muoto .tga
-	wb->intFoliageTexture = GetTgaTexNumber(folTex);
-    }
-    
-    wb->bmpImage = bmpImage;
+    if (f2){
+      bool bmpImage = false;
+      if (folTex.find("bmp")< folTex.size() || folTex.find("BMP")< folTex.size())
+	{
+	  bmpImage = true;	
+	}  
+      if (bmpImage)
+	{
+	  CTexture fol_texture;
+	  fol_texture.Load(folTex.c_str(), 512, 512);
+	  fol_texture.use();
+	  wb->intFoliageTexture = fol_texture.texturenum;
+	}
+      else
+	{
+	  //Tätä käytetään jos halutaa visualisoida lehdet läpinäkyvyyden avulla.
+	  // Tiedosto-muoto .tga
+	  wb->intFoliageTexture = GetTgaTexNumber(folTex);	
+	}
+      wb->bmpImage = bmpImage;
+     }
     trees.push_back(wb);
-}
+  }
 
   
  template <class TS, class BUD>
