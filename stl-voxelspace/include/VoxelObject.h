134d9cb548147d4c56478460f4ffb6c7c0ec0922
jarip <>
2006-01-22 13:02:01 +0000

    Added data member Point p2 to CfCylinder that is simple the possible start point of the light beam (computed now once in the constructor). When the foliage is gone LGARf == LGAR, i.e. LGAVf == 0. CylinderBeamShading however returns as if a foliage hit. Checking LGAVf and interpreting the hit as wood hit.

--- stl-voxelspace/include/VoxelObject.h
+++ stl-voxelspace/include/VoxelObject.h
@@ -6,10 +6,12 @@
 //Wrapper class for different photosynthesising elements a voxel box
 class VoxelObject{
 public: 
+  VoxelObject():hit_self(false){}
   virtual int getRoute(const Point& p, const PositionVector& dir,
 		       LGMdouble& length)const=0;
   virtual LGMdouble getExtinction(const Point& p, const PositionVector& dir,
 				  const ParametricCurve& K)const=0;
+  mutable bool hit_self;//debug: true if segment compares itself 
 };
 
 
@@ -17,19 +19,25 @@ public:
 class CfCylinder:public VoxelObject{
 public:
   CfCylinder(const CfCylinder& cfobj)
-    :p0(cfobj.p0),d(cfobj.d),l(cfobj.l),rw(cfobj.rw),rf(cfobj.rf),
+    :p0(cfobj.p0),p2(cfobj.p2),d(cfobj.d),l(cfobj.l),rw(cfobj.rw),rf(cfobj.rf),
      af(cfobj.af),vf(cfobj.vf),sp(cfobj.sp){}
   CfCylinder(const Point&p, const PositionVector& dir,double length, double rwood, 
 	     double rfol, double folarea, double folvol, double beam_start)
-    :p0(p),d(dir),l(length),rw(rwood),rf(rfol),af(folarea),vf(folvol),sp(beam_start){}
+    :p0(p),d(dir),l(length),rw(rwood),rf(rfol),af(folarea),vf(folvol),sp(beam_start)
+  {
+    PositionVector tmp = PositionVector(p0)+sp*l*d;//Possibly the start point of the light beam    
+    p2 = Point(tmp);
+  }
   virtual int getRoute(const Point& p, const PositionVector& dir,
 		       double& length)const
   {
-    Point p2 = p0+sp*l*Point(d);//Check the midpoint
-    if (fabs(p||p2) < R_EPSILON){
-      //The light beam starts from the shading segment 
+    hit_self = false;
+    if (fabs(p||p2) < R_EPSILON){//Check if start point of the light beam
+      hit_self = true;
+      //The light beam starts from the shading segment
+      //cout << p << dir << endl << p0 << d << endl << p2 << " " << sp << " " << l << endl<< endl; 
       return 0;
-    }
+    } 
     return CylinderBeamShading(p,dir,p0,d,rf,rw,l,length);
   }
 
@@ -43,6 +51,12 @@ public:
     //Check the two extreme cases
     if (result == 0){ //Not hit
       tau = 1.0;//clear sky so far
+      //Debug misses
+      //cout << tau << " " << p0.getX() << " "  << p0.getY() << " " << p0.getZ() << " "
+      //     << d.getX() << " " << d.getY() << " " << d.getZ() << " "
+      //     << p.getX() << " " << p.getY() << " " << p.getZ() << " " 
+      //     << dir.getX() << " "  << dir.getY() << " "  << dir.getZ() << " " 
+      //     << l << " " << rw << " " << rf << " " << rf-rw << endl;
       return tau;
     }
     else if (result == -1){//Wood hit
@@ -62,12 +76,16 @@ public:
     
     if (vf > R_EPSILON)
       tau = exp(-K(phi)*length*af/vf);
-    else
-      cerr << "Vf < R_EPSILON " << vf << " Af: " << af << endl;
+    else{//If there is no foliage Rf==R and Vf is 0, CylinderBeamShading returns then as if a hit to foliage
+      //cerr << "Vf < R_EPSILON: " << vf << " Af: " << af << " Rf: " << rf 
+      //     << " R: " << rw << " Rf-R " << rf-rw << " L: " << length << endl;
+      tau = 0.0; //Sector blocked
+    }
     return tau;
   }
 private:
   Point p0;//Start point of the cylinder
+  Point p2;//p0+sp*l*Point(d), point that could be the start point of the light beam
   PositionVector d;//direction of the cylinder
   double l;//Length of the cylinder
   double rw;//Wood radius
