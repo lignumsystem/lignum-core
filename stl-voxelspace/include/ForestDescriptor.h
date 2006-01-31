e23f33156ebec6a6eee9c4c1abaf05cc857e3193
jarip <>
2006-01-31 17:43:14 +0000

    Separated variables for broad leaved and conifers. One can access LAI for conifers and broadleaved with LGALAIc and LGALAIb respectively. Alsoa LGAAfc and LGAAfb is the leaf area for conifers and bradleaved. The height of the crown (height of the tallest tree in the stand) is in LGAH and the crown limit (lowest segment having folaige) is in LGAcbase.

--- stl-voxelspace/include/ForestDescriptor.h
+++ stl-voxelspace/include/ForestDescriptor.h
@@ -9,15 +9,24 @@ using namespace Lignum;
 //forest in the voxel space. The natural place to insert data 
 class ForestDescriptor{
   friend double SetValue(ForestDescriptor& fd, LGMAD name, double value);
-  friend double GetValue(ForestDescriptor& fd, LGMAD name);
+  friend double GetValue(const ForestDescriptor& fd, LGMAD name);
 public:
-  ForestDescriptor():af(0.0),lai(0.0),cbase(R_HUGE){}
-  //update LAI, A is the area of the voxel space in  meters
-  double updateLAI(double A){lai = af/A;}//call after the last call to InsertVoxelObject
-  void reset(){af=0.0,lai=0.0,cbase=R_HUGE;}//Reset by VoxelSpace::reset()
+  ForestDescriptor()
+    :afb(0.0),afc(0.0),laib(0.0),laic(0.0),h(-1.0),cbase(R_HUGE){}
+  //Update LAI, A is the area of the voxel space in  meters
+  //Call after  the last call to InsertVoxelObjects
+  double updateLAI(double A){laic = afc/A;laib = afb/A;}
+  //Reset by VoxelSpace::reset()
+  void reset(){
+    afb=0.0;afc=0.0;laib=0.0;
+    laic=0.0;h=-1.0;cbase=R_HUGE;
+  }
 private:
-  double af;//needle area
-  double lai;//leaf area index for coniferous
+  double afb;//leaf area, broad leaved
+  double afc;//needle area, conifers
+  double laib;//leaf area index for broad leaved
+  double laic;//leaf area index for conifers
+  double h;//height of the tallest tree 
   double cbase;//lower limit  for tree crowns (defined  as the smallest
 	      //z-coord of all segments that have foliage)
 };
@@ -25,13 +34,20 @@ private:
 inline double SetValue(ForestDescriptor& fd, LGMAD name, double value)
 {
   double val = GetValue(fd,name);
-  if (name == LGAAf){
-    fd.af = value;
+  if (name == LGAAfb){
+    fd.afb = value;
+  }
+  if (name == LGAAfc){
+    fd.afc = value;
   }
   //The lowest segment having foliage
   else if (name == LGAcbase){
     if (value < fd.cbase)
       fd.cbase = value;
+  }
+   else if (name == LGAH){
+    if (value > fd.h)
+      fd.h = value;
   }
   else{
     cerr << "ForestDescriptor SetValue Unknown name " << name <<endl;
@@ -41,16 +57,25 @@ inline double SetValue(ForestDescriptor& fd, LGMAD name, double value)
   return val;
 }
     
-inline double GetValue(ForestDescriptor& fd, LGMAD name)
+inline double GetValue(const ForestDescriptor& fd, LGMAD name)
 {
-  if (name == LGAAf){
-    return fd.af;
+  if (name == LGAAfb){
+    return fd.afb;
+  }
+  if (name == LGAAfc){
+    return fd.afc;
   }
   else if (name == LGAcbase){
     return fd.cbase;
   }
-  else if (name == LGAlai){
-    return fd.lai;
+  else if (name == LGAH){
+    return fd.h;
+  }
+  else if (name == LGALAIb){
+    return fd.laib;
+  }
+  else if (name == LGALAIc){
+    return fd.laic;
   }
   else{
     cerr << "ForestDescriptor GetValue Unknown name " << name << endl;
