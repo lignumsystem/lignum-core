9aa3802d5c22b8ae610025087b7379e4c5298983
jhartika <>
2006-08-10 06:33:49 +0000

    Added the possibility for calling external programs. Programs are called by writing the commands to the command line found in the lower part of the window above the text output box, and clicking the 'Run'-button.
    External programs can also be stopped with 'Kill program'-button, but this
    might still be a bit buggy.
    
    Also the working directory can be changed from 'Options'->'Change working
    directory' as some external programs might need files etc. to be found
    in the current working directory. Note that changing the working directory
    changes the working directory of the whole workbench, not only the external
    programs. This affects the loading of textures as by default they are
    loaded relatively from the current working directory of the workbench.
    The file names can though be changed to absolute values from the visualization
    options dialog.

--- qt-workbench/LignumWB.cpp
+++ qt-workbench/LignumWB.cpp
@@ -1,7 +1,7 @@
 #include "LignumWB.h"
 
 LignumWB::LignumWB(QWidget *parent)
-  : QMainWindow(parent), viz_config(NULL)
+  : QMainWindow(parent), viz_config(NULL), externalProgram(NULL)
 {
   ui.setupUi(this);
   connect(ui.actionQuit, SIGNAL(triggered()), this, SLOT(close()));
@@ -10,6 +10,9 @@ LignumWB::LignumWB(QWidget *parent)
   connect(ui.actionFree_roam, SIGNAL(triggered()), ui.gldrawer, SLOT(freeRoamMode()));
   connect(ui.actionOrbit, SIGNAL(triggered()), ui.gldrawer, SLOT(orbitCameraMode()));
   connect(ui.actionMove_center, SIGNAL(triggered()), ui.gldrawer, SLOT(moveCenterMode()));
+  connect(ui.actionWorking_directory, SIGNAL(triggered()), this, SLOT(setWorkingDirectory()));
+  connect(ui.runButton, SIGNAL(clicked()), this, SLOT(startExternalProgram()));
+  connect(ui.killButton, SIGNAL(clicked()), this, SLOT(killExternalProgram()));
 }
 
 void LignumWB::loadTree() {
@@ -44,4 +47,70 @@ void LignumWB::options() {
   conf->raise();*/
 }
 
+void LignumWB::setWorkingDirectory() {
+  QString directory = QFileDialog::getExistingDirectory(this,
+							"Choose a directory",
+							QDir::currentPath(),
+							QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+  
+  // Should this be implemented with signals & slots?
+  if(!directory.isEmpty()) {
+    if(QDir::setCurrent(directory))
+      ui.textBrowser->append(QString("Working directory changed succesfully."));
+    else 
+      ui.textBrowser->append(QString("Working directory not changed."));
+  }
+}
 
+void LignumWB::startExternalProgram() {
+  if(externalProgram == NULL) {
+    QStringList text = ui.console->displayText().split(QString(" "));
+    QString program = text.first();
+    text.removeFirst();
+    
+    externalProgram = new QProcess(this);
+    externalProgram->start(program, text);
+    connect(externalProgram, SIGNAL(finished(int, QProcess::ExitStatus)),
+	    this, SLOT(endExternalProgram(int, QProcess::ExitStatus)));
+    connect(externalProgram, SIGNAL(readyReadStandardOutput()),
+	    this, SLOT(addExternalOutput()));
+    connect(externalProgram, SIGNAL(error(QProcess::ProcessError)),
+	    this, SLOT(externalError(QProcess::ProcessError)));
+  }
+  else 
+    ui.textBrowser->append(QString("External program is already running! Wait for it to finish."));
+}
+
+void LignumWB::endExternalProgram(int exitCode, QProcess::ExitStatus exitStatus) {
+  clearExternalProgram();
+  if(exitStatus == QProcess::NormalExit)
+    ui.textBrowser->append(QString("External program exited normally."));
+  else
+    ui.textBrowser->append(QString("External program crashed."));
+}
+
+void LignumWB::addExternalOutput() {
+  if(externalProgram != NULL)
+    ui.textBrowser->append(QString(externalProgram->readAllStandardOutput()));
+}
+
+void LignumWB::externalError(QProcess::ProcessError error) {
+  ui.textBrowser->append(QString("Error running the external program. Exiting."));
+  clearExternalProgram();
+}
+
+void LignumWB::killExternalProgram() {
+  externalProgram->kill();
+  //  clearExternalProgram();
+}
+
+void LignumWB::clearExternalProgram() {
+  disconnect(externalProgram, SIGNAL(finished(int, QProcess::ExitStatus)),
+	     this, SLOT(endExternalProgram(int, QProcess::ExitStatus)));
+  disconnect(externalProgram, SIGNAL(readyReadStandardOutput()),
+	     this, SLOT(addExternalOutput()));
+  disconnect(externalProgram, SIGNAL(error(QProcess::ProcessError)),
+	     this, SLOT(externalError(QProcess::ProcessError)));
+  delete externalProgram;
+  externalProgram = NULL;
+}
--- qt-workbench/LignumWB.h
+++ qt-workbench/LignumWB.h
@@ -2,6 +2,7 @@
 #define LIGNUMWB_H
 
 #include <QMainWindow>
+#include <QProcess>
 #include "qt_wb.h"
 #include "VisualizationConfig.h"
 
@@ -15,11 +16,20 @@ public:
 private:
   Ui::MainWindow ui;
   VisualizationConfig* viz_config;
+  QProcess *externalProgram;
+  //QString workingDirectory;
+
+  void clearExternalProgram();
 
 private slots:
   void loadTree();
   void options();
-  
+  void setWorkingDirectory();
+  void startExternalProgram();
+  void endExternalProgram(int, QProcess::ExitStatus);
+  void addExternalOutput();
+  void externalError(QProcess::ProcessError error);
+  void killExternalProgram();
 };
 
 #endif
