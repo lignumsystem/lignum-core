4656b04e2932e9be922b4a0890da453edaf34710
jhartika <>
2006-08-14 09:40:06 +0000

    It's possible to start new files with the editor. If previous file were not saved a dialog is popped prompting the user whether to save to file or not. Same kind of dialog is also now used when loading a function from a file.
    Window now displays the current file in it's title. If there are any unsaved
    changes the character "*" is displayed after the file name.
    
    The painter should also work better now, and it displays numbers beside
    the ticks along the axes.

--- qt-workbench/FunctionEditor.cpp
+++ qt-workbench/FunctionEditor.cpp
@@ -1,27 +1,73 @@
 #include "FunctionEditor.h"
 
 #include <QFileDialog>
+#include <QMessageBox>
 #include <QTextStream>
 
 FunctionEditor::FunctionEditor(QWidget *parent) 
-  : QMainWindow(parent) {
+  : QMainWindow(parent), fileSaved(false) {
   ui.setupUi(this);
-  f_painter = new FunctionPainter(this);
-  f_painter->show();
-  f_painter->setGeometry(5, 30, 400, 400);
+  //  f_painter = new FunctionPainter(this);
+  //f_painter->show();
+  //f_painter->setGeometry(5, 30, 400, 400);
   // f_painter->changeFunction(QString("-2 2 -0.2 -3.1 0.5 0.5 1.0 2.0 1.5 0.5"));
   //  f_painter->setView(-5, -5, 5, 5);
-  f_painter->setTicks(0.5, 0.5);
+  ui.f_painter->setTicks(1, 1);
   
+  connect(ui.actionNew, SIGNAL(triggered()), this, SLOT(newFile()));
   connect(ui.actionOpen, SIGNAL(triggered()), this, SLOT(openFile()));
   connect(ui.actionSave, SIGNAL(triggered()), this, SLOT(saveFile()));
   connect(ui.actionSave_as, SIGNAL(triggered()), this, SLOT(saveFileAs()));
   connect(ui.actionQuit, SIGNAL(triggered()), this, SLOT(close()));
-  connect(ui.updateButton, SIGNAL(clicked()), this, SLOT(updateFunction()));
   connect(ui.textEdit, SIGNAL(textChanged()), this, SLOT(updateFunction()));
+  connect(ui.newButton, SIGNAL(clicked()), this, SLOT(newFile()));
+  connect(ui.openButton, SIGNAL(clicked()), this, SLOT(openFile()));
+  connect(ui.saveButton, SIGNAL(clicked()), this, SLOT(saveFile()));
+  connect(ui.saveAsButton, SIGNAL(clicked()), this, SLOT(saveFileAs()));
+
+  setWindowName();
+}
+
+void FunctionEditor::newFile() {
+  if(!ui.textEdit->toPlainText().isEmpty() && !fileSaved) {
+    int message = QMessageBox::question(this,
+					QString("Save changes?"),
+					QString("Do you want to save changes before starting a new file?"),
+					QMessageBox::Yes,
+					QMessageBox::No,
+					QMessageBox::Cancel);
+    if(message == QMessageBox::Cancel)
+      return;
+    else {
+      if(message == QMessageBox::Yes) {
+	saveFile();
+      }
+    }
+  }
+  fileSaved = false;
+  fileName.clear();
+  ui.textEdit->clear();
+  ui.f_painter->clear();
+  setWindowName();
+
 }
 
 void FunctionEditor::openFile() {
+  if(!ui.textEdit->toPlainText().isEmpty() && !fileSaved) {
+    int message = QMessageBox::question(this,
+					QString("Save changes?"),
+					QString("Do you want to save changes before opening a file?"),
+					QMessageBox::Yes,
+					QMessageBox::No,
+					QMessageBox::Cancel);
+    if(message == QMessageBox::Cancel)
+      return;
+    else {
+      if(message == QMessageBox::Yes) {
+	saveFile();
+      }
+    }
+  }
   fileName = QFileDialog::getOpenFileName(this,
 					  "Choose a file",
 					  QDir::currentPath(),
@@ -33,7 +79,10 @@ void FunctionEditor::openFile() {
     QString text(file.readAll());
     
     ui.textEdit->setPlainText(text);
-    f_painter->changeFunction(text);
+    ui.f_painter->changeFunction(text);
+    fileSaved = true;
+    setWindowName();
+  
   }
 }
 
@@ -42,8 +91,11 @@ void FunctionEditor::saveFile() {
     QFile file(fileName);
     if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
       return;
+    
     QTextStream out(&file);
     out << ui.textEdit->toPlainText();
+    fileSaved = true;
+    setWindowName();
   }
   else 
     saveFileAs();
@@ -62,6 +114,20 @@ void FunctionEditor::saveFileAs() {
 }
 
 void FunctionEditor::updateFunction() {
-  f_painter->changeFunction(ui.textEdit->toPlainText());
+  ui.f_painter->changeFunction(ui.textEdit->toPlainText());
+  fileSaved = false;
+  setWindowName();
   //f_painter->setView(-5, -5, 5, 5);
+}
+
+void FunctionEditor::setWindowName() {
+  if(fileName.isEmpty()) {
+    setWindowTitle(QString("Function Editor - *untitled*"));
+  }
+  else {
+    if(fileSaved)
+      setWindowTitle(QString("Function Editor - %1").arg(fileName));
+    else
+      setWindowTitle(QString("Function Editor - %1*").arg(fileName));
+  }
 }
--- qt-workbench/FunctionPainter.cpp
+++ qt-workbench/FunctionPainter.cpp
@@ -4,10 +4,15 @@
 using namespace std;
 
 
-FunctionPainter::FunctionPainter(QWidget *parent)
-: QWidget(parent), xMarginal(1.1), yMarginal(1.1), xTick(1.0), yTick(1.0),
-  tickLength(4) {
-
+FunctionPainter::FunctionPainter(QWidget* parent)
+  :QWidget(parent), xMarginal(1.1), yMarginal(1.1), xTick(1.0), yTick(1.0),
+      tickLength(4) {
+  emit xTicksChanged(xTick);
+  emit yTicksChanged(yTick);
+  emit minXChanged(0);
+  emit maxXChanged(0);
+  emit minYChanged(0);
+  emit maxYChanged(0);
 }
 
 void FunctionPainter::changeFunction(QString values) {
@@ -28,18 +33,40 @@ void FunctionPainter::changeFunction(QString values) {
       else if(points[i] > maxYValue)
 	maxYValue = points[i];
     }
-    minXValue *= xMarginal;
-    maxXValue *= xMarginal; 
-    minYValue *= yMarginal;
-    maxXValue *= yMarginal;
+    xTick = (maxXValue - minXValue) / 10;
+    yTick = (maxYValue - minYValue) / 10;
+    emit xTicksChanged(xTick);
+    emit yTicksChanged(yTick);  
+
+
+    minXValue -= xTick;
+    maxXValue += xTick; 
+    minYValue -= yTick;
+    maxYValue += yTick;
     
     xRange = maxXValue - minXValue;
     yRange = maxYValue - minYValue;
-    
+   
+    emit minXChanged(minXValue);
+    emit maxXChanged(maxXValue);
+    emit minYChanged(minYValue);
+    emit maxYChanged(maxYValue);
   }
   update();
 }
 
+void FunctionPainter::clear() {
+  xTick = 1;
+  yTick = 1;
+  minXValue = -xTick;
+  maxXValue = xTick;
+  minYValue = -yTick;
+  maxYValue = yTick;
+  xRange = maxXValue - minXValue;
+  yRange = maxYValue - minYValue;
+  points.clear();
+  update();
+}
 
 void FunctionPainter::paintEvent(QPaintEvent *event) {
   // Create the painter object
@@ -55,20 +82,33 @@ void FunctionPainter::paintEvent(QPaintEvent *event) {
   // Draw the ticks for x-axis
   if (size().height() + minYValue / yRange * size().height() + tickLength >= 0 &&
       size().height() + minYValue / yRange * size().height() - tickLength <= size().height()) {
+    // Positive axis
     int i = 1;
     while(i*xTick < maxXValue) {
+      // Draw the ticks
       painter.drawLine(QPoint((i*xTick - minXValue) / xRange * size().width(),
 			      size().height() + minYValue / yRange * size().height() + tickLength),
 		       QPoint((i*xTick - minXValue) / xRange * size().width(),
 			      size().height() + minYValue / yRange * size().height() - tickLength));
+      // Draw the numbers beside the ticks
+      painter.drawText(QPoint((i*xTick - minXValue) / xRange * size().width() - tickLength/2,
+			      size().height() + minYValue / yRange * size().height() - tickLength*2),
+		       QString("%1").arg(i*xTick));
       i++;
     }
+
+    // Negative axis
     i = -1;
     while(i*xTick > minXValue) {
+      // Draw the ticks
       painter.drawLine(QPoint((i*xTick - minXValue) / xRange * size().width(),
 			      size().height() + minYValue / yRange * size().height() + tickLength),
 		       QPoint((i*xTick - minXValue) / xRange * size().width(),
 			      size().height() + minYValue / yRange * size().height() - tickLength));
+      // Draw the numbers beside the ticks
+      painter.drawText(QPoint((i*xTick - minXValue) / xRange * size().width() - tickLength/2,
+			      size().height() + minYValue / yRange * size().height() - tickLength*2),
+		       QString("%1").arg(i*xTick));
       i--;
     }
   }
@@ -82,20 +122,35 @@ void FunctionPainter::paintEvent(QPaintEvent *event) {
    // Draw the ticks for y-axis
   if (-minXValue / xRange * size().width() + tickLength >= 0 &&
       -minXValue / xRange * size().width() - tickLength <= size().width()) {
+    // Positive axis 
     int i = 1;
     while(i*yTick < maxYValue) {
+      // Draw the ticks
       painter.drawLine(QPoint(-minXValue / xRange * size().width() + tickLength,
 			      size().height() - (i*yTick - minYValue) / yRange * size().height()),
 		       QPoint(-minXValue / xRange * size().width() - tickLength,
 			      size().height() - (i*yTick - minYValue) / yRange * size().height()));
+      // Draw the number beside the ticks
+      painter.drawText(QPoint(-minXValue / xRange * size().width() + tickLength*2,
+			      size().height() - (i*yTick - minYValue) / yRange * size().height() +
+			      tickLength),
+		       QString("%1").arg(i*yTick));
       i++;
     }
+
+    // Negative axis
     i = -1;
     while(i*yTick > minYValue) {
+      // Draw the ticks
       painter.drawLine(QPoint(-minXValue / xRange * size().width() + tickLength,
 			      size().height() - (i*yTick - minYValue) / yRange * size().height()),
 		       QPoint(-minXValue / xRange * size().width() - tickLength,
 			      size().height() - (i*yTick - minYValue) / yRange * size().height()));
+      // Draw the numbers beside the ticks
+      painter.drawText(QPoint(-minXValue / xRange * size().width() + tickLength*2,
+			      size().height() - (i*yTick - minYValue) / yRange * size().height() +
+			      tickLength),
+		       QString("%1").arg(i*yTick));
       i--;
     }
   }
@@ -123,12 +178,16 @@ void FunctionPainter::setView(double minX, double minY, double maxX, double maxY
     minXValue = minX;
     maxXValue = maxX;
     xRange = maxXValue - minXValue;
+    emit minXChanged(minXValue);
+    emit maxXChanged(maxXValue);
   }
 
   if(minY <= maxY) {
     minYValue = minY;
     maxYValue = maxY;
     yRange = maxYValue - minYValue;
+    emit minYChanged(minYValue);
+    emit maxYChanged(maxYValue);
   }
   update();
 }
@@ -136,5 +195,55 @@ void FunctionPainter::setView(double minX, double minY, double maxX, double maxY
 void FunctionPainter::setTicks(double width, double height) {
   xTick = width;
   yTick = height;
+  emit xTicksChanged(xTick);
+  emit yTicksChanged(yTick);
+  update();
+}
+
+void FunctionPainter::setXTicks(double value) {
+  xTick = value;
+  emit xTicksChanged(xTick);
+  update();
+}
+
+void FunctionPainter::setYTicks(double value) {
+  yTick = value;
+  emit yTicksChanged(yTick);
+  update();
+}
+
+void FunctionPainter::setMinX(double value) {
+  if(value < maxXValue) {
+    minXValue = value; 
+    xRange = maxXValue - minXValue;
+  }
+  emit minXChanged(minXValue);
+  update();
+}
+
+void FunctionPainter::setMaxX(double value) {
+  if(value > minXValue) {
+    maxXValue = value;
+    xRange = maxXValue - minXValue;
+  }
+  emit maxXChanged(maxXValue);
+  update();
+}
+
+void FunctionPainter::setMinY(double value) {
+  if(value < maxYValue) {
+    minYValue = value;
+    yRange = maxYValue - minYValue;
+  }
+  emit minYChanged(minYValue);
+  update();
+}
+
+void FunctionPainter::setMaxY(double value) {
+  if(value > minYValue) {
+    maxYValue = value;
+    yRange = maxYValue - minYValue;
+  }
+  emit maxYChanged(maxYValue);
   update();
 }
