0c056dd2b92403144b8b8a7cb0d10b8199d5dfff
jhartika <>
2006-08-24 10:02:19 +0000

    Changed the viewer to be a dock widget.

--- qt-workbench/xml_viewer.h
+++ qt-workbench/xml_viewer.h
@@ -6,85 +6,85 @@
 #include <QtGui/QApplication>
 #include <QtGui/QButtonGroup>
 #include <QtGui/QHBoxLayout>
-#include <QtGui/QMainWindow>
-#include <QtGui/QMenu>
-#include <QtGui/QMenuBar>
-#include <QtGui/QStatusBar>
-#include <QtGui/QToolBar>
+#include <QtGui/QPushButton>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QVBoxLayout>
 #include <QtGui/QWidget>
 #include "XMLViewer.h"
 
 class Ui_TreeViewer
 {
 public:
-    QAction *actionAdd_tree;
-    QAction *actionQuit;
-    QAction *actionUpdate_visualization;
-    QWidget *centralwidget;
     QHBoxLayout *hboxLayout;
+    QVBoxLayout *vboxLayout;
+    QHBoxLayout *hboxLayout1;
+    QPushButton *addButton;
+    QPushButton *removeButton;
+    QPushButton *updatevButton;
+    QSpacerItem *spacerItem;
     XMLViewer *xml_viewer;
-    QMenuBar *menubar;
-    QMenu *menuFile;
-    QStatusBar *statusbar;
-    QToolBar *toolBar;
 
-    void setupUi(QMainWindow *TreeViewer)
+    void setupUi(QWidget *TreeViewer)
     {
     TreeViewer->setObjectName(QString::fromUtf8("TreeViewer"));
-    TreeViewer->resize(QSize(800, 600).expandedTo(TreeViewer->minimumSizeHint()));
-    actionAdd_tree = new QAction(TreeViewer);
-    actionAdd_tree->setObjectName(QString::fromUtf8("actionAdd_tree"));
-    actionQuit = new QAction(TreeViewer);
-    actionQuit->setObjectName(QString::fromUtf8("actionQuit"));
-    actionUpdate_visualization = new QAction(TreeViewer);
-    actionUpdate_visualization->setObjectName(QString::fromUtf8("actionUpdate_visualization"));
-    centralwidget = new QWidget(TreeViewer);
-    centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-    hboxLayout = new QHBoxLayout(centralwidget);
+    TreeViewer->resize(QSize(581, 582).expandedTo(TreeViewer->minimumSizeHint()));
+    hboxLayout = new QHBoxLayout(TreeViewer);
     hboxLayout->setSpacing(6);
     hboxLayout->setMargin(9);
     hboxLayout->setObjectName(QString::fromUtf8("hboxLayout"));
-    xml_viewer = new XMLViewer(centralwidget);
+    vboxLayout = new QVBoxLayout();
+    vboxLayout->setSpacing(6);
+    vboxLayout->setMargin(0);
+    vboxLayout->setObjectName(QString::fromUtf8("vboxLayout"));
+    hboxLayout1 = new QHBoxLayout();
+    hboxLayout1->setSpacing(6);
+    hboxLayout1->setMargin(0);
+    hboxLayout1->setObjectName(QString::fromUtf8("hboxLayout1"));
+    addButton = new QPushButton(TreeViewer);
+    addButton->setObjectName(QString::fromUtf8("addButton"));
+
+    hboxLayout1->addWidget(addButton);
+
+    removeButton = new QPushButton(TreeViewer);
+    removeButton->setObjectName(QString::fromUtf8("removeButton"));
+
+    hboxLayout1->addWidget(removeButton);
+
+    updatevButton = new QPushButton(TreeViewer);
+    updatevButton->setObjectName(QString::fromUtf8("updatevButton"));
+
+    hboxLayout1->addWidget(updatevButton);
+
+    spacerItem = new QSpacerItem(71, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+    hboxLayout1->addItem(spacerItem);
+
+
+    vboxLayout->addLayout(hboxLayout1);
+
+    xml_viewer = new XMLViewer(TreeViewer);
     xml_viewer->setObjectName(QString::fromUtf8("xml_viewer"));
+    xml_viewer->setMinimumSize(QSize(300, 400));
 
-    hboxLayout->addWidget(xml_viewer);
+    vboxLayout->addWidget(xml_viewer);
 
-    TreeViewer->setCentralWidget(centralwidget);
-    menubar = new QMenuBar(TreeViewer);
-    menubar->setObjectName(QString::fromUtf8("menubar"));
-    menubar->setGeometry(QRect(0, 0, 800, 31));
-    menuFile = new QMenu(menubar);
-    menuFile->setObjectName(QString::fromUtf8("menuFile"));
-    TreeViewer->setMenuBar(menubar);
-    statusbar = new QStatusBar(TreeViewer);
-    statusbar->setObjectName(QString::fromUtf8("statusbar"));
-    TreeViewer->setStatusBar(statusbar);
-    toolBar = new QToolBar(TreeViewer);
-    toolBar->setObjectName(QString::fromUtf8("toolBar"));
-    toolBar->setOrientation(Qt::Horizontal);
-    TreeViewer->addToolBar(static_cast<Qt::ToolBarArea>(4), toolBar);
 
-    menubar->addAction(menuFile->menuAction());
-    menuFile->addAction(actionAdd_tree);
-    menuFile->addSeparator();
-    menuFile->addAction(actionQuit);
-    toolBar->addAction(actionAdd_tree);
-    toolBar->addSeparator();
-    toolBar->addAction(actionUpdate_visualization);
+    hboxLayout->addLayout(vboxLayout);
+
     retranslateUi(TreeViewer);
 
     QMetaObject::connectSlotsByName(TreeViewer);
     } // setupUi
 
-    void retranslateUi(QMainWindow *TreeViewer)
+    void retranslateUi(QWidget *TreeViewer)
     {
-    TreeViewer->setWindowTitle(QApplication::translate("TreeViewer", "MainWindow", 0, QApplication::UnicodeUTF8));
-    actionAdd_tree->setText(QApplication::translate("TreeViewer", "Add tree...", 0, QApplication::UnicodeUTF8));
-    actionQuit->setText(QApplication::translate("TreeViewer", "Quit", 0, QApplication::UnicodeUTF8));
-    actionUpdate_visualization->setText(QApplication::translate("TreeViewer", "Update visualization", 0, QApplication::UnicodeUTF8));
+    TreeViewer->setWindowTitle(QApplication::translate("TreeViewer", "Form", 0, QApplication::UnicodeUTF8));
+    addButton->setText(QApplication::translate("TreeViewer", "Add tree...", 0, QApplication::UnicodeUTF8));
+    removeButton->setText(QApplication::translate("TreeViewer", "Remove selected", 0, QApplication::UnicodeUTF8));
+    updatevButton->setText(QApplication::translate("TreeViewer", "Update visualization", 0, QApplication::UnicodeUTF8));
     xml_viewer->setToolTip(QApplication::translate("TreeViewer", "Function painter for Lignum", 0, QApplication::UnicodeUTF8));
     xml_viewer->setWhatsThis(QApplication::translate("TreeViewer", " Function painter for Lignum", 0, QApplication::UnicodeUTF8));
-    menuFile->setTitle(QApplication::translate("TreeViewer", "File", 0, QApplication::UnicodeUTF8));
+    Q_UNUSED(TreeViewer);
     } // retranslateUi
 
 };
