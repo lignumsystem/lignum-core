374e9dc480ad96aa61ded934a47a29a5ab34d6d2
jhartika <>
2006-08-29 07:15:22 +0000

    Thread class for loading the BSP-tree outside the main thread. Should improve the general usability of the workbench as the GUI doesn't get blocked any longer while loading trees from XML viewer to visualization.
    However, loading new files to XML viewer still blocks the GUI, but that can
    be done in a it's own thread also.
    
    It would also be nice to display some kind of progress bar beside the XML
    viewer telling how much of the BSP-tree is constructed, but that can quite
    tricky to do as the actual size of the BSP ain't known until it's fully
    constructed. However, the bar doesn't have to be 100% precise.

---
+++ qt-workbench/BSPLoaderThread.cpp
@@ -0,0 +1,139 @@
+#include <QtGui>
+
+#include "BSPLoaderThread.h"
+#include <Lignum.h>
+#include <GenericCfTree.h>
+#include <GenericHwTree.h>
+#include <XMLTree.h>
+#include <Point.h>
+#include <PositionVector.h>
+#include <LGMPolygonTree.h>
+
+using namespace cxxadt;
+
+BSPLoaderThread::BSPLoaderThread(QObject *parent)
+  : QThread(parent)
+{
+  abort = false;
+}
+
+BSPLoaderThread::~BSPLoaderThread()
+{
+  mutex.lock();
+  abort = true;
+  condition.wakeOne();
+  mutex.unlock();
+
+  wait();
+}
+
+
+void BSPLoaderThread::loadTrees(QList<QString> files, VisualizationParameters parameters) 
+{
+
+  QMutexLocker locker(&mutex);
+
+  this->files = files;
+  this->parameters = parameters;
+
+  if(!isRunning()){
+    start(LowPriority);
+  } else {
+    condition.wakeOne();
+  }
+
+}
+
+void BSPLoaderThread::run()
+{
+  forever {
+    mutex.lock();
+    QList<QString> files = this->files;
+    VisualizationParameters parameters = this->parameters;
+    mutex.unlock();
+
+    BSPTree *tree = new BSPTree;
+    PositionVector r_axis;
+    Point t_point;
+    double t_height;
+    QHash<QString, QMultiHash<int, SceneObject*>* > *sceneObjects = new QHash<QString, QMultiHash<int, SceneObject*>*>;
+
+    BSPPolygonSet polygons;
+        
+    for(int i = 0; i < files.size(); i++) {
+      QString fileName = files[i];
+      XMLDomTreeReader<GenericCfTreeSegment, GenericCfBud> cf_reader;
+      
+      if(cf_reader.treeType(fileName.toStdString()) == XMLDomTreeReader<GenericCfTreeSegment, GenericCfBud>::Cf) {
+	Tree<GenericCfTreeSegment, GenericCfBud> cftree(Point(0,0,0), PositionVector(0,1,0));
+	cf_reader.readXMLToTree(cftree, fileName.toStdString());
+	
+	LGMPolygonTree<GenericCfTreeSegment, GenericCfBud> constructor;
+	BSPPolygonSet* treePolygons = constructor.buildTree(cftree, parameters,
+							    cf_reader.getTreeCompartmentHash(),
+							    cf_reader.getLeafHash());
+	polygons.addPolygons(treePolygons);
+	delete treePolygons;
+	sceneObjects->insert(fileName, constructor.getSceneObjects());
+	r_axis = GetDirection(GetRootAxis(cftree));
+	t_point = GetPoint(cftree);
+	t_height = GetValue(cftree, LGAH);
+	r_axis = r_axis.normalize();
+	r_axis = PositionVector(r_axis.getX(), r_axis.getZ(), -r_axis.getY());
+	t_point = Point(t_point.getX(), t_point.getZ(), -t_point.getY());
+      }
+      else {
+	if(cf_reader.leafType(fileName.toStdString()) == XMLDomTreeReader<GenericCfTreeSegment, GenericCfBud>::TRIANGLE) {
+	  
+	  XMLDomTreeReader<GenericHwTriangleTreeSegment, GenericHwTriangleBud, Triangle> hwt_reader;	
+	  Tree<GenericHwTriangleTreeSegment, GenericHwTriangleBud> hwtree(Point(0,0,0), PositionVector(0,1,0));
+	  hwt_reader.readXMLToTree(hwtree, fileName.toStdString());
+	  
+	  LGMPolygonTree<GenericHwTriangleTreeSegment, GenericHwTriangleBud, Triangle> constructor;
+	  BSPPolygonSet* treePolygons = constructor.buildTree(hwtree, parameters,
+							      hwt_reader.getTreeCompartmentHash(),
+							      hwt_reader.getLeafHash());
+	  polygons.addPolygons(treePolygons);
+	  delete treePolygons;
+	  sceneObjects->insert(fileName, constructor.getSceneObjects());
+	  
+	  r_axis = GetDirection(GetRootAxis(hwtree));
+	  t_point = GetPoint(hwtree);
+	  t_height = GetValue(hwtree, LGAH);
+	  r_axis = r_axis.normalize();
+	  r_axis = PositionVector(r_axis.getX(), r_axis.getZ(), -r_axis.getY());
+	  t_point = Point(t_point.getX(), t_point.getZ(), -t_point.getY());
+	}
+	else if (cf_reader.leafType(fileName.toStdString()) == XMLDomTreeReader<GenericCfTreeSegment, GenericCfBud>::ELLIPSE) {
+	  XMLDomTreeReader<GenericHwEllipseTreeSegment, GenericHwEllipseBud, cxxadt::Ellipse> hwt_reader;	
+	  Tree<GenericHwEllipseTreeSegment, GenericHwEllipseBud> hwtree(Point(0,0,0), PositionVector(0,1,0));
+	  hwt_reader.readXMLToTree(hwtree, fileName.toStdString());
+	  
+	  LGMPolygonTree<GenericHwEllipseTreeSegment, GenericHwEllipseBud, cxxadt::Ellipse> constructor;
+	  BSPPolygonSet* treePolygons = constructor.buildTree(hwtree, parameters,
+							      hwt_reader.getTreeCompartmentHash(),
+							      hwt_reader.getLeafHash());
+	  polygons.addPolygons(treePolygons);
+	  delete treePolygons;
+	  sceneObjects->insert(fileName, constructor.getSceneObjects());
+	  
+	  r_axis = GetDirection(GetRootAxis(hwtree));
+	  t_point = GetPoint(hwtree);
+	  t_height = GetValue(hwtree, LGAH);
+	  r_axis = r_axis.normalize();
+	  r_axis = PositionVector(r_axis.getX(), r_axis.getZ(), -r_axis.getY());
+	  t_point = Point(t_point.getX(), t_point.getZ(), -t_point.getY());
+	}
+      }
+    }
+    
+    tree->buildBSPTree(polygons);
+
+    emit treesLoaded(tree, t_point, r_axis, t_height, sceneObjects);
+    
+    mutex.lock();
+    condition.wait(&mutex);
+    mutex.unlock();
+    //tree->addPolygonsToTree(polygons);
+  }
+}
---
+++ qt-workbench/BSPLoaderThread.h
@@ -0,0 +1,41 @@
+#ifndef BSPLOADERTHREAD_H
+#define BSPLOADERTHREAD_H
+
+#include <QMutex>
+#include <QThread>
+#include <QWaitCondition>
+#include <QList>
+
+#include <BSPTree.h>
+#include <VisualizationParameters.h>
+
+class BSPLoaderThread : public QThread
+{
+  Q_OBJECT
+
+public: 
+
+  BSPLoaderThread(QObject *parent = 0);
+  ~BSPLoaderThread();
+
+  void loadTrees(QList<QString> files, VisualizationParameters parameters);
+  
+signals:
+  void treesLoaded(BSPTree *tree, Point t_point, PositionVector r_axis,
+		   double t_height, QHash<QString, QMultiHash<int, SceneObject*>* > *sceneObjects);
+
+protected:
+  void run();
+
+private:
+  QMutex mutex;
+  QWaitCondition condition;
+  
+  bool abort;
+  
+  QList<QString> files;
+  VisualizationParameters parameters;
+
+};
+
+#endif
