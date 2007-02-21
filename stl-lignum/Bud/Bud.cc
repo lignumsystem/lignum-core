5833b5bafd8539a6d374e38c3187f43dbc01877c
jarip <>
2007-02-21 13:39:55 +0000

    Removed conditional compiling for Windows. Added intitalizer for qin=0.0.

--- stl-lignum/Bud/Bud.cc
+++ stl-lignum/Bud/Bud.cc
@@ -2,22 +2,11 @@
 #include <Bud.h>
 namespace Lignum{
 
-
-#ifdef _MSC_VER
-
-//cvs: korjaa!
-BudAttributes::BudAttributes()
-  :ip(0.0),omega(0.0),state(ALIVE)
-{
-}
-
-#else
-BudAttributes::BudAttributes()
-  :collision(0.0),ip(0.0),omega(0.0),type(0.0),status(0.0),state(ALIVE)
-{
-}
-
-#endif // _MSC_VER
+  BudAttributes::BudAttributes()
+    :collision(0.0),ip(0.0),omega(0.0),qin(0.0),
+     type(0.0),status(0.0),state(ALIVE)
+  {
+  }
 
 
 }//closing namespace Lignum
