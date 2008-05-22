148f070c2572e1abd2d0e6486a5a12858cffe34c
jarip <>
2008-05-22 09:12:37 +0000

    Using abs(int) to take absolute value for integers. It seems that at least on Mac  <numeric> must be included to use abs (in cmath abs takes floating point values (in contrast to c++ standard).

--- c++adt/include/Bernoulli.h
+++ c++adt/include/Bernoulli.h
@@ -2,14 +2,15 @@
 #define BERNOULLI_H
 
 #include <Uniform.h>
+#include <numeric>
 #include <cmath>
 using namespace std;
 namespace cxxadt{
   //Implement Bernoulli distribution: Pr(X=1) = 1 - Pr(X=0) = 1 - q = p
   class Bernoulli{
   public:
-    Bernoulli(int seed = -1):uniform(-fabs(seed)){}
-    double init(int seed = -1){return uniform.init(-fabs(seed));}
+    Bernoulli(int seed = -1):uniform(-abs(seed)){}
+    double init(int seed = -1){return uniform.init(-abs(seed));}
     double operator()(double p, int seed=1);
   private:
     Uniform uniform;
