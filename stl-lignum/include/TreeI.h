b3a57c920fe247efb69d99eb8e3fb9d61274f3db
matrps <>
2008-09-03 06:20:11 +0000

    Cancelling the previous commit: The LGP name is LGPlen_random.

--- stl-lignum/include/TreeI.h
+++ stl-lignum/include/TreeI.h
@@ -173,9 +173,6 @@ LGMdouble GetValue(const Tree<TS,BUD>& tree, const LGMPD name)
   else if (name == LGPzbrentEpsilon)
      return tree.tp.zbrentEpsilon;
 
-  else if (name == LGPlenRandom)
-     return tree.tp.lenRandom;
-
   else{
     cerr << "Tree::GetValue() uknown parameter: " << name << " returning 0.0" 
 	 << endl;
@@ -264,9 +261,6 @@ LGMdouble SetValue(Tree<TS,BUD>& tree, const LGMPD name, const LGMdouble value)
 
   else if (name == LGPzbrentEpsilon)
     tree.tp.zbrentEpsilon = value;
-
-  else if (name == LGPlenRandom)
-    tree.tp.lenRandom = value;
 
   else{
     cerr << "Tree::SetValue unknown parameter: " << name << " returning 0.0" 
