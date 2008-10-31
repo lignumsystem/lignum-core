a9790e8f270149be82455ac5bf843f335c1e6841
matrps <>
2008-10-31 14:13:21 +0000

    Updated version.

--- c++adt/src/InvNorm.cc
+++ c++adt/src/InvNorm.cc
@@ -2,9 +2,35 @@
 
 namespace cxxadt{
 
-  double InvNorm::operator() (const double u) const {
-    double xx = inv_norm(u);
-    return 1.0;
-  }
+  double InvNorm::operator()(const double u)const{
+    if(u >= 0.5)
+      return inv_norm(u);
+    else
+      return -inv_norm(1.0-u);
+    }
+
 }//closing namespace cxxadt
 
+
+#ifdef INVNORM
+
+#include <iostream>
+using namespace cxxadt;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+ std::cout << "Usage: InvNorm(u)" << endl;
+ 
+
+  //Print out function
+  cout << "u      InvNorm(u)" << endl;
+  InvNorm q;
+  for (double u = 0; u < 1.01; u += 0.1){
+    cout << u << " " <<  q(u) << endl;
+  }
+
+
+}
+#endif
