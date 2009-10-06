78591503f4520dcc1052ea932f4a6dc3a837ca75
matrps <>
2009-10-06 08:31:24 +0000

    Included attribute number_of_segments_real in reset & initialization.

--- stl-voxelspace/src/VoxelBox.cc
+++ stl-voxelspace/src/VoxelBox.cc
@@ -11,7 +11,8 @@ namespace Lignum {
     needleArea(0.0),leafArea(0.0), Q_in(0.0), Q_abs(0.0), Qin_mean(0.0),Qabs_mean(0.0),star(0.0),
     starSum(0.0),weight(0.0),Q_inStdDiffuse(0.0),interceptedRadiation(0.0),
     needleMass(0.0),leafMass(0.0),number_of_segments(0),number_of_leaves(0),
-    big_leaf_normal(0,0,0),val_c(0.0),val_b(0.0),woodMass(0.0),woodArea(0.0)
+    big_leaf_normal(0,0,0),val_c(0.0),val_b(0.0),woodMass(0.0),woodArea(0.0),
+    number_of_segments_real(0.0)
   { 
     space = s;
   }
@@ -24,7 +25,8 @@ namespace Lignum {
     needleArea(0.0),leafArea(0.0),Q_in(0.0),Q_abs(0.0),Qin_mean(0.0),Qabs_mean(0.0),star(0.0),
     starSum(0.0),weight(0.0),Q_inStdDiffuse(0.0),interceptedRadiation(0.0),
     needleMass(0.0), leafMass(0.0),number_of_segments(0),number_of_leaves(0), 
-    big_leaf_normal(0,0,0),val_c(0.0),val_b(0.0),woodMass(0.0),woodArea(0.0)
+    big_leaf_normal(0,0,0),val_c(0.0),val_b(0.0),woodMass(0.0),woodArea(0.0),
+    number_of_segments_real(0.0)
   { 
     space = NULL; 
   }
