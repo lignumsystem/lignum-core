fac11a72820e7a970da0cad1267e1d1f6960a042
jarip <>
2010-10-20 14:04:54 +0000

    Methods alpha, beta, gamma normalize the vector before the angle between X,Y and Z respectively is taken.

--- c++adt/src/PositionVector.cc
+++ c++adt/src/PositionVector.cc
@@ -72,17 +72,23 @@ PositionVector::operator Point () const
 
 double PositionVector::alpha()const
 {
-  return acos(Dot(PositionVector(1,0,0),*this));
+  PositionVector d(*this);
+  d.normalize();
+  return acos(Dot(PositionVector(1,0,0),d));
 }
 
 double PositionVector::beta()const
 {
-  return acos(Dot(PositionVector(0,1,0),*this));
+  PositionVector d(*this);
+  d.normalize();
+  return acos(Dot(PositionVector(0,1,0),d));
 }
 
 double PositionVector::gamma()const
 {
-  return acos(Dot(PositionVector(0,0,1),*this));
+  PositionVector d(*this);
+  d.normalize();
+  return acos(Dot(PositionVector(0,0,1),d));
 }
 
 PositionVector& PositionVector::rotate(ROTATION direction, RADIAN angle)
