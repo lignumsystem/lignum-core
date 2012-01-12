f898fb628c8f9c750f2e597793b4080ea2ae8396
jarip <>
2012-01-12 08:40:06 +0000

    LSystem::print(): Corrected to the comparison in if statement    if (arg == true) (Previously an apparent typing error caused assignment)

--- LEngine/include/LSystem.h
+++ LEngine/include/LSystem.h
@@ -178,7 +178,7 @@ inline void LSystem<TS,BUD,N,F>::print(bool arg)
     cout << name;
     //fprintf(stdout,"%s",name);
     //print the argument value of f.
-    if (arg = true){ 
+    if (arg == true){ 
       if (strcmp(name,"F") == 0||strcmp(name,"f") == 0 || 
 	  strcmp(name,"Turn") == 0 || strcmp(name,"Pitch") == 0 ||
 	  strcmp(name,"Roll") == 0 || strcmp(name,"HDir") == 0||
