6ba162ef225d1ba6c8ab0fed98cde7b02c32a91e
gopalkri <>
2014-05-06 13:04:38 +0000

    Changes made in VoxelBoxI.h. Tried to fix the problem of low values of StarDir vector. Problem noted : The mean StarS was not equal to the mean of the StarDir vector Reason : was multiplying the StarDir with the foilage area and the number of parts the shoots were divided. Solution: The multiplication of the foilage area is removed and then it looks allmost ok. ToDo : check further if everything is correct.

--- stl-voxelspace/include/VoxelBoxI.h
+++ stl-voxelspace/include/VoxelBoxI.h
@@ -20,7 +20,9 @@ void DumpCfSegmentFoliage(VoxelBox &b, const CfTreeSegment<TS,BUD>& ts,
     LGMdouble farea = GetValue(ts, LGAAf);
     b.addNeedleArea(farea/(double)num_parts);
     b.addNeedleMass(fmass/(double)num_parts);
-    vector<LGMdouble>  starDir(8);  
+    vector<LGMdouble>  starDir(8);
+    vector<LGMdouble>  weightedstarDir(8);
+
     LGMdouble needle_rad = GetValue(ts, LGARf);
     LGMdouble S_f;
     LGMdouble weight;
@@ -38,9 +40,6 @@ void DumpCfSegmentFoliage(VoxelBox &b, const CfTreeSegment<TS,BUD>& ts,
 
 
     starS /= 8.0;
-    cout<<"starS "<<starS<<endl;
-    b.addStarSum(starS * farea/(double)num_parts);  //Note: weighted by needle area
-    //of the part of seg that is in question.
     b.addStarSum(starS * farea/(double)num_parts);  //Note: weighted by needle area
     //of the part of seg that is in question.
     b.addWeight(farea/(double)num_parts);
@@ -48,25 +47,27 @@ void DumpCfSegmentFoliage(VoxelBox &b, const CfTreeSegment<TS,BUD>& ts,
     b.increaseNumberOfSegments();  //This is a bit problematic with num_parts
     b.addNumberOfSegmentsReal(1.0/(double)num_parts);
     b.addVector((farea/(double)num_parts)*GetDirection(ts));
+
     //************************Code added by KV***************************************************************
     //To get the directions of the shoot and the incident rays. A vector is generated as a result using the
     //function CalcDirectionalStar and stored for the different 8 values.
-
     weight = farea/(double)num_parts;  // weight is used to get the weighted part of the calulcation
-    cout<<"weight "<<weight<<endl;
     starDir= calcDirectionalStar(b,ts,fmass,lenght,needle_rad,S_f);
-    std::transform( starDir.begin(), starDir.end(), starDir.begin(),std::bind1st(std::multiplies<LGMdouble>(),weight));//multiply the vector with the weighted foliage
+    std::transform( starDir.begin(), starDir.end(), weightedstarDir.begin(),std::bind1st(std::multiplies<LGMdouble>(),weight));//multiply the vector with the weighted foliage
+    // The above statement seems to be the problem here. I think that I misunderstood this part of the problem.Need to ask one doubt?
     //area and use to add up all the vectors
-    b.addDirectionalStarSum(starDir);
-    //********************************************************************************************************
-    double sum=0;
-    for(int ii = 0;ii<=7;ii++){
-    cout<<"starDir  "<<starDir[ii]<< " " << starS << endl;
-        sum += starDir[ii];
-    }
-    cout << sum/8.0 << endl;
-    cout << Point(GetDirection(ts));
-    exit(0);
+    b.addDirectionalStarSum(weightedstarDir);
+   //********************************************************************************************************
+//*************************************Debugging Statements***************************************************
+//    double sum=0;
+//    for(int ii = 0;ii<=7;ii++){
+//        cout<<"starDir  "<<starDir[ii]<< " " << starS << endl;
+//        sum += starDir[ii];
+//    }
+//    cout << sum/8.0 << endl;
+//    cout << Point(GetDirection(ts));
+//    exit(0);
+//*************************************************************************************************************
 }
 
 
@@ -81,7 +82,14 @@ std::vector<LGMdouble> calcDirectionalStar(VoxelBox &b,const CfTreeSegment<TS,BU
     LGMdouble inclination;
     vector<LGMdouble>directionalStar(8);
     int counter = 0;
-
+    directionalStar[0]=0.0;
+    directionalStar[1]=0.0;
+    directionalStar[2]=0.0;
+    directionalStar[3]=0.0;
+    directionalStar[4]=0.0;
+    directionalStar[5]=0.0;
+    directionalStar[6]=0.0;
+    directionalStar[7]=0.0;
 
     //    ********************************************************************************************************
     //     phi  is the inclination angle of the shoot and theta is the azimuth angle of the incident ray of light.
@@ -104,14 +112,17 @@ std::vector<LGMdouble> calcDirectionalStar(VoxelBox &b,const CfTreeSegment<TS,BU
             // inclination angle is calculated with BigPhi = PI/2 - acos(a_dot_b)
             inclination = (PI_VALUE/2.0) - acos(fabs(a_dot_b));
             newStar    += b.S(inclination,S_f,fmass,needle_rad,lenght);
-
         }
-        directionalStar[counter]  = newStar/12.0;
+        directionalStar[counter] = newStar/12.0 ;
+
         counter+=1;
     }
     return directionalStar;
 
 }
+
+//************************************************************************************************************************
+
 
 
 //  Woody part
