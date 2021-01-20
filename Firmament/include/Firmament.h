2fa2b2700ca66a5be0b8dded10151d517395542f
jarip <jari.perttunen@luke.fi>
2021-01-20 17:37:20 +0200

    doxygen comments for Firmament class.

--- Firmament/include/Firmament.h
+++ Firmament/include/Firmament.h
@@ -19,155 +19,187 @@ using namespace cxxadt;
 
 #define NUM_OF_AZIM 24
 #define NUM_OF_INCL 9
-/*=====================================================================================*/
-//Class Firmament implements the radiation coming from the sky.
-//Both direct (=sun) and diffuse radiation is dealt with separately.
-//The intensity of both radiation types can be set (by defining intensity of
-//direct and diffuse radiation on a horizontal plane).
-//Initially (constructor) direct radiation is set to 0 and diffuse radiation is set
-// to 1200 MJ/m2 (from the whole sky).  This corresponds to the radiation sum
-//(direct+diffuse) during the growing period in southern Finland (Stenberg).
-//
-// Setting direct radiation to 0 and defining all radiation as diffuse is the way
-// to deal with radiation distribution of the sky over a prolonged period.
-// If you want to consider momentary values update direct and diffuse radiation
-// components according to your needs.
-//
-//1) Direct radiation: sun's position and the intensity of radiation can be both set
-//   and queried. Initially intensity = 0, direction = (0,0,0)
-//
-//2) Diffuse radiation. The upper hemisphere is divided into sectors in the follwing way:
-//  2.1) number of sectors = (number of inclinations)*(number of azimuths) + 1
-//  2.2) area of zenith segment of the sphere = 2*PI/numOfSectors => angle of
-//       the zenith segment
-//  2.3) width of inclination zone = (PI/2 - angle of zenith segment)/(num of incls)
-//  2.4) The (number of inclinations)*(number of azimuths) sectors are divided
-//       between (number of inclinations)  zones so that all sectors have as equal solid
-//       angle (=area on the surface of the unit sphere) as possible. The variable
-//       number of azimuths defines the mean number of sectors the inclination
-//       zones contain.
-//
-//  2.5) The radiation coming from each sector is stored as well as the inclinations
-//       and azimuths & some other radiation values
-//  2.6) The flux of diffuse radiation coming from the sky is 
-//      according to J. Ross 1981 The radiation regime and architecture of plant
-//      stands, Dr W. Junk Publishers Eq. II.2.4a which defines the zonal brightness
-//      of the standard overcast sky. The brigthness is thus changing only with
-//      inclination inc according to the following equation:
-//      d(inc)=(6/7)*(1 + 2 * sin(inc)) / (2*PI)
-//      This formula is scaled so that the irradiance caused by it on the horizontal
-//      plane equals one, that is,
-//      integral(from 0, to PI/2) d(x) * sin(x) * 2PI*cos(x)*dx = 1
-//      (2PI*cos(x) comes from geometry, da = 2PI*cos(inc) on the surface of the unit
-//      sphere) If we know the irradiance on the horizontal plane, Q, we have thus the
-//      brightness of the sky as Q * d(inc)
+/*! \class Firmament 
+    \brief Implements the radiation coming from the sky.
+     
+     Both direct (=sun) and diffuse radiation is dealt with separately.
+     The intensity of both radiation types can be set (by defining intensity of
+     direct and diffuse radiation on a horizontal plane).
+     Initially (constructor) direct radiation is set to 0 and diffuse radiation is set
+     to 1200 MJ/m2 (from the whole sky).  This corresponds to the radiation sum
+     (direct+diffuse) during the growing period in southern Finland (Stenberg).
+     Setting direct radiation to 0 and defining all radiation as diffuse is the way
+     to deal with radiation distribution of the sky over a prolonged period.
+     If you want to consider momentary values update direct and diffuse radiation
+     components according to your needs.
 
+1) Direct radiation: sun's position and the intensity of radiation can be both set
+   and queried. Initially intensity = 0, direction = (0,0,0)
 
+2) Diffuse radiation. The upper hemisphere is divided into sectors in the follwing way:
+2.1) number of sectors = (number of inclinations)*(number of azimuths) + 1
+2.2) area of zenith segment of the sphere = 2*PI/numOfSectors => angle of
+       the zenith segment
+2.3) width of inclination zone = (PI/2 - angle of zenith segment)/(num of incls)
+2.4) The (number of inclinations)*(number of azimuths) sectors are divided
+       between (number of inclinations)  zones so that all sectors have as equal solid
+       angle (=area on the surface of the unit sphere) as possible. The variable
+       number of azimuths defines the mean number of sectors the inclination
+       zones contain.
+
+2.5) The radiation coming from each sector is stored as well as the inclinations
+      and azimuths & some other radiation values
+2.6) The flux of diffuse radiation coming from the sky is 
+      according to J. Ross 1981 The radiation regime and architecture of plant
+      stands, Dr W. Junk Publishers Eq. II.2.4a which defines the zonal brightness
+      of the standard overcast sky. The brigthness is thus changing only with
+      inclination inc according to the following equation:
+      d(inc)=(6/7)*(1 + 2 * sin(inc)) / (2*PI)
+      This formula is scaled so that the irradiance caused by it on the horizontal
+      plane equals one, that is,
+      integral(from 0, to PI/2) d(x) * sin(x) * 2PI*cos(x)*dx = 1
+      (2PI*cos(x) comes from geometry, da = 2PI*cos(inc) on the surface of the unit
+      sphere) If we know the irradiance on the horizontal plane, Q, we have thus the
+      brightness of the sky as Q * d(inc)
+
+*/
+  
 //Public methods:
 //(see Firmament.cc for more precise definition of input and output of methods)
-/*
-1.  Firmament(int no_incl = NUM_OF_INCL, int no_azim = NUM_OF_AZIM )
-     Arguments: 
-     in: 'no_incl', number of sectors dividing the sky horizontally.
-         'no_azim', number of sectors dividing the sky vertically.
-         'rad_plane=1200' 1200 MJ (PAR)/m2 equal to radiation sum (direct+diffuse) 
+/*! 
+1. \memberof Firmament  Firmament(int no_incl = NUM_OF_INCL, int no_azim = NUM_OF_AZIM )    
+         \param no_incl number of sectors dividing the sky horizontally.
+         \param no_azim number of sectors dividing the sky vertically.
+         \param rad_plane=1200 1200 MJ (PAR)/m2 equal to radiation sum (direct+diffuse) 
                           during the growing peariod in Southern Finlandxs 
 
-2.   void setDiffuseRadiation(const double rad)
-     Set the diffuse radiation (rad = radiation coming to horiz. plane)
-
-3.   void setDirectRadiation(const double rad)
+2.    \memberof Firmament void setDiffuseRadiation(const double rad)
+        \param rad radiation coming to horiz. plane
+        \par 
+        Set the diffuse radiation (rad = radiation coming to horiz. plane)    
+3.   \memberof Firmament void setDirectRadiation(const double rad)
+          \param rad
+     \par 
      Set the direct radiation (rad = radiation coming to horiz. plane)
 
-4.   void setSunPosition(const vector<double>& v)
-     v = direction vector
-
-5.   vector<double>  getSunPosition()
+4.   \memberof Firmament void setSunPosition(const vector<double>& v)
+      \param v direction vector
+     
+5.   \memberof vector<double>  getSunPosition()
+     \par 
      Get the direction vector
-
-6.  MJ directRadiation(vector<double>& direction)
+    
+6.  \memberof Firmament MJ directRadiation(vector<double>& direction)
+    \param direction
+    \par 
     Get the intensity (on plane perpendicular to direction vector) and
     the direction of the sun
 
-7.  MJ diffuseRadiationSum( const vector<double>& v)
-    Get intensity of diffuse radiation (on plane perpendicular to direction vector)
-    from sector input v is poining to.
+7.  \memberof Firmament MJ diffuseRadiationSum( const vector<double>& v)
+    \param v
+    \par 
+     Get intensity of diffuse radiation (on plane perpendicular to direction vector)
+     from sector input v is poining to.
 
-8.  int numberOfRegions()
+8.  \memberof Firmament int numberOfRegions()
+    \par 
     Get number of sectors.
 
-9.  MJ diffuseRegionRadiationSum(int n, vector<double>& direction)
+9.  \memberof Firmament MJ diffuseRegionRadiationSum(int n, vector<double>& direction)
+    \param n
+    \param direction
+    \par 
     Get intensity of diffuse radiation (on plane perpendicular to direction vector)
     from sector no n (numbering starts from horizont) and direction of the sector.
 
-10.  MJ diffuseHalfRegionRadiationSum(int n, vector<double>& direction)
-     As 9 but the other half of the upper hemisphere gives no radiation.
+10.  \memberof Firmament MJ diffuseHalfRegionRadiationSum(int n, vector<double>& direction)
+     \param n
+     \param direction
+     \par 
+      As 9 but the other half of the upper hemisphere gives no radiation.
 
-11.  MJ directHalfRegionRadiationSum(vector<double>& direction)
+11. \memberof Firmament MJ directHalfRegionRadiationSum(vector<double>& direction)
+    \param direction
+     \par 
      As 10 but direct radiation
 
-12.  MJ diffusePlaneSensor(void)
-     Intensity of diffuse radiation on a horizontal plane.
+12.  \memberof Firmament MJ diffusePlaneSensor(void)
+     \par 
+      Intensity of diffuse radiation on a horizontal plane.
 
-13.   MJ diffuseBallSensor(void)
-       Intensity of diffuse radiation as recorded by a ball sensor.
+13.  \memberof Firmament MJ diffuseBallSensor(void)
+      \par 
+      Intensity of diffuse radiation as recorded by a ball sensor.
 
-14.   MJ diffuseForestRegionRadiationSum(int n, float z, float x,  float la, float ke,
+14.   \memberof Firmament MJ diffuseForestRegionRadiationSum(int n, float z, float x,  float la, float ke,
                                          float H, float Hc,vector<double>& direction,
                                          double density)
-       As 9. but the point where the radiation is coming to is in a forest.
-       INPUT:
-          n        number of region
-	  z        height of the point from ground, m
-          x        distance of the point from the tree stem,  m
-	  la       needle area (total area) per tree (= sf * Wf),   m2
-	  ke       extinction cofficient  (= 0.14 for Scots pine),  unitless
-	  H        height of tree (h. of stand),  m
-	  Hc       height of the crown base of the tree (stand), m
-          density  Density of the stand (trees/ha)
-        OUTPUT:
+       \par As 9. but the point where the radiation is coming to is in a forest.
+       \param   n        number of region
+       \param	z        height of the point from ground, m
+       \param   x        distance of the point from the tree stem,  m
+       \param  la       needle area (total area) per tree (= sf * Wf),   m2
+       \param  ke       extinction cofficient  (= 0.14 for Scots pine),  unitless
+       \param  H        height of tree (h. of stand),  m
+       \param  Hc       height of the crown base of the tree (stand), m
+       \param  density  Density of the stand (trees/ha)
+        \par OUTPUT:
          The annual radiation sum (MJ) from the nth region of the
          firmament as shaded by the neighboring stand
 	 direction  the direction of nth region
          If n < 0 or n > total number of regions - 1, return -1.0
         (see comments in program code for more information about this method)
 
-15.  void outDiff()
-       Prints out ( cout << ) the diffuse radiation intensity sector by sector.
+15. \memberof Firmament void outDiff()
+       \par Prints out ( cout << ) the diffuse radiation intensity sector by sector.
 
-16.  void outAz() 
-      Prints out azimuths of sector mid points in all inclination zones.
+16.  \memberof Firmament void outAz() 
+      \par Prints out azimuths of sector mid points in all inclination zones.
 
-17. double getInclination(int n)
+17. \memberof Firmament double getInclination(int n)
+      \param n
+      \par 
       Returns the inclination (readians, 0...PI/2) of sector (middlepoint) n.
       Returns -1.0, if n is out of bounds.
 
-18. double getAzimuth(int n)
+18. \memberof Firmament double getAzimuth(int n)
+     \param n
+      \par 
       Returns the azimuth (radians, 0...2PI) of sector (middlepoint) n.
       Returns -1.0, if n is out of bounds.
 
-19. int getInclinationIndex(int n)
-     Returns the index of inclination of sector n in TMatrix<double> diffuseRad 
-     where radiation is stored. Return -1 if n is out of bounds.
+19. \memberof Firmament int getInclinationIndex(int n)
+    \param n
+     \par 
+     Returns the index of inclination of sector n in TMatrix<double> diffuseRad  where radiation is stored. Return -1 if n is out of bounds.
 
-20. int getAzimuthIndex(int n)
-    As getInclinationIndex but for azimuth.
+20. \memberof Firmament int getAzimuthIndex(int n)
+    \param n
+    \par 
+     As getInclinationIndex but for azimuth.
 
-21. double getSectorArea(int n)
+21. \memberof Firmament double getSectorArea(int n)
+    \param n
+    \par 
     returns area of sectors in each inclination class
     Returns -1, if n out of bounds.
 
-22. int getNoOfInclinations()
+22. \memberof Firmament int getNoOfInclinations()
+    \par 
     Returns number of inclination classes.
 
-23. PositionVector getDirection(int n)const
-    Returns direction to sector n
+23. \memberof Firmament PositionVector getDirection(int n)const
+     \param n
+     \par 
+      Returns direction to sector n
 
-24. void outInclinations()
-    Prints out inclinations of inclination classes
+24. \memberof Firmament void outInclinations()
+    \par 
+     Prints out inclinations of inclination classes
 
-25. vector<pair<double,double> > getIncAz()
-    Returnn inclinations and azimuths of all sectors
+25. \memberof Firmament vector<pair<double,double> > getIncAz()
+    \par 
+     Return inclinations and azimuths of all sectors
 =====================================================================================
 */
 
