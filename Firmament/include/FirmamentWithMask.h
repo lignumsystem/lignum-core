19db6c32bf3c6edf42e83c067dc235859733f34b
jarip <jari.perttunen@luke.fi>
2021-01-21 12:47:53 +0200

    Created doxygen comments.

--- Firmament/include/FirmamentWithMask.h
+++ Firmament/include/FirmamentWithMask.h
@@ -29,26 +29,27 @@ namespace sky{
   private:
     int iter;
   };
-//This class tries to model a gap in a forest.
-//The gaps are often (?) analyzed by foresters
-//by taking photograhs  with fish-eye lenses.
-//The size and the form of the gap is further
-//analyzed by marking the path of the sun 
-//in the photograph and the effect of the direct and 
-//diffuse radiation can be estimated (somehow).
-//The idea of this class is simply to mimic such
-//fish-eye photograph and to model the effect
-//of the blocked incoming radiation, direct and diffuse.
-//Thus the name FirmamentWithMask.
-//Ideally the user could define the gap (i.e., the photograph)
-//in a file, though in a somewhat simplified manner.    
-//Methods:
-//1. readMaskFile
-//   Read the mask file and adjust radiation
-//2.  setMask
-//   This naive approach: in a file for each inclination 
-//   the percentage of remaining radiation is given
-//   100% means gap, less than 100% means obstacle.
+
+//!FirmamentWithMask models a gap in a forest.
+//!The gaps are often analyzed by foresters
+//!by taking photograhs  with fish-eye lenses.
+//!The size and the form of the gap is further
+//!analyzed by marking the path of the sun 
+//!in the photograph and the effect of the direct and 
+//!diffuse radiation can be estimated (somehow).
+//!The idea of this class is simply to mimic such
+//!fish-eye photograph and to model the effect
+//!of the blocked incoming radiation, direct and diffuse.
+//!Thus the name FirmamentWithMask.
+//!Ideally the user could define the gap (i.e., the photograph)
+//!in a file, though in a somewhat simplified manner.    
+//!Methods:
+//!1. readMaskFile
+//!   Read the mask file and adjust radiation
+//!2.  setMask
+//!   This naive approach: in a file for each inclination 
+//!   the percentage of remaining radiation is given
+//!   100% means gap, less than 100% means obstacle.
 class FirmamentWithMask:public Firmament{
 public: 
   FirmamentWithMask(int no_incl = NUM_OF_INCL/*9*/, int no_azim = NUM_OF_AZIM /*24*/,double rad_plane=1200.0);
