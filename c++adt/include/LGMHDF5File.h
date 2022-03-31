caa5a0152c77d641dd0c3f245680e6055f4a00fc
jariperttunen <jari.perttunen@luke.fi>
2022-03-31 09:28:04 +0300

    Wrapped LGMHDF5File to cxxadt namespace

--- c++adt/include/LGMHDF5File.h
+++ c++adt/include/LGMHDF5File.h
@@ -19,83 +19,85 @@
 #include <H5Cpp.h>
 using namespace H5;
 using namespace std;
-using namespace cxxadt; 
-// DataSpace RANKS for 2D and 3D arrays
-const int DSPACE_RANK3 = 3; ///< DataSpace rank 3D array
-const int DSPACE_RANK2 = 2; ///< DataSpace rank 2D array (matrix) 
-/// \brief Create HDF5 file from 2D or 3D data arrays.
-///
-/// After Lignum simulation all data is assumed toi be in 2D or 3D arrays. LGMHDF5File can
-/// then take that data and create a single HDF5 file for future analysis with R, python
-/// or other HDF5 compatible tool. Thus LGMHDF5 does not (yet) have methods to read HDF5 files.
-///
-/// The datasets will be of NATIVE_DOUBLE type defined in HDF5. Thus user provides 2D or 3D arrays
-/// of *double* type. Future versions may support HDF5 compound types. Column names are implemented
-/// as HDF5 attributes.
-///
-/// The 3D data should be organised as `array[years][rows][columns]` where the
-/// `rows` are Lignum trees, `columns` are data for a tree and `years` are simulation years.
-/// For example `array[i][j]` retrieves data row for the `j`:th tree on the year ì`.
-/// The 2D data is up to user. 
-class LGMHDF5File{
-public:
-  /// Constructor prepares for datasets by creating HDF5 file. Previous existing file will be truncated.
-  /// \exception DataSpaceIException
-  /// \sa hdf5_file
-  LGMHDF5File(const string& file_name);
-  /// Close the H5File `hdf5_file`. \sa hdf5_file
-  ~LGMHDF5File();
-  int createDataSet(const string& dataset_name, int years, int rows, int cols, const TMatrix3D<double>& data);
-  /// Create 3D array DataSet by giving explicitely the 3 dimensions and `data`
-  /// \param dataset_name Name of the dataset
-  /// \param years Years dimension
-  /// \param rows Rows (trees) dimension
-  /// \param cols Columns (data) dimension
-  /// \param data The 3D array of type *double*
-  /// \return -1 if error 0 otherwise
-  /// \exception DataSetIException
-  /// \note Technically 3D arrray `data` must be void*. It will be converted to
-  /// 3D array of NATIVE_DOUBLE HDF5 dataset of given dimensions. 
-  /// \sa writeDataSet
-  int createDataSet(const string& dataset_name, int years, int rows, int cols, void* data);
-  /// Create 2D array DataSet by giving explicitely the 2 dimensions and the `data`
-  /// \param dataset_name Name of the dataset
-  /// \param rows Rows dimension
-  /// \param cols Columns dimension
-  /// \param data The 2D array of type *double*
-  /// \return -1 if error 0 otherwise
-  /// \exception DataSetIException
-  /// \note Technically 2D arrray `data` must be void*. It will be converted to
-  /// 2D array of NATIVE_DOUBLE  dataset of given dimensions.  
-  /// \sa writeDataSet
-  int createDataSet(const string& dataset_name, int rows, int cols, void* data);
-  /// Create column names as HDF5 attribute. Column names can be of variable length strings.
-  /// \param dset_name DataSet name for  which the column names will be given 
-  /// \param attr_name Attribute name for the column names 
-  /// \param col_names vector of column names 
-  /// \return -1 if error 0 otherwise
-  /// \exception AttributeIException
-  /// \note HDF5 C++ interface does not (yet) support dimension scales which would be a more natural way to describe
-  /// the meaning of each N dimensional array dimension.
-  /// \warning You can try to use UTF-8 characters in column names (including Scandinavian alphabet) but
-  /// not recommended. HDF5 does not *enforce* UTF-8. May be OK on one platform but not on some others. 
-  int createColumnNames(const string& dset_name, const string& attr_name,const vector<string>& col_names);
-  /// Retrieve object names known to `hdf5_file`
-  /// \return vector of object names 
-  /// \note Object names include groups, dataspaces, datasets etc.
-  /// \sa hdf5_file
-  vector<string> getObjectNames();
-  /// Close the H5File `hdf5_file`. \sa hdf5_file
-  void close();
-protected:
-  /// Write the dataset to dataspace
-  /// \param dset DataSet 
-  /// \param data Lignum simulation data
-  /// \return -1 if error 0 otherwise
-  /// \exception DataSetIException
-  /// \sa createDataSet
-  int writeDataSet(const DataSet& dset, void* data);
-private:
-  H5File hdf5_file; /// HDF5 file containing simulation results
-};
+using namespace cxxadt;
+namespace cxxadt{
+  // DataSpace RANKS for 2D and 3D arrays
+  const int DSPACE_RANK3 = 3; ///< DataSpace rank 3D array
+  const int DSPACE_RANK2 = 2; ///< DataSpace rank 2D array (matrix) 
+  /// \brief Create HDF5 file from 2D or 3D data arrays.
+  ///
+  /// After Lignum simulation all data is assumed toi be in 2D or 3D arrays. LGMHDF5File can
+  /// then take that data and create a single HDF5 file for future analysis with R, python
+  /// or other HDF5 compatible tool. Thus LGMHDF5 does not (yet) have methods to read HDF5 files.
+  ///
+  /// The datasets will be of NATIVE_DOUBLE type defined in HDF5. Thus user provides 2D or 3D arrays
+  /// of *double* type. Future versions may support HDF5 compound types. Column names are implemented
+  /// as HDF5 attributes.
+  ///
+  /// The 3D data should be organised as `array[years][rows][columns]` where the
+  /// `rows` are Lignum trees, `columns` are data for a tree and `years` are simulation years.
+  /// For example `array[i][j]` retrieves data row for the `j`:th tree on the year ì`.
+  /// The 2D data is up to user. 
+  class LGMHDF5File{
+  public:
+    /// Constructor prepares for datasets by creating HDF5 file. Previous existing file will be truncated.
+    /// \exception DataSpaceIException
+    /// \sa hdf5_file
+    LGMHDF5File(const string& file_name);
+    /// Close the H5File `hdf5_file`. \sa hdf5_file
+    ~LGMHDF5File();
+    int createDataSet(const string& dataset_name, int years, int rows, int cols, const TMatrix3D<double>& data);
+    /// Create 3D array DataSet by giving explicitely the 3 dimensions and `data`
+    /// \param dataset_name Name of the dataset
+    /// \param years Years dimension
+    /// \param rows Rows (trees) dimension
+    /// \param cols Columns (data) dimension
+    /// \param data The 3D array of type *double*
+    /// \return -1 if error 0 otherwise
+    /// \exception DataSetIException
+    /// \note Technically 3D arrray `data` must be void*. It will be converted to
+    /// 3D array of NATIVE_DOUBLE HDF5 dataset of given dimensions. 
+    /// \sa writeDataSet
+    int createDataSet(const string& dataset_name, int years, int rows, int cols, void* data);
+    /// Create 2D array DataSet by giving explicitely the 2 dimensions and the `data`
+    /// \param dataset_name Name of the dataset
+    /// \param rows Rows dimension
+    /// \param cols Columns dimension
+    /// \param data The 2D array of type *double*
+    /// \return -1 if error 0 otherwise
+    /// \exception DataSetIException
+    /// \note Technically 2D arrray `data` must be void*. It will be converted to
+    /// 2D array of NATIVE_DOUBLE  dataset of given dimensions.  
+    /// \sa writeDataSet
+    int createDataSet(const string& dataset_name, int rows, int cols, void* data);
+    /// Create column names as HDF5 attribute. Column names can be of variable length strings.
+    /// \param dset_name DataSet name for  which the column names will be given 
+    /// \param attr_name Attribute name for the column names 
+    /// \param col_names vector of column names 
+    /// \return -1 if error 0 otherwise
+    /// \exception AttributeIException
+    /// \note HDF5 C++ interface does not (yet) support dimension scales which would be a more natural way to describe
+    /// the meaning of each N dimensional array dimension.
+    /// \warning You can try to use UTF-8 characters in column names (including Scandinavian alphabet) but
+    /// not recommended. HDF5 does not *enforce* UTF-8. May be OK on one platform but not on some others. 
+    int createColumnNames(const string& dset_name, const string& attr_name,const vector<string>& col_names);
+    /// Retrieve object names known to `hdf5_file`
+    /// \return vector of object names 
+    /// \note Object names include groups, dataspaces, datasets etc.
+    /// \sa hdf5_file
+    vector<string> getObjectNames();
+    /// Close the H5File `hdf5_file`. \sa hdf5_file
+    void close();
+  protected:
+    /// Write the dataset to dataspace
+    /// \param dset DataSet 
+    /// \param data Lignum simulation data
+    /// \return -1 if error 0 otherwise
+    /// \exception DataSetIException
+    /// \sa createDataSet
+    int writeDataSet(const DataSet& dset, void* data);
+  private:
+    H5File hdf5_file; /// HDF5 file containing simulation results
+  };
+}//namepace cxxadt
 #endif
--- c++adt/src/LGMHDF5File.cc
+++ c++adt/src/LGMHDF5File.cc
@@ -3,129 +3,129 @@
 /// \brief LGMHDF5File implementation with examples to save Lignum simulations to HDF5 file.
 #include <LGMHDF5File.h>
 
+namespace cxxadt{
+  LGMHDF5File::LGMHDF5File(const string& file_name)
+    :hdf5_file(file_name,H5F_ACC_TRUNC)
+  {
+  }
 
-LGMHDF5File::LGMHDF5File(const string& file_name)
-  :hdf5_file(file_name,H5F_ACC_TRUNC)
-{
-}
+  LGMHDF5File::~LGMHDF5File()
+  {
+    hdf5_file.close();
+  }
 
-LGMHDF5File::~LGMHDF5File()
-{
-  hdf5_file.close();
-}
-
-int LGMHDF5File::createDataSet(const string& dataset_name, int years, int rows, int cols, const TMatrix3D<double>& data)
-{
-  double data_array3D[years][rows][cols];
-  for (int i = 0; i < years; i++){
-    for (int j = 0; j < rows; j++){
-      for (int k=0; k < cols; k++){
-	data_array3D[i][j][k] = data[i][j][k];
+  int LGMHDF5File::createDataSet(const string& dataset_name, int years, int rows, int cols, const TMatrix3D<double>& data)
+  {
+    double data_array3D[years][rows][cols];
+    for (int i = 0; i < years; i++){
+      for (int j = 0; j < rows; j++){
+	for (int k=0; k < cols; k++){
+	  data_array3D[i][j][k] = data[i][j][k];
+	}
       }
     }
+    return createDataSet(dataset_name,years,rows,cols,data_array3D);
   }
-  return createDataSet(dataset_name,years,rows,cols,data_array3D);
-}
 
-int LGMHDF5File::createDataSet(const string& dataset_name, int years, int rows, int cols, void* data)
-{
-  FloatType datatype(PredType::NATIVE_DOUBLE);
-  try{
-    Exception::dontPrint();
-    /// DataSpace needs dimensions and rank
-    hsize_t dspace_dims[DSPACE_RANK3];
-    dspace_dims[0] = years;
-    dspace_dims[1] = rows;
-    dspace_dims[2] = cols;
-    DataSpace dataspace(DSPACE_RANK3,dspace_dims);
-    DataSet dset = hdf5_file.createDataSet(dataset_name,datatype,dataspace);
-    writeDataSet(dset, data);
-  }
-  catch (DataSetIException error){
-    error.printErrorStack();
-    return -1;
-  }
-  return 0;
-}
-
-int LGMHDF5File::createDataSet(const string& dataset_name, int rows, int cols, void* data)
-{
-  FloatType datatype(PredType::NATIVE_DOUBLE);
-  try{
-    Exception::dontPrint();
-    /// DataSpace needs dimensions and rank
-    hsize_t dspace_dims[DSPACE_RANK2];
-    dspace_dims[0] = rows;
-    dspace_dims[1] = cols;
-    DataSpace dataspace(DSPACE_RANK2,dspace_dims);
-    DataSet dset = hdf5_file.createDataSet(dataset_name,datatype,dataspace);
-    writeDataSet(dset, data);
-  }
-  catch (DataSetIException error){
-    error.printErrorStack();
-    return -1;
-  }
-  return 0;
-}
-
-int LGMHDF5File::createColumnNames(const string& dset_name, const string& attr_name, const vector<string>& col_names)
-{
-  ///Use variable length string type
-  StrType str_type(PredType::C_S1, H5T_VARIABLE);
-  const int rank = 1;
-  hsize_t dims[rank];
-  ///Reserve space for the string vector
-  dims[0]=col_names.size();
-  try{
-    Exception::dontPrint();
-    ///Create atribute dataspace
-    DataSpace attr_dspace(rank,dims);
-    DataSet dset = hdf5_file.openDataSet(dset_name);
-    Attribute attr(dset.createAttribute(attr_name,str_type,attr_dspace));
-    ///Convert `vector<string>& col_names ` to char* vector. Required by HDF5
-    vector<const char*> c_vector;
-    for (vector<std::string>::size_type i = 0; i < col_names.size(); i++){
-      c_vector.push_back(col_names[i].c_str());
+  int LGMHDF5File::createDataSet(const string& dataset_name, int years, int rows, int cols, void* data)
+  {
+    FloatType datatype(PredType::NATIVE_DOUBLE);
+    try{
+      Exception::dontPrint();
+      /// DataSpace needs dimensions and rank
+      hsize_t dspace_dims[DSPACE_RANK3];
+      dspace_dims[0] = years;
+      dspace_dims[1] = rows;
+      dspace_dims[2] = cols;
+      DataSpace dataspace(DSPACE_RANK3,dspace_dims);
+      DataSet dset = hdf5_file.createDataSet(dataset_name,datatype,dataspace);
+      writeDataSet(dset, data);
     }
-    ///Finally write string to attribute 
-    attr.write(str_type,c_vector.data());
-    dset.close();
+    catch (DataSetIException error){
+      error.printErrorStack();
+      return -1;
+    }
+    return 0;
   }
-  catch (AttributeIException error) {
-        error.printErrorStack();
-        return -1;
-  }
-  return 0;
-}
 
-int LGMHDF5File::writeDataSet(const DataSet& dset, void* data)
-{
-  try{
-    Exception::dontPrint();
-    dset.write(data,PredType::NATIVE_DOUBLE);
+  int LGMHDF5File::createDataSet(const string& dataset_name, int rows, int cols, void* data)
+  {
+    FloatType datatype(PredType::NATIVE_DOUBLE);
+    try{
+      Exception::dontPrint();
+      /// DataSpace needs dimensions and rank
+      hsize_t dspace_dims[DSPACE_RANK2];
+      dspace_dims[0] = rows;
+      dspace_dims[1] = cols;
+      DataSpace dataspace(DSPACE_RANK2,dspace_dims);
+      DataSet dset = hdf5_file.createDataSet(dataset_name,datatype,dataspace);
+      writeDataSet(dset, data);
+    }
+    catch (DataSetIException error){
+      error.printErrorStack();
+      return -1;
+    }
+    return 0;
   }
-  catch (DataSetIException error){
-    error.printErrorStack();
-    return -1;
+
+  int LGMHDF5File::createColumnNames(const string& dset_name, const string& attr_name, const vector<string>& col_names)
+  {
+    ///Use variable length string type
+    StrType str_type(PredType::C_S1, H5T_VARIABLE);
+    const int rank = 1;
+    hsize_t dims[rank];
+    ///Reserve space for the string vector
+    dims[0]=col_names.size();
+    try{
+      Exception::dontPrint();
+      ///Create atribute dataspace
+      DataSpace attr_dspace(rank,dims);
+      DataSet dset = hdf5_file.openDataSet(dset_name);
+      Attribute attr(dset.createAttribute(attr_name,str_type,attr_dspace));
+      ///Convert `vector<string>& col_names ` to char* vector. Required by HDF5
+      vector<const char*> c_vector;
+      for (vector<std::string>::size_type i = 0; i < col_names.size(); i++){
+	c_vector.push_back(col_names[i].c_str());
+      }
+      ///Finally write string to attribute 
+      attr.write(str_type,c_vector.data());
+      dset.close();
+    }
+    catch (AttributeIException error) {
+      error.printErrorStack();
+      return -1;
+    }
+    return 0;
   }
-  return 0;
-}
 
-vector<string> LGMHDF5File::getObjectNames()
-{
-  ssize_t nobj = hdf5_file.getNumObjs();
-  vector<string> v;
-  cout << "Num objects: " << nobj <<endl;
-  for (ssize_t i = 0; i < nobj; i++){
-    string str = hdf5_file.getObjnameByIdx(i);
-    v.push_back(str);
+  int LGMHDF5File::writeDataSet(const DataSet& dset, void* data)
+  {
+    try{
+      Exception::dontPrint();
+      dset.write(data,PredType::NATIVE_DOUBLE);
+    }
+    catch (DataSetIException error){
+      error.printErrorStack();
+      return -1;
+    }
+    return 0;
   }
-  return v;
-}
 
-void LGMHDF5File::close()
-{
-  hdf5_file.close();
-}
+  vector<string> LGMHDF5File::getObjectNames()
+  {
+    ssize_t nobj = hdf5_file.getNumObjs();
+    vector<string> v;
+    cout << "Num objects: " << nobj <<endl;
+    for (ssize_t i = 0; i < nobj; i++){
+      string str = hdf5_file.getObjnameByIdx(i);
+      v.push_back(str);
+    }
+    return v;
+  }
 
+  void LGMHDF5File::close()
+  {
+    hdf5_file.close();
+  }
+}//namespace cxxadt
 
