d263ab0e52b13e43ed4d17018543f550f193a2c8
jariperttunen <jari.perttunen@luke.fi>
2022-10-03 12:09:08 +0300

    Implementation of Parallelogram for "diamond shaped" leaves. The interface is meant to be the same as for Ellipse and Triangle and compatible with BroadLeaf.
    There constructors to create Parallelogram, to rezise and to do 3D rotations.

---
+++ c++adt/include/Parallelogram.h
@@ -0,0 +1,142 @@
+#ifndef PARALLELOGRAM_H
+#define PARALLELOGRAM_H
+#include <cstdlib>
+#include <algorithm>
+#include <Point.h>
+#include <PositionVector.h>
+using namespace cxxadt;
+/// \file Parallelogram.h
+/// \brief Leaf shape as Parallelogram.
+///
+/// Implements Parallelogram for a "diamond shape" leaf.
+/// The interface includes constructors, resizing the Parallelogram area,
+/// rotations in 3D space and detection of line (light ray) - Parallelogram (leaf) intersection.
+/// The inteface is compatible with BroadLeaf.
+/// \sa cxxadt::Triangle cxxadt::Ellipse Lignum::BroadLeaf
+/// \note Parts of the documentation contain basic LaTeX mathematical typesetting.
+/// If \f(\LaTeX \f) is installed Doxygen should automatically render those parts in the final document.
+/// MacPorts has \f(\TeX \f) Live package.
+
+namespace cxxadt{
+  ///\brief Parallelogram for diamond shaped leaves.
+  ///
+  ///Parallelogram area \f$A = |\overrightarrow{ab}||\overrightarrow{ad}\sin(\alpha) \f$  and \f$ \alpha = \overrightarrow{ab} \angle \overrightarrow{ad} \f$   
+  ///The Parallelogram normal \f$\vec{N} = \overrightarrow{ad} \times \overrightarrow{ab} \f$ pointing upwards and normalised.  
+  class Parallelogram{
+  public:
+    ///Create parallelogram with four clockwise corner points
+    ///\pre The points a,b,c and d are in clockwise order
+    ///\param a nadir point (leaf petiole end)
+    ///\param b 2nd point
+    ///\param c apex point
+    ///\param d 4th point
+    Parallelogram(const Point& a,const Point& b, const Point& c, const Point& d);
+    ///Create parallelogram with three clockwise corner points
+    ///\pre The points a,b,and d are in clockwise order
+    ///\param a nadir point (leaf petiole end)
+    ///\param b 2nd point
+    ///\param d 4th point
+    ///\post Point \f$ c =  \vec{b}+\overrightarrow{ad} \f$
+    Parallelogram(const Point& a,const Point& b,const Point& d);
+    ///Create parallelogram given intial (leaf) area.
+    ///\param A parallelogram area
+    ///\param a parallelogram nadir point (leaf petiole end)
+    ///\param dp direction (of a petiole) 
+    ///\param alpha The angle \f$ \alpha = \overrightarrow{ab} \angle \overrightarrow{ad} \f$
+    ///\param ratio length ratio \f$ |\overrightarrow{ab}| / |\overrightarrow{ad}| \f$ .  
+    ///\parblock
+    ///For symmetry the *ratio* is 1 which is probably the best choice for a leaf.
+    ///\endparblock
+    ///\param is_leaf_normal Default *true*.  
+    ///\parblock
+    ///If *true* set \f$ \vec{N} = \overrightarrow{d_p} \f$. If *false* set \f$\ \vec{N} \perp \overrightarrow{d_p} \f$ and pointing upwards.  
+    ///In the latter case \f$\overrightarrow{d_p} \f$ is e.g. petiole or bud direction.
+    ///\endparblock 
+    ///\post The angle \f$ \alpha = \overrightarrow{ab} \angle \overrightarrow{ad} \f$, symmetrically by \f$ (\alpha/2,-\alpha/2) \f$
+    ///away from \f$\overrightarrow{ac} \f$
+    ///\note The final orientation of Parallelogram (leaf) is implementation depended \sa pitch roll turn
+    Parallelogram(double A,const Point& a, const PositionVector& dp, double alpha, double ratio, bool is_leaf_normal=true);
+    ///Rotation around \f$\overrightarrow{bd} \f$ axis (pitch up or down) at nadir point a 
+    ///\param angle Magnitude of rotation in radians
+    ///\note Convenience method to Parallelogram::rotate
+    ///\sa rotate
+    Parallelogram& pitch(double angle);
+    //Rotation around \f$\overrightarrow{ac} \f$ axis (roll left or right) at nadir point *a*
+    ///\param angle Magnitude of rotation in radians
+    ///\note Convenience method to Paeallelogram::rotate
+    ///\sa rotate
+    Parallelogram& roll(double angle);
+    ///Rotation around Parallelogram normal \f$\vec{N} \f$ (turn left or right) at nadir point *a*
+    ///\param angle Magnitude of rotation in radians
+    ///\note Convenience method to Parallelogram::rotate
+    ///\sa rotate
+    Parallelogram& turn(double angle);
+    ///Resize Parallelogram. See Parallelogram 3rd constructor to set Parallelogram dimensions.
+    ///\param A New size
+    ///\post The nadir point *a* remains in place.
+    ///\note Convenience method to Parallelogram::resize
+    ///\sa resize 
+    Parallelogram& setArea(double A);
+    ///Parallelogram area
+    double getArea()const{return A;}
+    ///Parallelogram normal vector
+    PositionVector getNormal()const{return N;}
+    ///Center point
+    Point getCenterPoint()const;
+    ///Line (light beam) intersection with parallelogram (leaf)
+    ///The algorithm works in three steps.
+    ///-# Simple heuristics: For our purposes light beam never
+    ///points downwards. Check observation point is strictly below Parallelogram (leaf) maximum height,
+    ///-# Check if the (infinite) line intersects plane defined by the parallelogram.
+    ///-# Check if the plane intersection point is inside the parallelogram.
+    ///\param o Observer point (center point of a another leaf)
+    ///\param b The line (light beam) direction in 3D space
+    ///\return *true* if Parallelogram intersection, *false* if no intersction 
+    bool intersectShape(const Point& o, const PositionVector& b)const;
+    ///Find line (beam) - plane intersection point
+    ///\pre The intersection point with the line (beam) - plane (defined by the Parallelogram) must exist.
+    ///\param o the observation point
+    ///\param b the line direction in 3D space
+    ///\return The intersection point
+    ///\sa intersectShape
+    Point intersectionPoint(const Point& o, const PositionVector& b)const;
+    ///The angle in radians between vectors \f$\overrightarrow{ab} \f$ and \f$\overrightarrow{ad} \f$
+    double getAlpha()const;
+    ///Retrieve the nadir point *a*
+    ///\sa getB getC getD
+    Point getA()const{return a;}
+    Point getB()const{return b;}
+    Point getC()const{return c;}
+    Point getD()const{return d;}
+    ///Maximum Z-coordinate of corner points 
+    double getMaxZ()const;
+  private:
+    ///\pre The intersection point with  line (beam) - plane (defined by the Parallelogram) must exist.
+    ///\param p Point in the plane defined by the Parallelogram
+    ///\return *true* if *p* inside Parallelogram, *false* if not
+    ///\sa intersectShape
+    bool insideShape(const Point& p)const;
+    ///Resize parallelogram
+    ///\param A New area
+    ///\param alpha The angle in radians between vectors \f$\overrightarrow{ab} \f$ and \f$\overrightarrow{ad} \f$
+    ///\param ratio The ratio \f$|\overrightarrow{ab}| / |\overrightarrow{ad}| \f$
+    Parallelogram&  resize(double A,double alpha, double ratio);
+    ///Rotation in 3D space
+    ///\param p Point in 3D space 
+    ///\param d Direction vector
+    ///\param angle Magnitude of rotation in radian
+    ///\note Point *p* (most likely the nadir point *a*) and \f$|\vec{d}|  \f$ define the axis of rotation
+    Parallelogram& rotate(const Point& p, const PositionVector& d,double angle);
+    ///\f$|\overrightarrow{ab}|\f$ (vector length) 
+    double lengthAB(){return (a||b);}
+    ///\f$|\overrightarrow{ad}|\f$ (vector length)
+    double lengthAD(){return (a||d);}
+    Point a;///< The parallelogram nadir point where petiole will be attached 
+    Point b;///< Second corner point clockwise
+    Point c;///< Third (apex) corner point clockwise (\f$ c = \overrightarrow{ab} + \overrightarrow{ad} \f$)
+    Point d;///< Fourth corner point clockwise
+    double A;///< Parallelogram area 
+    PositionVector N;///< Parallelogram normal (\f$ \vec{N} = \overrightarrow{ad} \times \overrightarrow{ab},|\vec{N}| = 1 \f$)
+  };
+}//end namespace
+#endif
---
+++ c++adt/src/Parallelogram.cc
@@ -0,0 +1,330 @@
+#include <Parallelogram.h>
+/// \file Parallelogram.cc
+/// \brief Implementation of Parallelogram.
+
+namespace cxxadt{
+  
+  Parallelogram::Parallelogram(const Point& a1, const Point& b1, const Point& c1, const Point& d1)
+    :a(a1),b(b1),c(c1),d(d1)
+  
+  {
+    PositionVector a0(a);
+    PositionVector b0(b);
+    PositionVector d0(d);
+    PositionVector ab = b0-a0;
+    PositionVector ad = d0-a0;
+    double cos_alpha = Dot(ab,ad);
+    double alpha = acos(cos_alpha);
+    A = lengthAB()*lengthAD()*sin(alpha);
+    N = Cross(ad,ab);
+    N.normalize();
+  }
+
+  Parallelogram::Parallelogram(const Point& a1, const Point& b1, const Point& d1)
+    :a(a1),b(b1),d(d1)
+  {
+    PositionVector a0(a);
+    PositionVector b0(b);
+    PositionVector d0(d);
+    PositionVector ab = b0-a0;
+    PositionVector ad = d0-a0;
+    PositionVector c0 = b0+ad;
+    this->c=Point(c0);
+    double cos_alpha = Dot(ab,ad);
+    double alpha = acos(cos_alpha);
+    A = lengthAB()*lengthAD()*sin(alpha);
+    N = Cross(ad,ab);
+    N.normalize();
+  }
+
+  Parallelogram::Parallelogram(double A1,const Point& a1, const PositionVector& dp, double alpha, double ratio_ab, bool ndir)
+    :a(a1),A(A1)
+  {
+    PositionVector up(0,0,1);
+    PositionVector dir_tmp(dp);
+    PositionVector d1; 
+    PositionVector ab0;
+    PositionVector ad0;
+
+    /// **Find Parallelogram orientation**    
+    /// 1. Parameter *ndir* = *true*:   
+    /// Use parameter \f$ \overrightarrow{dp} \f$  as Parallelogram (leaf) normal
+    /// 2. Parameter *ndir* = *false*:
+    /// Interpret \f$ \overrightarrow{dp} \f$  as bud or leaf petiole direction (for example).   
+    /// Orient Parallelogram normal: \f$ \vec{N}  \perp \overrightarrow{dp} \f$:   
+    /// Find horizontal vector \f$ \overrightarrow{d_1} = \overrightarrow{dp} \times \overrightarrow{up} \f$.   
+    /// Find \f$ \vec{N} \f$ by rotating \f$ \overrightarrow{dp} \f$ around \f$ \overrightarrow{d_1}\f$ by \f$ -\pi/2 \f$.     
+    /// 3.  Find directions \f$ \hat{a_b} \f$ *and* \f$ \hat{a_d} \f$:     
+    /// Rotate \f$ \overrightarrow{dp} \f$ symmetrically \f$ (\alpha/2,-\alpha/2) \f$ around \f$\vec{N}\f$.
+    /// 4. Special cases:    
+    /// 4.1 Vector \f$ \overrightarrow{dp} \parallel \overrightarrow{up} \Rightarrow\f$ Use parameter *a1* as direction. We know the quadrant.    
+    /// 4.2 Parameter *a1* also on Z-axis \f$ \Rightarrow \f$ Choose X-axis as horixontal axis. No information to work with.
+   if (ndir == false){
+      double a1z_eps = (1.0-fabs(PositionVector(a1).normalize().getZ()));
+      double dirz_eps = (1.0-fabs(PositionVector(dp).normalize().getZ()));
+      //Very special case: no reference to use as direction
+      if (a1z_eps <= R_EPSILON && dirz_eps <= R_EPSILON){
+	//Choose reference (X-axis)
+	dir_tmp = PositionVector(1,0,0);
+      }
+      //Use *a1* as reference 
+      else if (dirz_eps <= R_EPSILON){
+	dir_tmp = PositionVector(a1);
+      }
+      /// \internal
+      /// \snippet{lineno} Parallelogram.cc Normal
+      // [Normal]
+      //Finding parallelogram normal
+      d1 = Cross(up,dir_tmp);
+      //Parallelogram normal d90 is pointing up perpendicular to **dir** and **up** vectors
+      PositionVector d90 = PositionVector(dir_tmp).normalize().rotate(Point(0,0,0),d1,-PI_VALUE/2.0);
+      // [Normal]
+      /// \endinternal
+      PositionVector tmp1(dir_tmp);
+      /// \internal
+      /// \snippet{lineno} Parallelogram.cc ab0ad0
+      // [ab0ad0]
+      //Unit vectors ab0 and ad0
+      //Left turn
+      ab0 = tmp1.normalize().rotate(Point(0,0,0),d90,alpha/2.0);
+      PositionVector tmp2(dir_tmp);
+      //Right turn
+      ad0 = tmp2.normalize().rotate(Point(0,0,0),d90,-alpha/2.0);
+      // [ab0ad0]
+      /// \endinternal
+    }
+    else{
+      //ndir=true then *dp* is interpreted as leaf normal
+      //User *a1* as the symmetry of rotation, at least we know the quadrant
+      PositionVector tmp1(dir_tmp);
+      //Left turn
+      ab0 = tmp1.normalize().rotate(Point(0,0,0),a1,alpha/2.0);
+      PositionVector tmp2(dir_tmp);
+      //Right turn
+      ad0 = tmp2.normalize().rotate(Point(0,0,0),a1,-alpha/2.0);
+    }
+    /// **Solution for** \f$ \overrightarrow{ab} \f$ **and** \f$ \overrightarrow{ad} \f$:  
+    /// 1. Denote \f$ \mathit{L1} = |\overrightarrow{ab}| \f$ and \f$\mathit{L2} = |\overrightarrow{ad}| \f$.  
+    /// 2. Let \f$ r = \mathit{L1}/\mathit{L2} \f$ then \f$ \mathit{L1} = r\mathit{L2} \f$ and \f$ \mathit{L2}=\mathit{L1}/r \f$.   
+    /// 3. \f$ A = \mathit{L1L2}\sin(\alpha) = \mathit{rL2^2}\sin(\alpha) \f$.   
+    /// \f$ \mathit{L2}^2 = A/(r\sin(\alpha)) \f$.         
+    /// \f$ \mathit{L2} = \sqrt{A/(r\sin(\alpha))} \f$.   
+    /// 4. Similarly \f$ \mathit{L1} = \sqrt{rA/\sin(\alpha)} \f$.   
+    /// \internal
+    /// \snippet{lineno} Parallelogram.cc L1L2
+    // [L1L2]
+    double L1 = sqrt(A1*ratio_ab/sin(alpha));
+    double L2 = sqrt(A1/(ratio_ab*sin(alpha)));
+    PositionVector ab = L1*ab0;
+    PositionVector ad = L2*ad0;
+    // [L1L2]
+    /// \endinternal
+    
+    // Point *b* is defined by **a**+**ab**
+    this->b = Point(PositionVector(a)+ab);
+    // Point *d* is defined by vector **a**+**ad**
+    this->d = Point(PositionVector(a)+ad);
+    // Point *c* is defined by vector **b**+**ad**
+    this->c = Point(PositionVector(b)+ad);
+    // Parallelogram (leaf) normal as Cross(**ad**,**ab**) makes the vector pointing up
+    N = Cross(ad,ab);
+    N.normalize();
+  }
+
+  Parallelogram& Parallelogram::rotate(const Point& p, const PositionVector& dir,double angle)
+  {
+    PositionVector av(this->a);
+    PositionVector bv(this->b);
+    PositionVector cv(this->c);
+    PositionVector dv(this->d);
+    av.rotate(p,dir,angle);
+    bv.rotate(p,dir,angle);
+    cv.rotate(p,dir,angle);
+    dv.rotate(p,dir,angle);
+    this->a = Point(av);
+    this->b = Point(bv);
+    this->c = Point(cv);
+    this->d = Point(dv);
+    PositionVector ab = PositionVector(b)-PositionVector(a);
+    PositionVector ad = PositionVector(d)-PositionVector(a);
+    N = Cross(ad,ab);
+    N.normalize();
+    return *this;
+  }
+
+  Parallelogram& Parallelogram::pitch(double angle)
+  {
+    PositionVector bd = PositionVector(b)-PositionVector(d);
+    rotate(this->a,bd,angle);
+    return *this;
+  }
+
+  Parallelogram& Parallelogram::roll(double angle)
+  {
+    PositionVector ac = PositionVector(c)-PositionVector(a);
+    rotate(this->a,ac,angle);
+    return *this;
+  }
+
+  Parallelogram& Parallelogram::turn(double angle)
+  {
+    rotate(this->a,getNormal(),angle);
+    return *this;
+  }
+  
+  Parallelogram& Parallelogram::setArea(double A)
+  {
+    double alpha = getAlpha();
+    double ratio = lengthAB()/lengthAD();
+    resize(A,alpha,ratio);
+    return *this;
+  }
+
+  Parallelogram&  Parallelogram::resize(double A1,double alpha, double ratio)
+  {
+    PositionVector ab0 = PositionVector(b)-PositionVector(a);
+    ab0.normalize();
+    PositionVector ad0 = PositionVector(d)-PositionVector(a);
+    ad0.normalize();
+    // Denote L1 = |**ab**| and L2 = |**ad**|. ratio = L1/L2 then L1 = L2*ratio and L2=L1/ratio
+    // A = L1*L2*sin(alpha) = L2*ratio*L2*sin(alpha)
+    // L2^2 = A/(ratio*sin(alpha)) and L2 = sqrt(A/ratio*sin(alpha))
+    // Equally: A = L1*(L1/ratio)*sin(alpha)
+    // L1^2 = A*ratio/sin(alpha) and L1 = sqrt(A*ratio/sin(alpha))
+    double L1 = sqrt(A1*ratio/sin(alpha));
+    double L2 = sqrt(A1/(ratio*sin(alpha)));
+    PositionVector ab = L1*ab0;
+    PositionVector ad = L2*ad0;
+    // Point *b* is defined by **a**+**ab**
+    this->b = Point(PositionVector(a)+ab);
+    // Point *d* is defined by vector **a**+**ad**
+    this->d = Point(PositionVector(a)+ad);
+    // Point *c* is defined by vector **b**+**ad**
+    this->c = Point(PositionVector(b)+ad);
+    this->A = A1;
+    return *this;
+  }
+
+  Point Parallelogram::getCenterPoint()const
+  {
+    Point center = (this->a+this->c)/2.0;
+    return center;
+  }
+
+  bool Parallelogram::intersectShape(const Point& o, const PositionVector& beam)const
+  {
+    //Check if the observation point is strictly below the Parallelogra.
+    double o_maxz = o.getZ();
+    double maxz = getMaxZ();
+    if (o_maxz >= maxz){
+      cout << "ABOVE" <<endl;
+      return false;
+    }
+    /// **Calculate line - plane intersection**    
+    /// Given observation point *o* is below the Parallelogram (leaf) calculate the intersection point.  
+    /// With vector notation a plane is defined by a set of all points \f$ P \f$ that satisfy
+    /// the equation \f$ (P - p_0) \cdot \vec{N} \f$  where \f$ p_0 \f$ is
+    /// a point in the plane and \f$ \vec{N} \f$ is the normal to the plane.   
+    /// The set of all points \f$ P_l \f$ on a line can be defined as \f$ P_l = l_0 + d\hat{l} \f$
+    /// where \f$l_0 \f$ is a point on the line, \f$\hat{l} \f$ is a unit vector defining the
+    /// direction of the line and *d* is a scalar.
+    ///
+    /// Substituting \f$ P \f$ for the line equation in the plane equation gives \f$ (l_0 + d\hat{l}-p_0) \cdot \vec{N})=0 \f$.   
+    /// Expanding gives \f$(\hat{l} \cdot d\vec{N}) + (l_0-p0) \cdot \vec{N} = 0 \f$  and solving for \f$ d \f$ gives
+    /// \f$ d = (p_0-l_0) \cdot \vec{N} / \hat{l} \cdot \vec{N} \f$.
+    ///
+    ///Geometric interpretation: when denominator \f$ \hat{l} \cdot \vec{N} = 0 \f$ then \f$ d \f$ is undefined. The line is perpendicular
+    ///plane normal and parallel with the plane. When the numerator is 0 the line is in the plane.
+    ///For our purposes (Parallelogram as a leaf model) we skip both of these cases.
+    ///When denominator \f$ \hat{l} \cdot \vec{N} != 0 \f$ we can solve for \f$ d \f$ and find the line-plane intersection point (Wikipedia).
+    double cosalpha = Dot(beam,N);
+    if (fabs(cosalpha)  < EPS15){
+      return false;
+    } 
+    /// \sa intersectionPoint Calculate intersection point
+    Point p_intersect = intersectionPoint(o,beam); 
+    /// \sa insideShape Check if the point is inside Parallelogram
+    bool p_inside = insideShape(p_intersect);
+    if (p_inside == true){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  Point Parallelogram::intersectionPoint(const Point& o, const PositionVector& l)const
+  {
+    //Intersection point must exist
+    double cosalpha = Dot(l,N);
+    //Just in case normalize beam direction
+    PositionVector l_tmp(l);
+    l_tmp.normalize();
+    //The intersection point with the plane defined by the Parallelogram exists.
+    //The nadir point is on the plane
+    Point p0(getA());
+    //The observation point is on the line
+    Point l0(o);
+    //PositionVector  pointing towards the intersection point
+    PositionVector p1(p0-l0);
+    /// \internal
+    /// The distance \f$ d \f$ from the observation point \f$ o \f$ to the intersection point \f$ d = (p0-o)\cdot\vec{N}/\hat{l} \cdot\vec{N} \f$,
+    /// where \f$ {p0} \f$ is the Parallelogram nadir point. The point of intersection is \f$ p = o+d\hat{l} \f$.
+    /// \snippet{lineno} Parallelogram.cc PI
+    // [PI]
+    double d =  Dot(p1,N)/cosalpha;
+    Point p_intersection = Point(PositionVector(o)+d*l_tmp);
+    // [PI]
+    ///\endinternal
+    return p_intersection;
+  }
+
+  bool Parallelogram::insideShape(const Point& p)const
+  { //Move parallelogram and p to origo
+    Point b0 = (getB()) - (getA());
+    Point d0 = (getD()) - (getA());
+    Point p0 = p - (getA());
+    ///\internal
+    ///The equation \f$ p = t_1\overrightarrow{ab}+t_2\overrightarrow{ad} \f$ defines all the points in Parallelogram.
+    ///We can expand the equation :
+    ///\f{eqnarray*}{
+    /// p_x = t_1\overrightarrow{ab}_x+t_2\overrightarrow{ad}_x\\
+    /// p_y = t_1\overrightarrow{ab}_y+t_2\overrightarrow{ad}_y\\
+    /// p_z = t_1\overrightarrow{ab}_z+t_2\overrightarrow{ad}_z
+    /// \f}
+    ///With three equations and two unknowns we can solve for \f$ t_1 \f$ and \f$ t_2 \f$.							       
+    ///\snippet{lineno} Parallelogram.cc IN
+    // [IN]
+    //Solution for t1
+    double t1 = (p.getY()-(p.getX()*d0.getY()/d0.getX()))/(b0.getY()-((b0.getX()*d0.getY())/d0.getX()));
+    //Solution for t2
+    double t2 = (p0.getX()-t1*b0.getX())/d0.getX();
+    // [IN]
+    ///\endinternal
+    ///If \f$ t_1 \f$ and \f$ t_2 \in \left[0,1\right] \f$  the point \f$ p \f$ is strictly inside Parallelogram
+    if ((t1 >= 0.0) && (t1 <= 1.0) && (t2 >= 0.0) && (t2 <= 1.0)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  double Parallelogram::getAlpha()const
+  {
+    PositionVector ab = PositionVector(b)-PositionVector(a);
+    PositionVector ad = PositionVector(d)-PositionVector(a);
+    double cos_alpha = Dot(ab.normalize(),ad.normalize());
+    double alpha = acos(cos_alpha);
+    return alpha;
+  }
+
+  double Parallelogram::getMaxZ()const
+  {
+    double maxz = max(a.getZ(),b.getZ());
+    maxz = max(maxz,c.getZ());
+    maxz = max(maxz,d.getZ());
+    return maxz;
+  }
+      
+}//end namespace
+
