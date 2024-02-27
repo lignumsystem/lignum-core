781516e64abc52080220c8201c7521b911856518
jariperttunen <jari.perttunen@icloud.com>
2024-02-27 13:22:11 +0200

    Doxygen comments.

--- stl-voxelspace/include/ForestDescriptor.h
+++ stl-voxelspace/include/ForestDescriptor.h
@@ -3,18 +3,22 @@
 
 #include <LGMSymbols.h>
 using namespace Lignum;
+///\file ForestDescriptor.h 
+///\brief Descriptive stand level statistics 
+///
+///Collect descriptive statistics from the forest stand, i.e. voxel space.
 
-
-//ForestDescriptor collects descriptive statistics and indices for the
-//forest in the voxel space. The natural place to insert data 
+///ForestDescriptor collects descriptive statistics and indices for the
+///forest in the voxel space. The natural place to insert data.
 class ForestDescriptor{
   friend double SetValue(ForestDescriptor& fd, LGMAD name, double value);
   friend double GetValue(const ForestDescriptor& fd, LGMAD name);
 public:
   ForestDescriptor()
     :afb(0.0),afc(0.0),laib(0.0),laic(0.0),h(-1.0),cbase(R_HUGE){}
-  //Update LAI, A is the area of the voxel space in  meters
-  //Call after  the last call to InsertVoxelObjects
+  ///Update LAI
+  ///\param A Area of the voxel space in  meters
+  ///\pre Call after  the last call to InsertVoxelObjects
   void updateLAI(double A){laic = afc/A;laib = afb/A;}
   //Reset by VoxelSpace::reset()
   void reset(){
@@ -22,13 +26,14 @@ public:
     laic=0.0;h=-1.0;cbase=R_HUGE;
   }
 private:
-  double afb;//leaf area, broad leaved
-  double afc;//needle area, conifers
-  double laib;//leaf area index for broad leaved
-  double laic;//leaf area index for conifers
-  double h;//height of the tallest tree 
-  double cbase;//lower limit  for tree crowns (defined  as the smallest
-	      //z-coord of all segments that have foliage)
+  double afb;///<Leaf area, broad leaved
+  double afc;///<Needle area, conifers
+  double laib;///<Leaf area index for broad leaved
+  double laic;///<Leaf area index for conifers
+  double h;///<Height of the tallest tree
+  ///Lower limit  for tree crowns (defined  as the smallest z-coord of all segments that have foliage)
+  double cbase;
+	      
 };
 
 inline double SetValue(ForestDescriptor& fd, LGMAD name, double value)
