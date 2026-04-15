461b5128c3fd76704b024392d523eece93d9e212
jariperttunen <jari.perttunen@icloud.com>
2026-04-15 10:30:35 +0300

    TMatrix2D and TMatrix3D: Added constructor from std::vector. Supplementary documentation, for example method parameter explanations.

--- c++adt/include/TMatrix3D.h
+++ c++adt/include/TMatrix3D.h
@@ -65,59 +65,123 @@ namespace cxxadt{
     int length;
     string message;
   };
-    
-  ///2D Matrix 
+
+  class TMatrixInitSizeError{
+  public:
+    TMatrixInitSizeError(const vector<int>& v)
+    :vdim(v){}
+  private:
+    std::vector<int> vdim;
+  };
+  
+  ///2D Matrix of rows and columns
   template <class T>
+  ///\brief 2D matrix
     class TMatrix2D: public vector<vector<T> >{
     public:
-    ///Default constructor, empty matrix
+    ///\brief Constructor, empty matrix
     TMatrix2D();
-    ///Constructor, dimensions only 
+    ///\brief Constructor, dimensions only
+    ///\param x Rows
+    ///\param y Columns
     TMatrix2D(int x, int y);
-    ///Constructior, initialize with copies of 't'
+    ///\brief Constructor, initialize with copies of 't'
+    ///\param x Rows
+    ///\param y Columns
+    ///\param t Vector element
     TMatrix2D(int x, int y, const T& t);
+    ///\brief Constructor, initialize from vector \p v
+    ///\param x Rows
+    ///\param y Cols
+    ///\param v Data vector
+    ///\pre \f$ x \times y = |v|\f$
+    ///\pre \f$ y = |v| \div x \f$ (Rows and columns must match the size of the vector \p v)
+    ///\exception TMatrixInitSizeError if preconditon violation
+    TMatrix2D(int x, int y, const vector<T>& v);
+    ///\brief Number of rows
+    ///\retval xd Number of rows
     int rows()const {return xd;}
+    ///\brief Number of columns
+    ///\retval yd Number of columns
     int cols()const {return yd;}
+    ///\brief Initialize matrix with copies of \p t.
+    ///\retval self Initialized matrix
     TMatrix2D<T>& init(const T& t);
     ///Resize clears the matrix first, i.e. data will be  lost
     ///\param x The number of the rows
     ///\param y The number of new columns
     ///\post No data in the matrix
-    ///\return The new matrix
+    ///\retval self Resized matrix
     ///\sa TMatrix2D::init TMatrix2D::append
     TMatrix2D<T>& resize(int x, int y);
-    ///Append one row to the matrix
+    ///\brief Append one row to the matrix
     ///\param v The new row as vector
     ///\pre The length of `v` must be the number of vector columns
-    ///\return The new matrix
-    ///\exception AppendException
-    ///If length of \p v != \p yd throw AppendException
+    ///\retval self Resized matrix with new row
+    ///\exception AppendException If length of \p v != \p yd throw AppendException
     TMatrix2D<T>& append(const vector<T>& v);
     private:
-    int xd,yd; //rows and columns
+    ///\brief Rows and columns
+    int xd,yd;
   };
-  ///3D Matrix
+  ///\brief 3D Matrix
+  ///
+  ///A stack of 2D matrices with rows, columns and depth (or layers).
   template <class T>
     class TMatrix3D: public vector<TMatrix2D<T> >{
     public:
-    ///Default constructor, empty 3D array
+    ///\brief Constructor, empty 3D matrix
     TMatrix3D();
-    ///Constructor, dimensions only 
+    ///\brief Constructor, dimensions only
+    ///\param x Rows
+    ///\param y Columns
+    ///\param z Depth
     TMatrix3D(int x, int y, int z);
-    ///Constructior, initialize with copies of 't'
+    ///\brief Constructior, initialize with copies of \p t
+    ///\param x Rows
+    ///\param y Columns
+    ///\param z Depth
+    ///\param t Matrix element
     TMatrix3D(int x, int y, int z, const T& t);
+    //\brief Constructor, intialize with vector \p v
+    ///\param x Rows
+    ///\param y Columns
+    ///\param z Depth
+    ///\param v Vector
+    ///\pre \f$ x \times y \times z  = |v|\f$
+    ///\pre \f$ |v| \bmod x = 0 \f$
+    ///\pre \f$ y*z \bmod (|v| \div x) = 0\f$
+    ///\exception TMatrixInitSizeError if preconditon violation
+    TMatrix3D(int x, int y, int z,const vector<T>& v);
+    ///\brief Number of rows (1st dimension)
+    ///\retval xd 1st dimension
     int rows()const {return xd;}
+    ///\brief Number of columns (2nd dimension)
+    ///\retval yd 2nd dimension
     int cols()const {return yd;}
+    ///\brief Number of layers (3rd dimension)
+    ///\retval zd 3rd dimension
     int zdim()const {return zd;}
+    ///\brief Number of layers (3rd dimension)
+    ///
+    ///Alias for zdim()
+    ///\retval zd 3rd dimension
+    int layers()const{return zdim();}
+    ///\brief Initialize the matrix with copies of \p t.
+    ///\pre Matrix must have dimensions defined 
+    ///\retval self Initialized matrix
     TMatrix3D<T>& init(const T& t);
-    ///Clear the data first, i.e. data will be lost
-    /// \post No data in the 3D array
-    /// \sa TMarix3D::init
+    ///\brief Resize matrix
+    ///
+    ///Clear data first, i.e. data will be lost
+    ///\retval self Initialized matrix
+    ///\post Data undefined in the 3D matrix
+    ///\sa TMarix3D::init()
     TMatrix3D<T>& resize(int x, int y, int z);
     private:
-    int xd; ///< Slices to 2D matrices
-    int yd; ///< Rows in 2D matrices
-    int zd; ///< Columns in 2D matrices
+    int xd; ///< 1st dimension, slices to 2D matrices
+    int yd; ///< 2nd dimension, rows in 2D matrices
+    int zd; ///< 3rd dimension, columns in 2D matrices
   };
 
 }
--- c++adt/include/TMatrix3DI.h
+++ c++adt/include/TMatrix3DI.h
@@ -5,7 +5,7 @@
 
 
 namespace cxxadt{
-  
+
   template <class T>
   TMatrix2D<T>::TMatrix2D()
     :xd(0),yd(0){}
@@ -22,6 +22,30 @@ namespace cxxadt{
     :vector<vector<T> >(x,vector<T>(y,t)),xd(x),yd(y)
     {
     }
+
+  template <class T>
+  TMatrix2D<T>::TMatrix2D(int x, int y, const vector<T>& v)
+    :vector<vector<T>>(x,vector<T>(y)),xd(x),yd(y)
+  {
+    if (x*y != v.size()){
+      int s = v.size();
+      vector<int> v1{x,y,s};
+      throw TMatrixInitSizeError(v1);
+    }
+    int cols =  v.size()/x;
+    if (cols != y){
+      int s = v.size();
+      vector<int> v1{x,y,s};
+      throw TMatrixInitSizeError(v1);
+    }
+    //Short cut to data vector
+    const T* d = v.data();
+    for (int i =0 ; i < xd; i++){
+      for (int j = 0; j < yd; j++){
+	(*this)[i][j] = d[i*yd+j];
+      }
+    }
+  }
   
   template <class T>
     TMatrix2D<T>& TMatrix2D<T>::init(const T& t)
@@ -67,6 +91,41 @@ namespace cxxadt{
     {
     }
 
+  template <class T>
+  TMatrix3D<T>::TMatrix3D(int x, int y, int z,const vector<T>& v)
+    :vector<TMatrix2D<T>>(x,TMatrix2D<T>(y,z)),xd(x),yd(y),zd(z)
+  {
+    //The requested sizes must match v size
+    if (x*y*z != v.size()){
+      int s = v.size();
+      vector<int> v1{x,y,z,s};
+      throw TMatrixInitSizeError(v1);
+    }
+    //v size must must be divisible by x 
+    if (v.size() % x != 0){
+      int s = v.size();
+      vector<int> v1{x,y,z,s};
+      throw TMatrixInitSizeError(v1);
+    }
+    //Number of Matrix2D<T>
+    int n = v.size() / x;
+    //Must have place for exactly n matrices
+    if (y*z % n != 0){
+      int s = v.size();
+      vector<int> v1{x,y,z,s};
+      throw TMatrixInitSizeError(v1);
+    }
+    //Short cut to data vector
+    const T* d = v.data();
+    for (int i = 0; i < xd; i++){
+      for (int j = 0; j < yd; j++){
+	for (int k = 0; k < zd; k++){
+	  (*this)[i][j][k] = d[i*yd*zd+j*zd+k];
+	}
+      }
+    }
+  }
+  
   template <class T>
     TMatrix3D<T>& TMatrix3D<T>::init(const T& t)
     {
